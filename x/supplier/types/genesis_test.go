55e6e91da6362611a913779b083de97409d61927
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-10-10 16:55:18 -0400

    [Supplier] Scaffolded the Supplier type (without CRUD messages) and nothing else (#48)
    Simply ran the following command:
    
    ```
    ignite scaffold map supplier --module supplier --signer address --no-message --index address --yes
    ```

--- x/supplier/types/genesis_test.go
+++ x/supplier/types/genesis_test.go
@@ -19,12 +19,34 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
+			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
+				SupplierList: []types.Supplier{
+					{
+						Address: "0",
+					},
+					{
+						Address: "1",
+					},
+				},
 				// this line is used by starport scaffolding # types/genesis/validField
 			},
 			valid: true,
+		},
+		{
+			desc: "duplicated supplier",
+			genState: &types.GenesisState{
+				SupplierList: []types.Supplier{
+					{
+						Address: "0",
+					},
+					{
+						Address: "0",
+					},
+				},
+			},
+			valid: false,
 		},
 		// this line is used by starport scaffolding # types/genesis/testcase
 	}
