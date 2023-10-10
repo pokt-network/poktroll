f9c7f7fa2960c945643dae435142fc0e6df122aa
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-10-10 16:13:44 -0400

    ignite scaffold map supplier --module supplier --signer address --no-message --index address --yes

--- x/supplier/client/cli/query.go
+++ x/supplier/client/cli/query.go
@@ -25,6 +25,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(CmdListSupplier())
+	cmd.AddCommand(CmdShowSupplier())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
