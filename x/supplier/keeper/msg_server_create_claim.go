44b421f6afd244f300d425fb8592db70dbd5513b
Bryan White <bryanchriswhite@gmail.com>
2023-10-11 23:56:14 +0200

    chore:  add TODO comment in msg handler

--- x/supplier/keeper/msg_server_create_claim.go
+++ x/supplier/keeper/msg_server_create_claim.go
@@ -10,7 +10,32 @@ import (
 func (k msgServer) CreateClaim(goCtx context.Context, msg *types.MsgCreateClaim) (*types.MsgCreateClaimResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
+	/*
+		INCOMPLETE: Handling the message
+
+		## Validation
+
+		### Session validation
+		1. [ ] claimed session ID == retrieved session ID
+		2. [ ] this supplier is in the session's suppliers list
+
+		### Msg distribution validation (depends on session validation)
+		1. [ ] pseudo-randomize earliest block offset
+		2. [ ] governance-based earliest block offset
+
+		### Claim validation
+		1. [ ] session validation
+		2. [ ] msg distribution validation
+
+		## Persistence
+		1. [ ] create claim message
+			- supplier address
+			- session header
+			- claim
+		2. [ ] last block height commitment; derives:
+			- last block committed hash, must match proof path
+			- session ID (?)
+	*/
 	_ = ctx
 
 	return &types.MsgCreateClaimResponse{}, nil
