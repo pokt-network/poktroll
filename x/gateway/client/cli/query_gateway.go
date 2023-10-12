fe588ec0f1797f4d78191de742247557b182756a
harry <53987565+h5law@users.noreply.github.com>
2023-10-12 13:09:04 +0100

    feat: scaffold gateway actor

---
+++ x/gateway/client/cli/query_gateway.go
@@ -0,0 +1,78 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+
+	"pocket/x/gateway/types"
+)
+
+func CmdListGateway() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-gateway",
+		Short: "list all gateway",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllGatewayRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.GatewayAll(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdShowGateway() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-gateway [address]",
+		Short: "shows a gateway",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			argAddress := args[0]
+
+			params := &types.QueryGetGatewayRequest{
+				Address: argAddress,
+			}
+
+			res, err := queryClient.Gateway(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
