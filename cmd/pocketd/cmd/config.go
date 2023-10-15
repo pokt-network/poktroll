80186a2cd58f68a741ec693b97a4ac0489247031
harry <53987565+h5law@users.noreply.github.com>
2023-10-15 14:10:43 +0100

    feat: test gateway module address is deterministic

--- cmd/pocketd/cmd/config.go
+++ cmd/pocketd/cmd/config.go
@@ -6,7 +6,7 @@ import (
 	"pocket/app"
 )
 
-func initSDKConfig() {
+func InitSDKConfig() {
 	// Set prefixes
 	accountPubKeyPrefix := app.AccountAddressPrefix + "pub"
 	validatorAddressPrefix := app.AccountAddressPrefix + "valoper"
