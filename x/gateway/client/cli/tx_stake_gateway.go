a15e1f78bcd198603a4748d07fa43fc31bc51fa5
harry <53987565+h5law@users.noreply.github.com>
2023-10-15 13:14:01 +0100

    feat: update stake message creation

--- x/gateway/client/cli/tx_stake_gateway.go
+++ x/gateway/client/cli/tx_stake_gateway.go
@@ -3,29 +3,35 @@ package cli
 import (
 	"strconv"
 
+	"pocket/x/gateway/types"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
-	"pocket/x/gateway/types"
 )
 
 var _ = strconv.Itoa(0)
 
 func CmdStakeGateway() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "stake-gateway",
+		Use:   "stake-gateway [amount]",
 		Short: "Broadcast message stake-gateway",
-		Args:  cobra.ExactArgs(0),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
-
+			stakeAmountString := args[0]
+			stakeAmount, err := sdk.ParseCoinNormalized(stakeAmountString)
+			if err != nil {
+				return err
+			}
 			msg := types.NewMsgStakeGateway(
 				clientCtx.GetFromAddress().String(),
+				stakeAmount,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
