ab0a1f1bf8362899b8ccdb6c4c4266a6efaec6fe
Bryan White <bryanchriswhite@gmail.com>
2023-10-17 16:12:10 +0200

    [Miner] chore: scaffold create-claim message (#43)
    * chore: scaffold create claim message
    
    `ignite scaffold message create-claim session_header:SessionHeader root_hash --signer SupplierAddress --module supplier`
    (NB: generated a temporary SessionHeader duplicate in the supplier module so the above would work; subsequently, removed and updated protobuf import and usage.)
    
    * refactor: use `bytes` type for `MsgCreateClaim#root_hash` field
    
    * chore:  add TODO comment in msg handler
    
    * chore: add TODOs
    
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    
    ---------
    
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

---
+++ x/supplier/types/message_create_claim_test.go
@@ -0,0 +1,42 @@
+package types
+
+import (
+	"testing"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+	"pocket/testutil/sample"
+)
+
+// TODO(@bryanchriswhite): Add unit tests for message validation when adding the business logic.
+
+func TestMsgCreateClaim_ValidateBasic(t *testing.T) {
+	tests := []struct {
+		name string
+		msg  MsgCreateClaim
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgCreateClaim{
+				SupplierAddress: "invalid_address",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		}, {
+			name: "valid address",
+			msg: MsgCreateClaim{
+				SupplierAddress: sample.AccAddress(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
