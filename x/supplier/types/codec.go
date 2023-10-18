cecace67b7b7eb85ff2f56c30356060d9ccefd6a
Bryan White <bryanchriswhite@gmail.com>
2023-10-18 23:24:15 +0200

    [Miner, Supplier] chore: scaffold submit-proof message (#44)
    * chore: scaffold submit proof message
    
    ignite scaffold message submit-proof session_header:SessionHeader proof --signer supplier_address --module supplier
    (NB: generated a temporary SessionHeader duplicate in the supplier module so the above would work; subsequently, removed and updated protobuf import and usage.)
    
    * refactor: use `bytes` type for `MsgSubmitProof#Proof` field
    
    * chore:  add TODO comment in msg handler
    
    * chore: add TODOs
    
    (cherry picked from commit b158071f551e9caea2408dec2883d0b301a2c541)
    
    * chore: add comment to `MsgSubmitProof#proof` field
    
    Co-authored-by: Olshansk <olshansky.daniel@gmail.com>
    
    * chore: review feedback improvements
    
    * trigger CI
    
    ---------
    
    Co-authored-by: Olshansk <olshansky.daniel@gmail.com>

--- x/supplier/types/codec.go
+++ x/supplier/types/codec.go
@@ -11,6 +11,7 @@ func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgStakeSupplier{}, "supplier/StakeSupplier", nil)
 	cdc.RegisterConcrete(&MsgUnstakeSupplier{}, "supplier/UnstakeSupplier", nil)
 	cdc.RegisterConcrete(&MsgCreateClaim{}, "supplier/CreateClaim", nil)
+	cdc.RegisterConcrete(&MsgSubmitProof{}, "supplier/SubmitProof", nil)
 	// this line is used by starport scaffolding # 2
 }
 
@@ -21,6 +22,9 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	)
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgCreateClaim{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgSubmitProof{},
 	)
 	// this line is used by starport scaffolding # 3
 
