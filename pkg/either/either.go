2f114b3c1acdd3400c707a80060c4f933c2dc960
Redouane Lakrache <r3d0ne@gmail.com>
2023-10-19 19:52:23 +0200

    chore: comply to Either props & use zeroValue fn

--- pkg/either/either.go
+++ pkg/either/either.go
@@ -1,28 +1,43 @@
 package either
 
-import "reflect"
-
-type Either[T any] struct {
-	value T
-	error error
+// Either represents a type that can hold either a value of type T or an error.
+// It's commonly used in functional programming to encapsulate functions that might fail.
+// This way, instead of returning a value and an error separately, both are combined into a single type.
+type Either[R any] struct {
+	right R     // conventionally holds the success value
+	left  error // conventionally holds the error, if any
 }
 
-func NewEither[T any](value T, error error) Either[T] {
-	return Either[T]{value: value, error: error}
+func NewEither[R any](right R, left error) Either[R] {
+	return Either[R]{right: right, left: left}
 }
 
-func Right[T any](value T) Either[T] {
-	return Either[T]{value: value, error: nil}
+// Success creates a successful Either value.
+func Success[T any](value T) Either[T] {
+	return Either[T]{right: value, left: nil}
 }
 
-func Left[T any](error error) Either[T] {
-	// see: https://stackoverflow.com/questions/73864711/get-type-parameter-from-a-generic-struct-using-reflection
-	var zeroT [0]T
-	typeT := reflect.TypeOf(zeroT).Elem()
-	zeroValue := reflect.Zero(typeT).Interface().(T)
-	return Either[T]{value: zeroValue, error: error}
+// Error creates an error Either value.
+func Error[T any](err error) Either[T] {
+	return Either[T]{right: zeroValue[T](), left: err}
 }
 
+// IsSuccess checks if the Either contains a success value.
+func (m Either[T]) IsSuccess() bool {
+	return m.left == nil
+}
+
+// IsError checks if the Either contains an error.
+func (m Either[T]) IsError() bool {
+	return m.left != nil
+}
+
+// ValueOrError unpacks the Either value.
 func (m Either[T]) ValueOrError() (T, error) {
-	return m.value, m.error
+	return m.right, m.left
+}
+
+// Helper function to get the zero value for any type.
+func zeroValue[T any]() (zero T) {
+	return zero
 }
