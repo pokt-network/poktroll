f6582b8f2f691a2ec50158364dafffe98b8d13cf
Redouane Lakrache <r3d0ne@gmail.com>
2023-10-20 01:50:20 +0200

    Fix grammar in comments
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- pkg/proxy/interface.go
+++ pkg/proxy/interface.go
@@ -8,19 +8,19 @@ import (
 )
 
 // RelayerProxy is the interface for the proxy that serves relays to the application.
-// It is responsible for starting and stopping all the supported the proxies.
-// Wile handling requests and responding to them in a closed loop, it also notifies
-// the miner about the relays that has been served.
+// It is responsible for starting and stopping all supported proxies.
+// While handling requests and responding in a closed loop, it also notifies
+// the miner about the relays that have been served.
 type RelayerProxy interface {
 
-	// Start starts all the supported proxies or returns an error if any of them fails to start.
+	// Start starts all supported proxies and returns an error if any of them fail to start.
 	Start(ctx context.Context) error
 
-	// Stop stops all the supported proxies or returns an error if any of them fails.
+	// Stop stops all supported proxies and returns an error if any of them fail.
 	Stop() error
 
-	// ServedRelays returns an observable that notifies the miner about the relays that has been served.
-	// A served relay is a relay that its RelayRequest's signature and session has been verified
+	// ServedRelays returns an observable that notifies the miner about the relays that have been served.
+	// A served relay is one whose RelayRequest's signature and session have been verified,
 	// and its RelayResponse has been signed and successfully sent to the client.
 	ServedRelays() observable.Observable[*types.Relay]
 }
