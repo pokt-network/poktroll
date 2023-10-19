378350dce667c1c4ec1b4eb7143dbf93bffa4551
Redouane Lakrache <r3d0ne@gmail.com>
2023-10-19 22:11:09 +0200

    chore: address change request about properties description

--- pkg/either/either.go
+++ pkg/either/either.go
@@ -4,8 +4,8 @@ package either
 // It's commonly used in functional programming to encapsulate functions that might fail.
 // This way, instead of returning a value and an error separately, both are combined into a single type.
 type Either[R any] struct {
-	right R     // conventionally holds the success value
-	left  error // conventionally holds the error, if any
+	right R     // holds the success value
+	left  error // holds the error, if any
 }
 
 func NewEither[R any](right R, left error) Either[R] {
