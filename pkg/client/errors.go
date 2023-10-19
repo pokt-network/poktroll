6273f52c0dfac3be74954d37b6476bff39538f4d
Bryan White <bryanchriswhite@gmail.com>
2023-10-19 22:42:16 +0200

    chore: add query client errors

---
+++ pkg/client/errors.go
@@ -0,0 +1,11 @@
+package client
+
+import errorsmod "cosmossdk.io/errors"
+
+var (
+	ErrDial       = errorsmod.Register(codespace, 1, "dialing for connection failed")
+	ErrConnClosed = errorsmod.Register(codespace, 2, "connection closed")
+	ErrSubscribe  = errorsmod.Register(codespace, 3, "failed to subscribe to events")
+	ErrReceive    = errorsmod.Register(codespace, 4, "failed to receive event")
+	codespace     = "query_client"
+)
