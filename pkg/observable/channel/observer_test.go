f520215b3c08b800ebcac7e1f6375ee312be6f20
Bryan White <bryanchriswhite@gmail.com>
2023-10-20 10:55:52 +0200

    [Pkg] feat: add channel observable (#31)
    * feat: add notifiable observable
    
    Co-authored-by: red-0ne <r3d0ne@gmail.com>
    
    * fixup: observable
    
    (cherry picked from commit bcf700405b5e4bd71bf9bb650c988526fa16c728)
    
    * refactor/fix: notifiable observable improvements
    
    * chore: more review improvements
    
    * refactor: renaming
    
    - `notifiable` pkg to `channel`
    - `notifiableObservable` struct to `channelObservable`
    - `observer` struct to `channelObserver`
    - `notifier` vars to `producer`
    - `notifee` vars to `observable` (or similar)
    
    * chore: update comments
    
    * refactor: simplify drainCh test helper
    
    * test: fix timeout
    
    * test: rename observable test functions
    
    * test: add test TODOs
    
    * chore: update comments
    
    * refactor: simplify observable & observer
    
    * test: fix & add observable tests
    
    * test: cleanup & comment observable tests
    
    * fixup: observable
    
    (cherry picked from commit 33f3196535b7dae154e01f93aab36f70cda8fc4f)
    
    * fixup: observable test
    
    (cherry picked from commit 9c206da115dc35843d588313c2215a0e649c6df6)
    
    * refactor: simplify & cleanup
    
    * chore: cleanup logs & comments
    
    * chore: improve comments
    
    * refactor: DrainChannel test helper
    
    * shore: cleanup & simplify
    
    * test: comment out flaky test cases
    
    * fixup: drain channel helper
    
    * chore: improve var name
    
    * fixup: drain channel helper
    
    * test: shorten timeout
    
    * chore: cleanup
    
    * chore: cleanup, simplification, review improvements
    
    (cherry picked from commit 92a547da29ec526d415f6967ccfa5988c3f5ca1d)
    
    * chore: improve comments
    
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    
    * chore: improve comments
    
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    
    * refactor: rename `Observable#Close()` to `#UnsubscribeAll()`
    
    * chore: improve comments
    
    * chore: misc. review feedback improvements
    
    * chore: improve comment
    
    * chore: review improvements
    
    * chore: last minute improvements
    
    * docs: add initial docs/pkg/observable docs
    
    * chore: add go package README.md template
    
    * chore: fix TODO comments
    
    * chore: move & rename pkg readme template
    
    * test: refactor async test errors
    
    ---------
    
    Co-authored-by: red-0ne <r3d0ne@gmail.com>
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

---
+++ pkg/observable/channel/observer_test.go
@@ -0,0 +1,81 @@
+package channel
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestObserver_Unsubscribe(t *testing.T) {
+	var (
+		onUnsubscribeCalled = false
+		publishCh           = make(chan int, 1)
+	)
+	obsvr := &channelObserver[int]{
+		observerMu: &sync.RWMutex{},
+		// using a buffered channel to keep the test synchronous
+		observerCh: publishCh,
+		onUnsubscribe: func(toRemove *channelObserver[int]) {
+			onUnsubscribeCalled = true
+		},
+	}
+
+	// should initially be open
+	require.Equal(t, false, obsvr.isClosed)
+
+	publishCh <- 1
+	require.Equal(t, false, obsvr.isClosed)
+
+	obsvr.Unsubscribe()
+	// should be isClosed after `#Unsubscribe()`
+	require.Equal(t, true, obsvr.isClosed)
+	require.True(t, onUnsubscribeCalled)
+}
+
+func TestObserver_ConcurrentUnsubscribe(t *testing.T) {
+	var (
+		onUnsubscribeCalled = false
+		publishCh           = make(chan int, 1)
+	)
+	obsvr := &channelObserver[int]{
+		ctx:        context.Background(),
+		observerMu: &sync.RWMutex{},
+		// using a buffered channel to keep the test synchronous
+		observerCh: publishCh,
+		onUnsubscribe: func(toRemove *channelObserver[int]) {
+			onUnsubscribeCalled = true
+		},
+	}
+
+	require.Equal(t, false, obsvr.isClosed, "observer channel should initially be open")
+
+	// concurrently & continuously publish until the test cleanup runs
+	done := make(chan struct{}, 1)
+	go func() {
+		for idx := 0; ; idx++ {
+			// return when done receives; otherwise,
+			select {
+			case <-done:
+				return
+			default:
+			}
+
+			// publish a value
+			obsvr.notify(idx)
+		}
+	}()
+	// send on done when the test cleans up
+	t.Cleanup(func() { done <- struct{}{} })
+
+	// it should still be open after a bit of inactivity
+	time.Sleep(10 * time.Millisecond)
+	require.Equal(t, false, obsvr.isClosed)
+
+	obsvr.Unsubscribe()
+	// should be isClosed after `#Unsubscribe()`
+	require.Equal(t, true, obsvr.isClosed)
+	require.True(t, onUnsubscribeCalled)
+}
