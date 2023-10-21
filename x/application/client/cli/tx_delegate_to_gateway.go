6710ded540c83e9a30137a7f8bee686f542ef481
harry <53987565+h5law@users.noreply.github.com>
2023-10-21 14:58:42 +0100

    feat: scaffold delegate-to-gateway message

---
+++ x/application/client/cli/tx_delegate_to_gateway.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+	"pocket/x/application/types"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdDelegateToGateway() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delegate-to-gateway",
+		Short: "Broadcast message delegate-to-gateway",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgDelegateToGateway(
+				clientCtx.GetFromAddress().String(),
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
