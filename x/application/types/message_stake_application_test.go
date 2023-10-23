5e031509cc40c0c0bd2d3b9f4c859c9b009746bc
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-10-23 16:56:42 -0400

    Implemented TestMsgStakeApplication_ValidateBasic

--- x/application/types/message_stake_application_test.go
+++ x/application/types/message_stake_application_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"pocket/testutil/sample"
+	sharedtypes "pocket/x/shared/types"
 )
 
 func TestMsgStakeApplication_ValidateBasic(t *testing.T) {
@@ -15,18 +16,28 @@ func TestMsgStakeApplication_ValidateBasic(t *testing.T) {
 		msg  MsgStakeApplication
 		err  error
 	}{
+		// address tests
 		{
 			name: "invalid address - nil stake",
 			msg: MsgStakeApplication{
 				Address: "invalid_address",
 				// Stake explicitly nil
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 			err: ErrAppInvalidAddress,
-		}, {
+		},
+
+		// stake related tests
+		{
 			name: "valid address - nil stake",
 			msg: MsgStakeApplication{
 				Address: sample.AccAddress(),
 				// Stake explicitly nil
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 			err: ErrAppInvalidStake,
 		}, {
@@ -34,12 +45,18 @@ func TestMsgStakeApplication_ValidateBasic(t *testing.T) {
 			msg: MsgStakeApplication{
 				Address: sample.AccAddress(),
 				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 		}, {
 			name: "valid address - zero stake",
 			msg: MsgStakeApplication{
 				Address: sample.AccAddress(),
 				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(0)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 			err: ErrAppInvalidStake,
 		}, {
@@ -47,6 +64,9 @@ func TestMsgStakeApplication_ValidateBasic(t *testing.T) {
 			msg: MsgStakeApplication{
 				Address: sample.AccAddress(),
 				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(-100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 			err: ErrAppInvalidStake,
 		}, {
@@ -54,6 +74,9 @@ func TestMsgStakeApplication_ValidateBasic(t *testing.T) {
 			msg: MsgStakeApplication{
 				Address: sample.AccAddress(),
 				Stake:   &sdk.Coin{Denom: "invalid", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 			err: ErrAppInvalidStake,
 		}, {
@@ -61,8 +84,64 @@ func TestMsgStakeApplication_ValidateBasic(t *testing.T) {
 			msg: MsgStakeApplication{
 				Address: sample.AccAddress(),
 				Stake:   &sdk.Coin{Denom: "", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+				},
 			},
 			err: ErrAppInvalidStake,
+		},
+
+		// service related tests
+		{
+			name: "invalid service configs - not present",
+			msg: MsgStakeApplication{
+				Address: sample.AccAddress(),
+				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+				// Services: omitted
+			},
+			err: ErrAppInvalidServiceConfigs,
+		},
+		{
+			name: "invalid service configs - empty",
+			msg: MsgStakeApplication{
+				Address:  sample.AccAddress(),
+				Stake:    &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{},
+			},
+			err: ErrAppInvalidServiceConfigs,
+		},
+		{
+			name: "invalid service configs - invalid service ID that's too long",
+			msg: MsgStakeApplication{
+				Address: sample.AccAddress(),
+				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "123456790"}},
+				},
+			},
+			err: ErrAppInvalidServiceConfigs,
+		},
+		{
+			name: "invalid service configs - invalid service ID that contains invalid characters",
+			msg: MsgStakeApplication{
+				Address: sample.AccAddress(),
+				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "12 45 !"}},
+				},
+			},
+			err: ErrAppInvalidServiceConfigs,
+		},
+		{
+			name: "valid service configs - multiple services",
+			msg: MsgStakeApplication{
+				Address: sample.AccAddress(),
+				Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+				Services: []*sharedtypes.ApplicationServiceConfig{
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc1"}},
+					{ServiceId: &sharedtypes.ServiceId{Id: "svc2"}},
+				},
+			},
 		},
 	}
 
