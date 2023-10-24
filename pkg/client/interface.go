01278b03f8d6214861c57f2e98bf15ba36402879
Bryan White <bryanchriswhite@gmail.com>
2023-10-24 13:58:05 +0200

    chore: improve godoc comments

--- pkg/client/interface.go
+++ pkg/client/interface.go
@@ -20,28 +20,39 @@ import (
 
 // EventsQueryClient is used to subscribe to chain event messages matching the given query,
 type EventsQueryClient interface {
+	// EventsBytes returns an observable which is notified about chain event messages
+	// matching the given query. It receives an either value which contains either an
+	// error or the event message bytes.
 	EventsBytes(
 		ctx context.Context,
 		query string,
 	) (EventsBytesObservable, error)
-	//EventsBytes(
-	//	ctx context.Context,
-	//	query string,
-	//) (observable.Observable[either.Either[[]byte]], error)
+	// Close unsubscribes all observers of each active query's events bytes
+	// observable and closes the connection.
 	Close()
 }
 
+// EventsBytesObservable is an observable which is notified with an either
+// value which contains either an error or the event message bytes.
+// TODO_HACK: The purpose of this type is to work around gomock's lack of
+// support for generic types. For the same reason, this type cannot be an
+// alias (i.e. EventsBytesObservable = observable.Observable[either.Either[[]byte]]).
 type EventsBytesObservable observable.Observable[either.Either[[]byte]]
 
 // Connection is a transport agnostic, bi-directional, message-passing interface.
 type Connection interface {
+	// Receive blocks until a message is received or an error occurs.
 	Receive() (msg []byte, err error)
+	// Send sends a message and may return a synchronous error.
 	Send(msg []byte) error
+	// Close closes the connection.
 	Close() error
 }
 
 // Dialer encapsulates the construction of connections.
 type Dialer interface {
+	// DialContext constructs a connection to the given URL and returns it or
+	// potentially a synchronous error.
 	DialContext(ctx context.Context, urlStr string) (Connection, error)
 }
 
