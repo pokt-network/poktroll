b706bab2e6df4361eb252a1880cbdb4b324cd4bb
Redouane Lakrache <r3d0ne@gmail.com>
2023-10-27 00:56:39 +0200

    chore: address change requests

--- pkg/relayer/session/interface.go
+++ pkg/relayer/session/interface.go
@@ -9,15 +9,19 @@ import (
 
 // RelayerSessions is an interface for managing the relayer's sessions and Sparse Merkle Sum Trees (SMSTs).
 // It provides notifications about closing sessions that are ready to be claimed, and handles the creation
-// and retrieval of SMSTs for a given session.
+// and retrieval of SMSTs for a given session. It can also be thought of as a SessionManager but dedicated
+// to the Relayer behavior.
 type RelayerSessions interface {
-	// ClosingSessions returns an observable that notifies about sessions ready to be claimed.
+	// ClosingSessions returns an observable that notifies of sessions ready to be claimed.
 	ClosingSessions() observable.Observable[SessionTree]
 
 	// EnsureSessionTree returns the SMST (Sparse Merkle State Tree) for a given session. It is used to retrieve
 	// the SMST and update it when a Relay has been successfully served. If the session is seen for the first time,
-	// it creates a new SMST for it before returning it. An error is returned if the corresponding KVStore for SMST fails to be created.
+	// it creates a new SMST for it before returning it.
+	// An error is returned if the corresponding KVStore for SMST fails to be created.
 	EnsureSessionTree(session *sessiontypes.Session) (SessionTree, error)
+
+	// TODO: Add `ExpiringSessions` to notify of session whose proof submission window is closing.
 }
 
 // SessionTree is an interface that wraps an SMST (Sparse Merkle State Tree) and its corresponding session.
@@ -27,18 +31,17 @@ type SessionTree interface {
 
 	// Update is a wrapper for the SMST's Update function. It updates the SMST with the given key, value, and weight.
 	// This function should be called when a Relay has been successfully served.
-	Update(key []byte, value []byte, weight uint64) error
+	Update(key, value []byte, weight uint64) error
 
 	// ProveClosest is a wrapper for the SMST's ProveClosest function. It returns the proof for the given path.
 	// This function should be called when a session has been claimed and needs to be proven.
 	ProveClosest(path []byte) (proof *smt.SparseMerkleClosestProof, err error)
 
-	// TODO_DISCUSS: This function should not be part of the interface as it is an optimization aiming to free up
-	// resources while waiting for the proof submission window to open.
-	// Close gets the root hash of the SMST needed for submitting the claim; then commits the entire tree and stops the KVStore.
-	// It should be called before submitting the claim on-chain. This function frees up the resources used by the SMST
-	// that are no longer needed while waiting for the proof submission window to open.
-	Close() (SMSTRoot []byte, err error)
+	// Close gets the root hash of the SMST needed for submitting the claim; then commits the entire tree to disk
+	// and stops the KVStore.
+	// It should be called before submitting the claim on-chain. This function frees up the in-memory resources
+	// used by the SMST that are no longer needed while waiting for the proof submission window to open.
+	Flush() (SMSTRoot []byte, err error)
 
 	// TODO_DISCUSS: This function should not be part of the interface as it is an optimization aiming to free up
 	// KVStore resources after the proof is no longer needed.
