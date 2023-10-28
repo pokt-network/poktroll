f01ba5f18c4045e74712311b875a7919257956a4
harry <53987565+h5law@users.noreply.github.com>
2023-10-28 15:03:32 +0100

    feat: add app and gateway address fields to the undelegate message

--- x/application/client/cli/tx_undelegate_from_gateway.go
+++ x/application/client/cli/tx_undelegate_from_gateway.go
@@ -3,22 +3,30 @@ package cli
 import (
 	"strconv"
 
+	"pocket/x/application/types"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cobra"
-	"pocket/x/application/types"
 )
 
 var _ = strconv.Itoa(0)
 
 func CmdUndelegateFromGateway() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "undelegate-from-gateway",
-		Short: "Broadcast message undelegate-from-gateway",
-		Args:  cobra.ExactArgs(0),
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
+		Use:   "undelegate-from-gateway [gateway address]",
+		Short: "Undelegate an application from a gateway",
+		Long: `Undelegate an application from the gateway whose address is provided. This is a broadcast operation
+that will remove the gateway from those which the application
+has delegated its trust to. This gateway will no longer have
+the ability to sign relays from the application.
 
+Example:
+$ pocketd --home=$(POCKETD_HOME) tx application undelegate-from-gateway $(GATEWAY_ADDR) --keyring-backend test --from $(APP) --node $(POCKET_NODE)`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			gatewayAddress := args[0]
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
@@ -26,10 +34,12 @@ func CmdUndelegateFromGateway() *cobra.Command {
 
 			msg := types.NewMsgUndelegateFromGateway(
 				clientCtx.GetFromAddress().String(),
+				gatewayAddress,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
+
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
