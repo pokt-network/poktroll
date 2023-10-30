910992a26befba55106ba90e1ca7ce1474f54380
Bryan White <bryanchriswhite@gmail.com>
2023-10-31 00:18:45 +0100

    refactor: add `either.Bytes` alias (#117)

--- pkg/either/types.go
+++ pkg/either/types.go
@@ -3,4 +3,7 @@ package either
 // AsyncError represents a value which could either be a synchronous error or
 // an asynchronous error (sent through a channel). It wraps the more generic
 // `Either` type specific for error channels.
-type AsyncError Either[chan error]
+type (
+	AsyncError Either[chan error]
+	Bytes      = Either[[]byte]
+)
