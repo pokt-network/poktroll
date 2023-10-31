e6ca636dd347e06adc04f9659f0e8c165858fc82
harry <53987565+h5law@users.noreply.github.com>
2023-10-31 19:35:17 +0000

    feat: add undelegation from unstaked gateway test

--- x/application/keeper/msg_server_undelegate_from_gateway_test.go
+++ x/application/keeper/msg_server_undelegate_from_gateway_test.go
@@ -156,3 +156,65 @@ func TestMsgServer_UndelegateFromGateway_FailNotDelegated(t *testing.T) {
 	require.Equal(t, 1, len(foundApp.DelegateeGatewayAddresses))
 	require.Equal(t, gatewayAddr2, foundApp.DelegateeGatewayAddresses[0])
 }
+
+func TestMsgServer_UndelegateFromGateway_SuccessfullyUndelegateFromUnstakedGateway(t *testing.T) {
+	k, ctx := keepertest.ApplicationKeeper(t)
+	srv := keeper.NewMsgServerImpl(*k)
+	wctx := sdk.WrapSDKContext(ctx)
+
+	// Generate an address for the application and gateways
+	appAddr := sample.AccAddress()
+	gatewayAddr := sample.AccAddress()
+	// Mock the gateway being staked via the staked gateway map
+	keepertest.StakedGatewayMap[gatewayAddr] = struct{}{}
+
+	// Prepare the application
+	stakeMsg := &types.MsgStakeApplication{
+		Address: appAddr,
+		Stake:   &sdk.Coin{Denom: "upokt", Amount: sdk.NewInt(100)},
+		Services: []*sharedtypes.ApplicationServiceConfig{
+			{
+				ServiceId: &sharedtypes.ServiceId{Id: "svc1"},
+			},
+		},
+	}
+
+	// Stake the application & verify that the application exists
+	_, err := srv.StakeApplication(wctx, stakeMsg)
+	require.NoError(t, err)
+	_, isAppFound := k.GetApplication(ctx, appAddr)
+	require.True(t, isAppFound)
+
+	// Prepare the delegation message and delegate the application to the gateway
+	delegateMsg := &types.MsgDelegateToGateway{
+		AppAddress:     appAddr,
+		GatewayAddress: gatewayAddr,
+	}
+	// Delegate the application to the gateway
+	_, err = srv.DelegateToGateway(wctx, delegateMsg)
+	require.NoError(t, err)
+
+	// Verify that the application exists
+	foundApp, isAppFound := k.GetApplication(ctx, appAddr)
+	require.True(t, isAppFound)
+	require.Equal(t, appAddr, foundApp.Address)
+	require.Equal(t, 1, len(foundApp.DelegateeGatewayAddresses))
+	require.Equal(t, gatewayAddr, foundApp.DelegateeGatewayAddresses[0])
+
+	// Mock unstaking the gateway
+	delete(keepertest.StakedGatewayMap, gatewayAddr)
+
+	// Prepare an undelegation message
+	undelegateMsg := &types.MsgUndelegateFromGateway{
+		AppAddress:     appAddr,
+		GatewayAddress: gatewayAddr,
+	}
+
+	// Undelegate the application from the gateway
+	_, err = srv.UndelegateFromGateway(wctx, undelegateMsg)
+	require.NoError(t, err)
+	foundApp, isAppFound = k.GetApplication(ctx, appAddr)
+	require.True(t, isAppFound)
+	require.Equal(t, appAddr, foundApp.Address)
+	require.Equal(t, 0, len(foundApp.DelegateeGatewayAddresses))
+}
