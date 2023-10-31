d5251ea527b4f55afcb14f6925f502b3da8a584d
harry <53987565+h5law@users.noreply.github.com>
2023-10-31 18:12:59 +0000

    feat: replace pubkeys with addresses

--- x/application/keeper/msg_server_delegate_to_gateway.go
+++ x/application/keeper/msg_server_delegate_to_gateway.go
@@ -6,8 +6,6 @@ import (
 	"pocket/x/application/types"
 
 	sdkerrors "cosmossdk.io/errors"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -37,35 +35,15 @@ func (k msgServer) DelegateToGateway(goCtx context.Context, msg *types.MsgDelega
 	}
 
 	// Check if the application is already delegated to the gateway
-	for _, gatewayPubKey := range app.DelegateeGatewayPubKeys {
-		// Convert the any type to a public key
-		gatewayPubKey, err := types.AnyToPubKey(gatewayPubKey)
-		if err != nil {
-			logger.Error("unable to convert any type to public key: %v", err)
-			return nil, sdkerrors.Wrapf(types.ErrAppAnyConversion, "unable to convert any type to public key: %v", err)
-		}
-		// Convert the public key to an address
-		gatewayAddress := types.PublicKeyToAddress(gatewayPubKey)
-		if gatewayAddress == msg.GatewayAddress {
-			logger.Info("Application already delegated to gateway with address: %s", msg.GatewayAddress)
+	for _, gatewayAddr := range app.DelegateeGatewayAddresses {
+		if gatewayAddr == msg.GatewayAddress {
+			logger.Info("Application already delegated to gateway with address [%s]", msg.GatewayAddress)
 			return nil, sdkerrors.Wrapf(types.ErrAppAlreadyDelegated, "application already delegated to gateway with address: %s", msg.GatewayAddress)
 		}
 	}
 
-	// Retrieve the public key of the gateway
-	pubKey, err := k.addressToPubKey(ctx, msg.GatewayAddress)
-	if err != nil {
-		logger.Error("unable to get public key from address [%s]: %v", msg.GatewayAddress, err)
-		return nil, sdkerrors.Wrapf(types.ErrAppInvalidGatewayAddress, "unable to get public key from address %s", msg.GatewayAddress)
-	}
-	anyPubKey, err := codectypes.NewAnyWithValue(pubKey)
-	if err != nil {
-		logger.Error("unable to create any type from public key: %v", err)
-		return nil, sdkerrors.Wrapf(types.ErrAppAnyConversion, "unable to create any type from public key: %v", err)
-	}
-
 	// Update the application with the new delegatee public key
-	app.DelegateeGatewayPubKeys = append(app.DelegateeGatewayPubKeys, *anyPubKey)
+	app.DelegateeGatewayAddresses = append(app.DelegateeGatewayAddresses, msg.GatewayAddress)
 	logger.Info("Successfully added delegatee public key to application")
 
 	// Update the application store with the new delegation
@@ -73,14 +51,4 @@ func (k msgServer) DelegateToGateway(goCtx context.Context, msg *types.MsgDelega
 	logger.Info("Successfully delegated application to gateway for app: %+v", app)
 
 	return &types.MsgDelegateToGatewayResponse{}, nil
-}
-
-func (k msgServer) addressToPubKey(ctx sdk.Context, address string) (cryptotypes.PubKey, error) {
-	// Retrieve the address of the address
-	accAddr, err := sdk.AccAddressFromBech32(address)
-	if err != nil {
-		return nil, err
-	}
-	// Return the public key of the address
-	return k.accountKeeper.GetPubKey(ctx, accAddr)
 }
