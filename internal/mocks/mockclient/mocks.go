622a47d1138d7ea8f8762c04ba0badfef1f20db2
Bryan White <bryanchriswhite@gmail.com>
2023-11-02 11:34:55 +0100

    fix: stub mock pkgs for CI

---
+++ internal/mocks/mockclient/mocks.go
@@ -0,0 +1,5 @@
+package mockclient
+
+// This file establishes the mockclient package such that `go mod tidy` will
+// not fail while traversing importers of this package. This is necessary because
+// `ignite generate proto-go` runs `go mod tidy` before generating protobufs.
