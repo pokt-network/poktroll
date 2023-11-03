f5e9737078ebeb6364e39324cdc591111141fc18
Bryan White <bryanchriswhite@gmail.com>
2023-11-03 08:27:56 +0100

    docs: fix client pkg godoc comment

--- pkg/client/interface.go
+++ pkg/client/interface.go
@@ -5,18 +5,6 @@
 //go:generate mockgen -destination=../../internal/mocks/mockclient/cosmos_keyring_mock.go -package=mockclient github.com/cosmos/cosmos-sdk/crypto/keyring Keyring
 //go:generate mockgen -destination=../../internal/mocks/mockclient/cosmos_client_mock.go -package=mockclient github.com/cosmos/cosmos-sdk/client AccountRetriever
 
-// Package client defines interfaces and types that facilitate interactions
-// with blockchain functionalities, both transactional and observational. It is
-// built to provide an abstraction layer for sending, receiving, and querying
-// blockchain data, thereby offering a standardized way of integrating with
-// various blockchain platforms.
-//
-// The client package leverages external libraries like cosmos-sdk and cometbft,
-// but there is a preference to minimize direct dependencies on these external
-// libraries, when defining interfaces, aiming for a cleaner decoupling.
-// It seeks to provide a flexible and comprehensive interface layer, adaptable to
-// different blockchain configurations and requirements.
-
 package client
 
 import (
