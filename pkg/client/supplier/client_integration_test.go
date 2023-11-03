5cdce1714475951ff79a756b28d1dcb98a7f4d0f
Bryan White <bryanchriswhite@gmail.com>
2023-11-03 13:25:43 +0100

    test: supplier client integration test

---
+++ pkg/client/supplier/client_integration_test.go
@@ -0,0 +1,36 @@
+//go:build integration
+
+package supplier_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"pocket/internal/testclient/testsupplier"
+	sessiontypes "pocket/x/session/types"
+)
+
+func TestNewSupplierClient_Localnet(t *testing.T) {
+	t.Skip("TODO_TECHDEBT: this test depends on some setup which is currently not implemented in this test: staked application and servicer with matching services")
+
+	var (
+		signingKeyName = "app1"
+		ctx            = context.Background()
+	)
+
+	supplierClient := testsupplier.NewLocalnetClient(t, signingKeyName)
+	require.NotNil(t, supplierClient)
+
+	var rootHash []byte
+	sessionHeader := sessiontypes.SessionHeader{
+		ApplicationAddress:      "",
+		SessionStartBlockHeight: 0,
+		SessionId:               "",
+	}
+	err := supplierClient.CreateClaim(ctx, sessionHeader, rootHash)
+	require.NoError(t, err)
+
+	require.True(t, false)
+}
