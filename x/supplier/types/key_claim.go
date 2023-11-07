1ee4487314bf26ef3efd825fef32fbdda14e175c
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-11-06 16:22:01 -0800

    make go_develop_and_test compiles again

--- x/supplier/types/key_claim.go
+++ x/supplier/types/key_claim.go
@@ -5,19 +5,19 @@ import "encoding/binary"
 var _ binary.ByteOrder
 
 const (
-    // ClaimKeyPrefix is the prefix to retrieve all Claim
+	// ClaimKeyPrefix is the prefix to retrieve all Claim
 	ClaimKeyPrefix = "Claim/value/"
 )
 
 // ClaimKey returns the store key to retrieve a Claim from the index fields
 func ClaimKey(
-index string,
+	index string,
 ) []byte {
 	var key []byte
-    
-    indexBytes := []byte(index)
-    key = append(key, indexBytes...)
-    key = append(key, []byte("/")...)
-    
+
+	indexBytes := []byte(index)
+	key = append(key, indexBytes...)
+	key = append(key, []byte("/")...)
+
 	return key
 }
