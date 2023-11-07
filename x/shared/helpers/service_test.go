dc8a37d55da2571d99194533a17ca82f8b3944fa
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-11-07 11:50:08 -0800

    Update x/shared/helpers/service_test.go
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

--- x/shared/helpers/service_test.go
+++ x/shared/helpers/service_test.go
@@ -97,9 +97,7 @@ func TestIsValidService(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := IsValidService(&tt.input)
-			if got != tt.expected {
-				t.Errorf("IsValidService(%v) = %v, want %v", tt.input, got, tt.expected)
-			}
+			require.Equalf(t, tt.expected, got, "IsValidService(%v)",  tt.input)
 		})
 	}
 }
