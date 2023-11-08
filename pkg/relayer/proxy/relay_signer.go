67273dd8a45955e1ba8693ebc840b182633fb263
Redouane Lakrache <r3d0ne@gmail.com>
2023-11-08 19:39:32 +0100

    chore: comment about current proxy signing approach

--- pkg/relayer/proxy/relay_signer.go
+++ pkg/relayer/proxy/relay_signer.go
@@ -8,6 +8,9 @@ import (
 
 // SignRelayResponse is a shared method used by the RelayServers to sign the hash of the RelayResponse..
 // It uses the keyring and keyName to sign the payload and returns the signature.
+// TODO_TECHDEBT(@red-0ne): This method should be moved out of the RelayerProxy interface
+// that should not be responsible for signing relay responses.
+// See https://github.com/pokt-network/poktroll/issues/160 for a better design.
 func (rp *relayerProxy) SignRelayResponse(relayResponse *types.RelayResponse) (*types.RelayResponse, error) {
 	var responseBz []byte
 	_, err := relayResponse.MarshalTo(responseBz)
