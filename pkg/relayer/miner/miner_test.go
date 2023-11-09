38329bb0608b3718415035613812fc14ee243d37
Bryan White <bryanchriswhite@gmail.com>
2023-11-09 14:33:59 +0100

    test: `Miner` implementation

---
+++ pkg/relayer/miner/miner_test.go
@@ -0,0 +1,17 @@
+package miner_test
+
+import (
+	"testing"
+)
+
+func TestNewMiner(t *testing.T) {
+	//tests := []struct {
+	//	desc string
+	//	deps depinject.Config
+	//	opts []relayer.MinerOption
+	//} {
+	//	{
+	//		desc: ""
+	//	},
+	//}
+}
