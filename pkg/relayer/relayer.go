cba145e67b49bae2089b0ea334f0666d28d544e5
Redouane Lakrache <r3d0ne@gmail.com>
2023-11-09 22:16:30 +0100

    chore: Rename to RelayMiner

--- pkg/relayer/relayer.go
+++ pkg/relayer/relayer.go
@@ -6,22 +6,22 @@ import (
 	"cosmossdk.io/depinject"
 )
 
-type RelayerOption func(*Relayer)
+type RelayerOption func(*relayMiner)
 
-// Relayer is the main struct that encapsulates the relayer's responsibilities.
+// relayMiner is the main struct that encapsulates the relayer's responsibilities (i.e. Relay Mining).
 // It starts and stops the RelayerProxy and provide the served relays observable to them miner.
-type Relayer struct {
+type relayMiner struct {
 	relayerProxy RelayerProxy
 	miner        Miner
 }
 
-// NewRelayer creates a new Relayer instance with the given dependencies.
+// NewRelayMiner creates a new Relayer instance with the given dependencies.
 // It injects the dependencies into the Relayer instance and returns it.
-func NewRelayer(
+func NewRelayMiner(
 	deps depinject.Config,
 	opts ...RelayerOption,
-) (*Relayer, error) {
-	rel := &Relayer{}
+) (*relayMiner, error) {
+	rel := &relayMiner{}
 
 	if err := depinject.Inject(deps, &rel.relayerProxy, &rel.miner); err != nil {
 		return nil, err
@@ -37,13 +37,14 @@ func NewRelayer(
 // Start provides the miner with the served relays observable and starts the relayer proxy.
 // This method is blocking while the relayer proxy is running and returns when Stop is called
 // or when the relayer proxy fails to start.
-func (rel *Relayer) Start(ctx context.Context) error {
-	rel.miner.MineRelays(ctx, rel.relayerProxy.ServedRelays())
+func (rel *relayMiner) Start(ctx context.Context) error {
+	// MineRelays does not block and subscribes to the served relays observable.
+	rel.miner.StartMiningRelays(ctx, rel.relayerProxy.ServedRelays())
 	return rel.relayerProxy.Start(ctx)
 }
 
 // Stop stops the relayer proxy which in turn stops all advertised relay servers
 // and unsubscribes the miner from the served relays observable.
-func (rel *Relayer) Stop(ctx context.Context) error {
+func (rel *relayMiner) Stop(ctx context.Context) error {
 	return rel.relayerProxy.Stop(ctx)
 }
