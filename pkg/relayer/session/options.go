f76f05c3d66c5453e3f26a0484188ab492ad1f73
Redouane Lakrache <r3d0ne@gmail.com>
2023-11-09 23:11:30 +0100

    chore: Add WithStoresDirectory function to set the path for KVStore data files.

--- pkg/relayer/session/options.go
+++ pkg/relayer/session/options.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pokt-network/poktroll/pkg/relayer"
 )
 
+// WithStoresDirectory sets the path on disk where KVStore data files are created.
 func WithStoresDirectory(storesDirectory string) relayer.RelayerSessionsManagerOption {
 	return func(relSessionMgr relayer.RelayerSessionsManager) {
 		relSessionMgr.(*relayerSessionsManager).storesDirectory = storesDirectory
