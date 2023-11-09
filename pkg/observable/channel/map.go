e3fc8ea22223cafb6ae80ce2e1c330bfcb7ab1f6
Bryan White <bryanchriswhite@gmail.com>
2023-11-09 20:09:58 +0100

    chore: review feedback improvement

--- pkg/observable/channel/map.go
+++ pkg/observable/channel/map.go
@@ -20,7 +20,7 @@ func Map[S, D any](
 	dstObservable, dstProducer := NewObservable[D]()
 	srcObserver := srcObservable.Subscribe(ctx)
 
-	mapInternal[S, D](
+	go goMapTransformNotification(
 		ctx,
 		srcObserver,
 		transformFn,
@@ -30,25 +30,6 @@ func Map[S, D any](
 	)
 
 	return dstObservable
-}
-
-func mapInternal[S, D any](
-	ctx context.Context,
-	srcObserver observable.Observer[S],
-	transformFn MapFn[S, D],
-	publishFn func(dstNotifications D),
-) {
-	go func() {
-		for srcNotification := range srcObserver.Ch() {
-			dstNotifications, skip := transformFn(ctx, srcNotification)
-			if skip {
-				continue
-			}
-
-			publishFn(dstNotifications)
-		}
-	}()
-
 }
 
 // MapReplay transforms the given observable by applying the given transformFn to
@@ -61,22 +42,38 @@ func MapReplay[S, D any](
 	ctx context.Context,
 	replayBufferSize int,
 	srcObservable observable.Observable[S],
-	// TODO_CONSIDERATION: if this were variadic, it could simplify serial transformations.
-	transformFn func(src S) (dst D, skip bool),
+	transformFn MapFn[S, D],
 ) observable.ReplayObservable[D] {
 	dstObservable, dstProducer := NewReplayObservable[D](ctx, replayBufferSize)
 	srcObserver := srcObservable.Subscribe(ctx)
 
-	go func() {
-		for srcNotification := range srcObserver.Ch() {
-			dstNotification, skip := transformFn(srcNotification)
-			if skip {
-				continue
-			}
-
+	go goMapTransformNotification(
+		ctx,
+		srcObserver,
+		transformFn,
+		func(dstNotification D) {
 			dstProducer <- dstNotification
-		}
-	}()
+		},
+	)
 
 	return dstObservable
+}
+
+// goMapTransformNotification transforms, optionally skips, and publishes
+// notifications via the given publishFn.
+func goMapTransformNotification[S, D any](
+	ctx context.Context,
+	srcObserver observable.Observer[S],
+	transformFn MapFn[S, D],
+	publishFn func(dstNotifications D),
+) {
+	for srcNotification := range srcObserver.Ch() {
+		dstNotifications, skip := transformFn(ctx, srcNotification)
+		if skip {
+			continue
+		}
+
+		publishFn(dstNotifications)
+	}
+
 }
