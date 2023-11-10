059b7dffe94884fbeec864463fadf12b1b2f165d
Redouane Lakrache <r3d0ne@gmail.com>
2023-11-10 22:26:10 +0100

    chore: Reflect responsibility changes of session manager

--- pkg/relayer/relayminer.go
+++ pkg/relayer/relayminer.go
@@ -2,6 +2,7 @@ package relayer
 
 import (
 	"context"
+	"log"
 
 	"cosmossdk.io/depinject"
 )
@@ -9,22 +10,29 @@ import (
 // relayMiner is the main struct that encapsulates the relayer's responsibilities (i.e. Relay Mining).
 // It starts and stops the RelayerProxy and provide the served relays observable to them miner.
 type relayMiner struct {
-	relayerProxy RelayerProxy
-	miner        Miner
+	relayerProxy           RelayerProxy
+	miner                  Miner
+	relayerSessionsManager RelayerSessionsManager
 }
 
 // NewRelayMiner creates a new Relayer instance with the given dependencies.
 // It injects the dependencies into the Relayer instance and returns it.
-func NewRelayMiner(deps depinject.Config) (*relayMiner, error) {
+func NewRelayMiner(ctx context.Context, deps depinject.Config) (*relayMiner, error) {
 	rel := &relayMiner{}
 
 	if err := depinject.Inject(
 		deps,
 		&rel.relayerProxy,
 		&rel.miner,
+		&rel.relayerSessionsManager,
 	); err != nil {
 		return nil, err
 	}
+
+	// Set up relay pipeline
+	servedRelaysObs := rel.relayerProxy.ServedRelays()
+	minedRelaysObs := rel.miner.MinedRelays(ctx, servedRelaysObs)
+	rel.relayerSessionsManager.InsertRelays(minedRelaysObs)
 
 	return rel, nil
 }
@@ -33,9 +41,20 @@ func NewRelayMiner(deps depinject.Config) (*relayMiner, error) {
 // This method is blocking while the relayer proxy is running and returns when Stop is called
 // or when the relayer proxy fails to start.
 func (rel *relayMiner) Start(ctx context.Context) error {
-	// StartMiningRelays does not block, it only subscribes to the served relays observable.
-	rel.miner.StartMiningRelays(ctx, rel.relayerProxy.ServedRelays())
-	return rel.relayerProxy.Start(ctx)
+	// relayerSessionsManager.Start does not block.
+	// Set up the session (proof/claim) lifecycle pipeline.
+	log.Println("INFO: Starting relayer sessions manager...")
+	rel.relayerSessionsManager.Start(ctx)
+
+	// Start the flow of relays by starting relayer proxy.
+	// This is a blocking call as it waits for the waitgroup to be done.
+	log.Println("INFO: Starting relayer proxy...")
+	if err := rel.relayerProxy.Start(ctx); err != nil {
+		return err
+	}
+
+	log.Println("INFO: Relayer proxy stopped; exiting")
+	return nil
 }
 
 // Stop stops the relayer proxy which in turn stops all advertised relay servers
