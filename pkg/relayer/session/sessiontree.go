9b836919617b6f0cc760278fda4d0447105c1987
Bryan White <bryanchriswhite@gmail.com>
2023-11-15 12:52:26 +0100

    fix: sessiontree store path check

--- pkg/relayer/session/sessiontree.go
+++ pkg/relayer/session/sessiontree.go
@@ -68,12 +68,8 @@ func NewSessionTree(
 	storePath := filepath.Join(storesDirectory, sessionHeader.SessionId)
 
 	// Make sure storePath does not exist when creating a new SessionTree
-	if _, err := os.Stat(storePath); err != nil {
-		if !os.IsNotExist(err) {
-			return nil, err
-		}
-
-		return nil, ErrSessionTreeStorePathExists
+	if _, err := os.Stat(storePath); err != nil && !os.IsNotExist(err) {
+		return nil, ErrSessionTreeStorePathExists.Wrapf("storePath: %q", storePath)
 	}
 
 	treeStore, err := smt.NewKVStore(storePath)
