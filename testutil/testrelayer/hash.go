4f35ebcc86a5c5ae3939ba0bc618f1d3d2591c27
Bryan White <bryanchriswhite@gmail.com>
2023-11-21 10:57:53 +0100

    chore: add TODO comment

--- testutil/testrelayer/hash.go
+++ testutil/testrelayer/hash.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TODO_TECHDEBT(@h5law): Retrieve the relay hasher mechanism from the `smt` repo.
 func HashBytes(t *testing.T, newHasher func() hash.Hash, relayBz []byte) []byte {
 	hasher := newHasher()
 	_, err := hasher.Write(relayBz)
