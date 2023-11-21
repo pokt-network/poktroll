7758611a53bc258ea6d96e15ea0b0627b649591d
Bryan White <bryanchriswhite@gmail.com>
2023-11-21 10:55:02 +0100

    revert: `RelayRequest` pointer receivers

--- x/service/types/relay.go
+++ x/service/types/relay.go
@@ -3,7 +3,7 @@ package types
 // GetSignableBytes returns the signable bytes for the relay request
 // this involves setting the signature to nil and marshaling the message.
 // A value receiver is used to avoid overwriting any pre-existing signature
-func (req *RelayRequest) GetSignableBytes() ([]byte, error) {
+func (req RelayRequest) GetSignableBytes() ([]byte, error) {
 	// set signature to nil
 	req.Meta.Signature = nil
 
@@ -14,7 +14,7 @@ func (req *RelayRequest) GetSignableBytes() ([]byte, error) {
 // GetSignableBytes returns the signable bytes for the relay response
 // this involves setting the signature to nil and marshaling the message.
 // A value receiver is used to avoid overwriting any pre-existing signature
-func (res *RelayResponse) GetSignableBytes() ([]byte, error) {
+func (res RelayResponse) GetSignableBytes() ([]byte, error) {
 	// set signature to nil
 	res.Meta.SupplierSignature = nil
 
