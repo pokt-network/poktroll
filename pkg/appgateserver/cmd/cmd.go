93048e07a9a015bcbe6b8413c096cc83954c378c
Redouane Lakrache <r3d0ne@gmail.com>
2023-11-21 14:26:58 +0100

    Revert "feat: Add app gate config"
    This reverts commit 9221c1447b6d969dbfe230c942bd59523a0c76a9.

--- pkg/appgateserver/cmd/cmd.go
+++ pkg/appgateserver/cmd/cmd.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
 
 	"cosmossdk.io/depinject"
 	cosmosclient "github.com/cosmos/cosmos-sdk/client"
@@ -15,7 +15,6 @@ import (
 
 	"github.com/pokt-network/poktroll/cmd/signals"
 	"github.com/pokt-network/poktroll/pkg/appgateserver"
-	appgateconfig "github.com/pokt-network/poktroll/pkg/appgateserver/config"
 	"github.com/pokt-network/poktroll/pkg/deps/config"
 )
 
@@ -23,7 +22,10 @@ import (
 const omittedDefaultFlagValue = "explicitly omitting default"
 
 var (
-	flagAppGateConfig string
+	flagSigningKey        string
+	flagSelfSigning       bool
+	flagListeningEndpoint string
+	flagQueryNodeUrl      string
 )
 
 func AppGateServerCmd() *cobra.Command {
@@ -34,31 +36,32 @@ func AppGateServerCmd() *cobra.Command {
 the necessary on-chain interactions (sessions, suppliers, etc) to receive the
 respective relay response.
 
--- App Mode--
-If the server is started with a defined 'self-signing' configuration directive,
-it will behave as an Application. Any incoming requests will be signed by using
-the private key and ring associated with the 'signing_key' directive.
+-- App Mode (Flag)- -
+If the server is started with a defined '--self-signing' flag, it will behave
+as an Application. Any incoming requests will be signed by using the private
+key and ring associated with the '--signing-key' flag.
 
--- Gateway Mode --
-If the 'self_signing' configuration directive is not provided, the server will
-behave as a Gateway.
+-- Gateway Mode (Flag)--
+If the '--self-signing' flag is not provided, the server will behave as a Gateway.
 It will sign relays on behalf of any Application sending it relays, provided
-that the address associated with 'signing_key' has been delegated to. This is
+that the address associated with '--signing-key' has been delegated to. This is
 necessary for the application<->gateway ring signature to function.
 
 -- App Mode (HTTP) --
-If an application doesn't provide the 'self_signing' configuration directive,
-it can still send relays to the AppGate server and function as an Application,
-provided that:
+If an application doesn't provide the '--self-signing' flag, it can still send
+relays to the AppGate server and function as an Application, provided that:
 1. Each request contains the '?senderAddress=[address]' query parameter
-2. The key associated with the 'signing_key' configuration directive belongs
-   to the address provided in the request, otherwise the ring signature will not be valid.`,
+2. The key associated with the '--signing-key' flag belongs to the address
+   provided in the request, otherwise the ring signature will not be valid.`,
 		Args: cobra.NoArgs,
 		RunE: runAppGateServer,
 	}
 
 	// Custom flags
-	cmd.Flags().StringVar(&flagAppGateConfig, "config", "", "The path to the appgate config file")
+	cmd.Flags().StringVar(&flagSigningKey, "signing-key", "", "The name of the key that will be used to sign relays")
+	cmd.Flags().StringVar(&flagListeningEndpoint, "listening-endpoint", "http://localhost:42069", "The host and port that the appgate server will listen on")
+	cmd.Flags().BoolVar(&flagSelfSigning, "self-signing", false, "Whether the server should sign all incoming requests with its own ring (for applications)")
+	cmd.Flags().StringVar(&flagQueryNodeUrl, "query-node", omittedDefaultFlagValue, "tcp://<host>:<port> to a full pocket node for reading data and listening for on-chain events")
 
 	// Cosmos flags
 	cmd.Flags().String(cosmosflags.FlagKeyringBackend, "", "Select keyring's backend (os|file|kwallet|pass|test)")
@@ -75,18 +78,14 @@ func runAppGateServer(cmd *cobra.Command, _ []string) error {
 	// Handle interrupt and kill signals asynchronously.
 	signals.GoOnExitSignal(cancelCtx)
 
-	configContent, err := os.ReadFile(flagAppGateConfig)
+	// Parse the listening endpoint.
+	listeningUrl, err := url.Parse(flagListeningEndpoint)
 	if err != nil {
-		return err
-	}
-
-	appGateConfigs, err := appgateconfig.ParseAppGateConfigs(configContent)
-	if err != nil {
-		return err
+		return fmt.Errorf("failed to parse listening endpoint: %w", err)
 	}
 
 	// Setup the AppGate server dependencies.
-	appGateServerDeps, err := setupAppGateServerDependencies(ctx, cmd, appGateConfigs)
+	appGateServerDeps, err := setupAppGateServerDependencies(ctx, cmd)
 	if err != nil {
 		return fmt.Errorf("failed to setup AppGate server dependencies: %w", err)
 	}
@@ -98,18 +97,18 @@ func runAppGateServer(cmd *cobra.Command, _ []string) error {
 		appGateServerDeps,
 		appgateserver.WithSigningInformation(&appgateserver.SigningInformation{
 			// provide the name of the key to use for signing all incoming requests
-			SigningKeyName: appGateConfigs.SigningKey,
+			SigningKeyName: flagSigningKey,
 			// provide whether the appgate server should sign all incoming requests
 			// with its own ring (for applications) or not (for gateways)
-			SelfSigning: appGateConfigs.SelfSigning,
+			SelfSigning: flagSelfSigning,
 		}),
-		appgateserver.WithListeningUrl(appGateConfigs.ListeningEndpoint),
+		appgateserver.WithListeningUrl(listeningUrl),
 	)
 	if err != nil {
 		return fmt.Errorf("failed to create AppGate server: %w", err)
 	}
 
-	log.Printf("INFO: Starting AppGate server, listening on %s...", appGateConfigs.ListeningEndpoint.String())
+	log.Printf("INFO: Starting AppGate server, listening on %s...", listeningUrl.String())
 
 	// Start the AppGate server.
 	if err := appGateServer.Start(ctx); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -124,17 +123,34 @@ func runAppGateServer(cmd *cobra.Command, _ []string) error {
 func setupAppGateServerDependencies(
 	ctx context.Context,
 	cmd *cobra.Command,
-	appGateConfig *appgateconfig.AppGateConfig,
 ) (depinject.Config, error) {
-	pocketNodeWebsocketUrl := fmt.Sprintf("ws://%s/websocket", appGateConfig.QueryNodeUrl.Host)
+	pocketNodeWebsocketUrl, err := getPocketNodeWebsocketUrl()
+	if err != nil {
+		return nil, err
+	}
 
 	supplierFuncs := []config.SupplierFn{
 		config.NewSupplyEventsQueryClientFn(pocketNodeWebsocketUrl),
 		config.NewSupplyBlockClientFn(pocketNodeWebsocketUrl),
-		newSupplyQueryClientContextFn(appGateConfig.QueryNodeUrl.String()),
+		newSupplyQueryClientContextFn(flagQueryNodeUrl),
 	}
 
 	return config.SupplyConfig(ctx, cmd, supplierFuncs)
+}
+
+// getPocketNodeWebsocketUrl returns the websocket URL of the Pocket Node to
+// connect to for subscribing to on-chain events.
+func getPocketNodeWebsocketUrl() (string, error) {
+	if flagQueryNodeUrl == omittedDefaultFlagValue {
+		return "", errors.New("missing required flag: --query-node")
+	}
+
+	pocketNodeURL, err := url.Parse(flagQueryNodeUrl)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("ws://%s/websocket", pocketNodeURL.Host), nil
 }
 
 // newSupplyQueryClientContextFn returns a new depinject.Config which is supplied with
