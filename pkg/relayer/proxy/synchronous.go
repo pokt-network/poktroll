464da6237482e3a911c01f5f7adf04c7e245d43e
harry <53987565+h5law@users.noreply.github.com>
2023-11-22 15:51:30 +0000

    chore: s/symmetric/synchronous/

--- pkg/relayer/proxy/synchronous.go
+++ pkg/relayer/proxy/synchronous.go
@@ -88,7 +88,7 @@ func (sync *synchronousRPCServer) Service() *sharedtypes.Service {
 func (sync *synchronousRPCServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	ctx := request.Context()
 
-	log.Printf("DEBUG: Serving symmetric relay request...")
+	log.Printf("DEBUG: Serving synchronous relay request...")
 
 	// Extract the relay request from the request body.
 	log.Printf("DEBUG: Extracting relay request from request body...")
