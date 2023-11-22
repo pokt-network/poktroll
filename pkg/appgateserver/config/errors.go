be1b650da88251a7d5e9c5bd0056f4fd665521b9
Redouane Lakrache <r3d0ne@gmail.com>
2023-11-22 08:15:04 +0100

    chore: Add missing change request

--- pkg/appgateserver/config/errors.go
+++ pkg/appgateserver/config/errors.go
@@ -7,5 +7,5 @@ var (
 	ErrAppGateConfigUnmarshalYAML            = sdkerrors.Register(codespace, 1, "config reader cannot unmarshal yaml content")
 	ErrAppGateConfigEmptySigningKey          = sdkerrors.Register(codespace, 2, "empty signing key in AppGateServer config")
 	ErrAppGateConfigInvalidListeningEndpoint = sdkerrors.Register(codespace, 3, "invalid listening endpoint in AppGateServer config")
-	ErrAppGateConfigInvalidQueryNodeUrl      = sdkerrors.Register(codespace, 4, "invalid query node url in AppGateServer config")
+	ErrAppGateConfigInvalidQueryNodeUrl      = sdkerrors.Register(codespace, 4, "invalid pocket query node url in AppGateServer config")
 )
