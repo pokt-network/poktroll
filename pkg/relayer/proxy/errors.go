7525358728ae03184af751f34fb1b8b37b3bbeb1
harry <53987565+h5law@users.noreply.github.com>
2023-11-22 21:48:47 +0000

    chore: increase golines max length

--- pkg/relayer/proxy/errors.go
+++ pkg/relayer/proxy/errors.go
@@ -5,50 +5,14 @@ import (
 )
 
 var (
-	codespace                         = "relayer_proxy"
-	ErrRelayerProxyUnsupportedRPCType = sdkerrors.Register(
-		codespace,
-		1,
-		"unsupported relayer proxy rpc type",
-	)
-	ErrRelayerProxyInvalidRelayRequestSignature = sdkerrors.Register(
-		codespace,
-		2,
-		"invalid relay request signature",
-	)
-	ErrRelayerProxyInvalidSession = sdkerrors.Register(
-		codespace,
-		3,
-		"invalid session in relayer request",
-	)
-	ErrRelayerProxyInvalidSupplier = sdkerrors.Register(
-		codespace,
-		4,
-		"invalid relayer proxy supplier",
-	)
-	ErrRelayerProxyUndefinedSigningKeyName = sdkerrors.Register(
-		codespace,
-		5,
-		"undefined relayer proxy signing key name",
-	)
-	ErrRelayerProxyUndefinedProxiedServicesEndpoints = sdkerrors.Register(
-		codespace,
-		6,
-		"undefined proxied services endpoints for relayer proxy",
-	)
-	ErrRelayerProxyInvalidRelayRequest = sdkerrors.Register(
-		codespace,
-		7,
-		"invalid relay request",
-	)
-	ErrRelayerProxyInvalidRelayResponse = sdkerrors.Register(
-		codespace,
-		8,
-		"invalid relay response",
-	)
-	ErrRelayerProxyEmptyRelayRequestSignature = sdkerrors.Register(
-		codespace,
-		9,
-		"empty relay response signature",
-	)
+	codespace                                        = "relayer_proxy"
+	ErrRelayerProxyUnsupportedRPCType                = sdkerrors.Register(codespace, 1, "unsupported relayer proxy rpc type")
+	ErrRelayerProxyInvalidRelayRequestSignature      = sdkerrors.Register(codespace, 2, "invalid relay request signature")
+	ErrRelayerProxyInvalidSession                    = sdkerrors.Register(codespace, 3, "invalid session in relayer request")
+	ErrRelayerProxyInvalidSupplier                   = sdkerrors.Register(codespace, 4, "invalid relayer proxy supplier")
+	ErrRelayerProxyUndefinedSigningKeyName           = sdkerrors.Register(codespace, 5, "undefined relayer proxy signing key name")
+	ErrRelayerProxyUndefinedProxiedServicesEndpoints = sdkerrors.Register(codespace, 6, "undefined proxied services endpoints for relayer proxy")
+	ErrRelayerProxyInvalidRelayRequest               = sdkerrors.Register(codespace, 7, "invalid relay request")
+	ErrRelayerProxyInvalidRelayResponse              = sdkerrors.Register(codespace, 8, "invalid relay response")
+	ErrRelayerProxyEmptyRelayRequestSignature        = sdkerrors.Register(codespace, 9, "empty relay response signature")
 )
