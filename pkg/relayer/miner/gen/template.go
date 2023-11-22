1bf298d32f07def19696024afbf611559118410b
Bryan White <bryanchriswhite@gmail.com>
2023-11-22 21:28:10 +0100

    chore: omitted improvments to #191

--- pkg/relayer/miner/gen/template.go
+++ pkg/relayer/miner/gen/template.go
@@ -24,7 +24,7 @@ var (
 
 	// marshaledUnminableRelaysHex are the hex encoded strings of serialized
 	// relayer.MinedRelays which have been pre-mined to **exclude** relays with
-	// difficulty 2 (or greater). Like marshaledMinableRelaysHex, this is done
+	// difficulty {{.difficultyBitsThreshold}} (or greater). Like marshaledMinableRelaysHex, this is done
 	// by populating the signature with random bytes. It is intended for use in
 	// tests.
 	marshaledUnminableRelaysHex = []string{
