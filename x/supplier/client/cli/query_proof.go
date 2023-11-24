df37f864e61fd36360799f7325724c7e94b4c2a4
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-11-24 14:16:59 -0800

    [Supplier] Scaffold the `Proof` type and adjust some of the defaults (#197)
    Scaffolding the `Proof` type + a few minor edits such as:
    - Removing proofs from the genesis state
    - Fixing the language around retrieving more than one proof
    - scaffold command:
    ```bash
    $ ignite scaffold map proof --module supplier supplier_address session_id merkle_proof --no-message --yes
    ```
    
    ---------
    
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    Co-authored-by: Redouane Lakrache <r3d0ne@gmail.com>

---
+++ x/supplier/client/cli/query_proof.go
@@ -0,0 +1,79 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+
+	"github.com/pokt-network/poktroll/x/supplier/types"
+)
+
+func CmdListProof() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-proofs",
+		Short: "list all proofs",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllProofsRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.AllProofs(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// TODO_UPNEXT(@Olshansk): Remove the dependency on index which was part of the default scaffolding behaviour
+func CmdShowProof() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-proof <index>",
+		Short: "shows a proof",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			argIndex := args[0]
+
+			params := &types.QueryGetProofRequest{
+				Index: argIndex,
+			}
+
+			res, err := queryClient.Proof(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
