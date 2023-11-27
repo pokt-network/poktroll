05fbe3a796bf6fc733f8d9ee7edc435ae41521ab
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-11-27 10:10:19 -0800

    Added a comment in go code

--- x/supplier/keeper/proof.go
+++ x/supplier/keeper/proof.go
@@ -3,7 +3,6 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/pokt-network/poktroll/x/supplier/types"
 )
 
@@ -41,6 +40,7 @@ func (k Keeper) RemoveProof(
 	index string,
 
 ) {
+	// OLSHANSKY CHANGING GO CODE
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.ProofKeyPrefix))
 	store.Delete(types.ProofKey(
 		index,
