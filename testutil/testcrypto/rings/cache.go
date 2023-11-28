3fc496882514dd0021e7a7c24a84de866069f3d8
harry <53987565+h5law@users.noreply.github.com>
2023-11-28 20:40:46 +0000

    feat: add real ring cache with mocked dependencies for use in integration tests

--- testutil/testcrypto/rings/cache.go
+++ testutil/testcrypto/rings/cache.go
@@ -1,3 +1,39 @@
 package testrings
 
-// TODO(@h5law): Add real/mocked RingCache implementations for use in integration tests
+import (
+	"testing"
+
+	"cosmossdk.io/depinject"
+	"github.com/stretchr/testify/require"
+
+	"github.com/pokt-network/poktroll/pkg/crypto/rings"
+	"github.com/pokt-network/poktroll/testutil/mockclient"
+)
+
+// NewRingCacheWithMockQueriers creates a new "real" RingCache with the given
+// mock Account and Application queriers supplied as dependencies.
+// The queriers are expected to maintain their respective mocked states:
+//   - Account querier: the account addresses and public keys
+//   - Application querier: the application addresses delegatee gateway addresses
+//
+// See:
+//
+//	testutil/testclient/testqueryclients/accquerier.go
+//	testutil/testclient/testqueryclients/appquerier.go
+//
+// for methods to create these queriers and maintain their states.
+func NewRingCacheWithMockQueriers(
+	t *testing.T,
+	accQuerier *mockclient.MockAccountQueryClient,
+	appQuerier *mockclient.MockApplicationQueryClient,
+) rings.RingCache {
+	t.Helper()
+
+	// Create the dependency injector with the mock queriers
+	deps := depinject.Supply(accQuerier, appQuerier)
+
+	ringCache, err := rings.NewRingCache(deps)
+	require.NoError(t, err)
+
+	return ringCache
+}
