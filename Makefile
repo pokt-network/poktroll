pokt-network/poktroll

commit de028c627fc280b19bac5c63c32facf9ce40698f

[Docs] Introduce Docusaurus documentation (#252)
Introduce the first iteration of dev.poktroll.com using docusarus while also adding a quickstart guide.

Done in this PR:
- [x] Introduce Docusaurus.io documentation using the `.md` files in this repo
- [x] Add a basic quickstart guide
- [x] Add a basic contribution guide
- [x] Move over existing documents
- [x] Add a few admonitions with warning signs & tips
- [x] GitHub action to automatically deploy it on merges to main

Not done in this PR:
- Align the architecture docs with the most up to date architecture
- Review all existing (moved over) docs from start to finish

Video reference: https://github.com/pokt-network/poktroll/assets/1892194/801a1859-4060-434e-b03e-

---------

Co-authored-by: DK <okdas@pm.me>
Co-authored-by: h5law <53987565+h5law@users.noreply.github.com>

Makefile
@@ -71,6 +71,26 @@ check_godoc:
 	fi; \
 	}
 
+.PHONY: check_npm
+# Internal helper target - check if npm is installed
+check_npm:
+	{ \
+	if ( ! ( command -v npm >/dev/null )); then \
+		echo "Seems like you don't have npm installed. Make sure you install it before continuing"; \
+		exit 1; \
+	fi; \
+	}
+
+.PHONY: check_node
+# Internal helper target - check if node is installed
+check_node:
+	{ \
+	if ( ! ( command -v node >/dev/null )); then \
+		echo "Seems like you don't have node installed. Make sure you install it before continuing"; \
+		exit 1; \
+	fi; \
+	}
+
 
 .PHONY: warn_destructive
 warn_destructive: ## Print WARNING to the user
@@ -117,7 +137,11 @@ localnet_regenesis: ## Regenerate the localnet genesis file
 	mkdir -p $(POKTROLLD_HOME)/config/
 	cp -r ${HOME}/.poktroll/keyring-test $(POKTROLLD_HOME)
 	cp ${HOME}/.poktroll/config/*_key.json $(POKTROLLD_HOME)/config/
-	cp ${HOME}/.poktroll/config/genesis.json $(POKTROLLD_HOME)/config/
+
+# TODO_BLOCKER(@okdas): Figure out how to copy these over w/ a functional state.
+# cp ${HOME}/.poktroll/config/app.toml $(POKTROLLD_HOME)/config/app.toml
+# cp ${HOME}/.poktroll/config/config.toml $(POKTROLLD_HOME)/config/config.toml
+# cp ${HOME}/.poktroll/config/client.toml $(POKTROLLD_HOME)/config/client.toml
 
 ###############
 ### Linting ###
@@ -226,25 +250,18 @@ go_develop_and_test: go_develop go_test ## Generate protos, mocks and run all te
 # TODO_NB                     - An important note to reference later
 # TODO_DISCUSS_IN_THIS_COMMIT - SHOULD NEVER BE COMMITTED TO MASTER. It is a way for the reviewer of a PR to start / reply to a discussion.
 # TODO_IN_THIS_COMMIT         - SHOULD NEVER BE COMMITTED TO MASTER. It is a way to start the review process while non-critical changes are still in progress
-TODO_KEYWORDS = -e "TODO" -e "TODO_COMMUNITY" -e "TODO_DECIDE" -e "TODO_TECHDEBT" -e "TODO_IMPROVE" -e "TODO_OPTIMIZE" -e "TODO_DISCUSS" -e "TODO_INCOMPLETE" -e "TODO_INVESTIGATE" -e "TODO_CLEANUP" -e "TODO_HACK" -e "TODO_REFACTOR" -e "TODO_CONSIDERATION" -e "TODO_IN_THIS_COMMIT" -e "TODO_DISCUSS_IN_THIS_COMMIT" -e "TODO_CONSOLIDATE" -e "TODO_DEPRECATE" -e "TODO_ADDTEST" -e "TODO_RESEARCH" -e "TODO_BUG" -e "TODO_NB"
 
 .PHONY: todo_list
 todo_list: ## List all the TODOs in the project (excludes vendor and prototype directories)
-	grep --exclude-dir={.git,vendor,prototype} -r ${TODO_KEYWORDS}  .
-
-TODO_SEARCH ?= $(shell pwd)
-
-.PHONY: todo_search
-todo_search: ## List all the TODOs in a specific directory specific by `TODO_SEARCH`
-	grep --exclude-dir={.git,vendor,prototype} -r ${TODO_KEYWORDS} ${TODO_SEARCH}
+	grep --exclude-dir={.git,vendor,./docusaurus} -r TODO  .
 
 .PHONY: todo_count
 todo_count: ## Print a count of all the TODOs in the project
-	grep --exclude-dir={.git,vendor,prototype} -r ${TODO_KEYWORDS} . | wc -l
+	grep --exclude-dir={.git,vendor,./docusaurus} -r TODO  . | wc -l
 
 .PHONY: todo_this_commit
 todo_this_commit: ## List all the TODOs needed to be done in this commit
-	grep --exclude-dir={.git,vendor,prototype,.vscode} --exclude=Makefile -r -e "TODO_IN_THIS_COMMIT" -e "TODO_DISCUSS_IN_THIS_COMMIT"
+	grep --exclude-dir={.git,vendor,.vscode} --exclude=Makefile -r -e "TODO_IN_THIS_"
 
 ####################
 ###   Gateways   ###
@@ -526,6 +543,10 @@ go_docs: check_godoc ## Generate documentation for the project
 .PHONY: openapi_gen
 openapi_gen: ## Generate the OpenAPI spec for the Ignite API
 	ignite generate openapi --yes
+
+.PHONY: docusaurus_start
+docusaurus_start: check_npm check_node ## Start the Docusaurus server
+	(cd docusaurus && npm i && npm run start)
 
 ######################
 ### Ignite Helpers ###

.github/workflows/upload-pages-artifact.yml
@@ -1,0 +1,59 @@
+# Based off of https://github.com/LayZeeDK/github-pages-docusaurus
+name: Deploy docusaurus docs to GitHub pages
+
+on:
+  push:
+    branches: ["main"]
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.head_ref || github.ref_name }}
+  cancel-in-progress: true
+
+jobs:
+  deploy:
+    permissions:
+      id-token: write
+      pages: write
+    environment:
+      name: github-pages
+      url: ${{ steps.deployment.outputs.page_url }}
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+
+      # 👇 Build steps
+      - name: Set up Node.js
+        uses: actions/setup-node@v4
+        with:
+          node-version: 18
+          cache: 'npm'
+          cache-dependency-path: docusaurus/package-lock.json
+
+      - name: Install dependencies
+        run: |
+          cd docusaurus
+          npm ci
+
+      - name: Build
+        run: |
+          cd docusaurus
+          npm run build
+
+      # 👆 Build steps
+
+      - name: Setup Pages
+        uses: actions/configure-pages@v4
+        with:
+          enablement: true
+
+      - name: Upload artifact
+        uses: actions/upload-pages-artifact@v2
+        with:
+          # 👇 Specify build output path relative to the subdirectory
+          path: docusaurus/build
+
+      - name: Deploy to GitHub Pages
+        id: deployment
+        uses: actions/deploy-pages@v2
+

.gitignore
@@ -77,3 +77,7 @@ go.work.sum
 
 # TODO_TECHDEBT: It seems that .dot files come and go so we need to figure out the root cause: https://github.com/pokt-network/poktroll/pull/177/files#r1392521547
 # **/*.dot
+
+# Quickstart helpers
+shannon_appgate_config.yaml
+shannon_app_config.yaml

config.yml
@@ -62,6 +62,7 @@ validators:
     bonded: 900000000upokt
     config:
       moniker: "sequencer1"
+
 # We can persist arbitrary genesis values via 1 to 1 mapping to genesis.json
 genesis:
   app_state:

docusaurus/.gitignore
@@ -1,0 +1,20 @@
+# Dependencies
+/node_modules
+
+# Production
+/build
+
+# Generated files
+.docusaurus
+.cache-loader
+
+# Misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*

docusaurus/babel.config.js
@@ -1,0 +1,3 @@
+module.exports = {
+  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
+};

commit ec9352013c9e8839e085b7ae4c617e44815c873d

Fix bug introduced by #252 where genesis file was no longer being copied to the right location

Makefile
@@ -137,6 +137,7 @@ localnet_regenesis: ## Regenerate the localnet genesis file
 	mkdir -p $(POKTROLLD_HOME)/config/
 	cp -r ${HOME}/.poktroll/keyring-test $(POKTROLLD_HOME)
 	cp ${HOME}/.poktroll/config/*_key.json $(POKTROLLD_HOME)/config/
+	cp ${HOME}/.poktroll/config/genesis.json $(POKTROLLD_HOME)/config/
 
 # TODO_BLOCKER(@okdas): Figure out how to copy these over w/ a functional state.
 # cp ${HOME}/.poktroll/config/app.toml $(POKTROLLD_HOME)/config/app.toml