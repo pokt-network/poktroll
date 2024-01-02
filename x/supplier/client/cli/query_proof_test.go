179ebdacdfe8f605167b7471f41a19cf6b01fd5f
Bryan White <bryanchriswhite@gmail.com>
2024-01-02 14:45:59 +0100

    test: proof cli querying

--- x/supplier/client/cli/query_proof_test.go
+++ x/supplier/client/cli/query_proof_test.go
@@ -1,145 +1,225 @@
 package cli_test
 
-// TODO_UPNEXT(@Olshansk): Add these tests back in after merging on-chain Proof persistence.
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	tmcli "github.com/cometbft/cometbft/libs/cli"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	testcli "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/pokt-network/poktroll/testutil/copy"
+	"github.com/pokt-network/poktroll/testutil/network/sessionnet"
+	"github.com/pokt-network/poktroll/testutil/nullify"
+	"github.com/pokt-network/poktroll/testutil/sample"
+	sessiontypes "github.com/pokt-network/poktroll/x/session/types"
+	"github.com/pokt-network/poktroll/x/supplier/client/cli"
+	suppliertypes "github.com/pokt-network/poktroll/x/supplier/types"
+)
+
 // Prevent strconv unused error
-// var _ = strconv.IntSize
+var _ = strconv.IntSize
 
-// func networkWithProofObjects(t *testing.T, n int) (*network.Network, []types.Proof) {
-// 	t.Helper()
-// 	cfg := network.DefaultConfig()
-// 	state := types.GenesisState{}
-// 	for i := 0; i < n; i++ {
-// 	proof := types.Proof{
-// 			Index: strconv.Itoa(i),
+func TestShowProof(t *testing.T) {
+	ctx := context.Background()
+	memnet := sessionnet.DefaultNetworkWithSessions(t)
+	memnet.Start(ctx, t)
 
-// 		}
-// 		nullify.Fill(&proof)
-// 		state.ProofList = append(state.ProofList, proof)
-// 	}
-// 	buf, err := cfg.Codec.MarshalJSON(&state)
-// 	require.NoError(t, err)
-// 	cfg.GenesisState[types.ModuleName] = buf
-// 	return network.New(t, cfg), state.ProofList
-// }
+	claims, sessionTrees := memnet.CreateClaims(t)
+	proofs := memnet.SubmitProofs(t, claims, sessionTrees)
 
-// func TestShowProof(t *testing.T) {
-// 	net, objs := networkWithProofObjects(t, 2)
+	wrongAppAddrSessionHeader := copy.DeepCopyJSON(t, claims[0].GetSessionHeader())
+	wrongAppAddrSessionHeader.ApplicationAddress = sample.AccAddress()
 
-// 	ctx := net.Validators[0].ClientCtx
-// 	common := []string{
-// 		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
-// 	}
-// 	tests := []struct {
-// 		desc string
-// 		idIndex string
+	invalidAppAddrSessionHeader := copy.DeepCopyJSON(t, claims[0].GetSessionHeader())
+	invalidAppAddrSessionHeader.ApplicationAddress = "invalid_application_bech32"
 
-// 		args []string
-// 		err  error
-// 		obj  types.Proof
-// 	}{
-// 		{
-// 			desc: "found",
-// 			idIndex: objs[0].Index,
+	wrongServiceSessionHeader := copy.DeepCopyJSON(t, claims[0].GetSessionHeader())
+	wrongServiceSessionHeader.Service.Id = "wrong_service_id"
 
-// 			args: common,
-// 			obj:  objs[0],
-// 		},
-// 		{
-// 			desc: "not found",
-// 			idIndex: strconv.Itoa(100000),
+	wrongSessionIdSessionHeader := copy.DeepCopyJSON(t, claims[0].GetSessionHeader())
+	wrongSessionIdSessionHeader.SessionId = "wrong_session_id"
 
-// 			args: common,
-// 			err:  status.Error(codes.NotFound, "not found"),
-// 		},
-// 	}
-// 	for _, tc := range tests {
-// 		t.Run(tc.desc, func(t *testing.T) {
-// 			args := []string{
-// 			    tc.idIndex,
+	common := []string{
+		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+	}
+	tests := []struct {
+		desc          string
+		sessionHeader *sessiontypes.SessionHeader
+		supplierAddr  string
 
-// 			}
-// 			args = append(args, tc.args...)
-// 			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdShowProof(), args)
-// 			if tc.err != nil {
-// 				stat, ok := status.FromError(tc.err)
-// 				require.True(t, ok)
-// 				require.ErrorIs(t, stat.Err(), tc.err)
-// 			} else {
-// 				require.NoError(t, err)
-// 				var resp types.QueryGetProofResponse
-// 				require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 				require.NotNil(t, resp.Proof)
-// 				require.Equal(t,
-// 					nullify.Fill(&tc.obj),
-// 					nullify.Fill(&resp.Proof),
-// 				)
-// 			}
-// 		})
-// 	}
-// }
+		args        []string
+		expectedErr error
+		proof       suppliertypes.Proof
+	}{
+		{
+			desc:          "found first",
+			sessionHeader: proofs[0].GetSessionHeader(),
+			supplierAddr:  proofs[0].GetSupplierAddress(),
 
-// func TestListProof(t *testing.T) {
-// 	net, objs := networkWithProofObjects(t, 5)
+			args:  common,
+			proof: proofs[0],
+		},
+		{
+			desc:          "found second",
+			sessionHeader: proofs[1].GetSessionHeader(),
+			supplierAddr:  proofs[1].GetSupplierAddress(),
 
-// 	ctx := net.Validators[0].ClientCtx
-// 	request := func(next []byte, offset, limit uint64, total bool) []string {
-// 		args := []string{
-// 			fmt.Sprintf("--%s=json", tmcli.OutputFlag),
-// 		}
-// 		if next == nil {
-// 			args = append(args, fmt.Sprintf("--%s=%d", flags.FlagOffset, offset))
-// 		} else {
-// 			args = append(args, fmt.Sprintf("--%s=%s", flags.FlagPageKey, next))
-// 		}
-// 		args = append(args, fmt.Sprintf("--%s=%d", flags.FlagLimit, limit))
-// 		if total {
-// 			args = append(args, fmt.Sprintf("--%s", flags.FlagCountTotal))
-// 		}
-// 		return args
-// 	}
-// 	t.Run("ByOffset", func(t *testing.T) {
-// 		step := 2
-// 		for i := 0; i < len(objs); i += step {
-// 			args := request(nil, uint64(i), uint64(step), false)
-// 			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListProof(), args)
-// 			require.NoError(t, err)
-// 			var resp types.QueryAllProofsResponse
-// 			require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 			require.LessOrEqual(t, len(resp.Proof), step)
-// 			require.Subset(t,
-//             	nullify.Fill(objs),
-//             	nullify.Fill(resp.Proof),
-//             )
-// 		}
-// 	})
-// 	t.Run("ByKey", func(t *testing.T) {
-// 		step := 2
-// 		var next []byte
-// 		for i := 0; i < len(objs); i += step {
-// 			args := request(next, 0, uint64(step), false)
-// 			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListProof(), args)
-// 			require.NoError(t, err)
-// 			var resp types.QueryAllProofsResponse
-// 			require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 			require.LessOrEqual(t, len(resp.Proof), step)
-// 			require.Subset(t,
-//             	nullify.Fill(objs),
-//             	nullify.Fill(resp.Proof),
-//             )
-// 			next = resp.Pagination.NextKey
-// 		}
-// 	})
-// 	t.Run("Total", func(t *testing.T) {
-// 		args := request(nil, 0, uint64(len(objs)), true)
-// 		out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListProof(), args)
-// 		require.NoError(t, err)
-// 		var resp types.QueryAllProofsResponse
-// 		require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 		require.NoError(t, err)
-// 		require.Equal(t, len(objs), int(resp.Pagination.Total))
-// 		require.ElementsMatch(t,
-// 			nullify.Fill(objs),
-// 			nullify.Fill(resp.Proof),
-// 		)
-// 	})
-// }
+			args:  common,
+			proof: proofs[1],
+		},
+		{
+			desc:          "proof not found (wrong session ID)",
+			sessionHeader: wrongSessionIdSessionHeader,
+			supplierAddr:  sample.AccAddress(),
+
+			args: common,
+			// TODO_IN_THIS_COMMIT: assert against sentinel error.
+			expectedErr: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc:          "proof not found (invalid application address)",
+			sessionHeader: invalidAppAddrSessionHeader,
+			supplierAddr:  proofs[0].GetSupplierAddress(),
+
+			args: common,
+			// TODO_IN_THIS_COMMIT: assert against sentinel error.
+			expectedErr: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc:          "proof not found (wrong application address)",
+			sessionHeader: wrongAppAddrSessionHeader,
+			supplierAddr:  proofs[0].GetSupplierAddress(),
+
+			args: common,
+			// TODO_IN_THIS_COMMIT: assert against sentinel error.
+			expectedErr: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc:          "proof not found (wrong service)",
+			sessionHeader: wrongServiceSessionHeader,
+			supplierAddr:  claims[0].GetSupplierAddress(),
+
+			args: common,
+			// TODO_IN_THIS_COMMIT: assert against sentinel error.
+			expectedErr: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc:          "proof not found (invalid bech32 supplier address)",
+			sessionHeader: proofs[0].GetSessionHeader(),
+			supplierAddr:  "invalid_supplier_bech32",
+
+			args: common,
+			// TODO_IN_THIS_COMMIT: assert against sentinel error.
+			expectedErr: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc:          "proof not found (wrong supplier address)",
+			sessionHeader: proofs[0].GetSessionHeader(),
+			supplierAddr:  sample.AccAddress(),
+
+			args: common,
+			// TODO_IN_THIS_COMMIT: assert against sentinel error.
+			expectedErr: status.Error(codes.NotFound, "not found"),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			args := []string{
+				tc.sessionHeader.GetSessionId(),
+				tc.supplierAddr,
+			}
+			args = append(args, tc.args...)
+			out, cliError := testcli.ExecTestCLICmd(memnet.GetClientCtx(t), cli.CmdShowProof(), args)
+
+			if tc.expectedErr != nil {
+				stat, ok := status.FromError(tc.expectedErr)
+				require.True(t, ok)
+				require.ErrorIs(t, stat.Err(), tc.expectedErr)
+			} else {
+				require.NoError(t, cliError)
+				var resp suppliertypes.QueryGetProofResponse
+				t.Logf("out: %s", out.String())
+				require.NoError(t, memnet.GetNetwork(t).Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+				require.NotNil(t, resp.Proof)
+				require.EqualValues(t, tc.proof, resp.Proof)
+			}
+		})
+	}
+}
+
+func TestListProof(t *testing.T) {
+	ctx := context.Background()
+	memnet := sessionnet.DefaultNetworkWithSessions(t)
+	memnet.Start(ctx, t)
+
+	claims, sessionTrees := memnet.CreateClaims(t)
+	proofs := memnet.SubmitProofs(t, claims, sessionTrees)
+
+	clientCtx := memnet.GetClientCtx(t)
+	request := func(next []byte, offset, limit uint64, total bool) []string {
+		args := []string{
+			fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+		}
+		if next == nil {
+			args = append(args, fmt.Sprintf("--%s=%d", flags.FlagOffset, offset))
+		} else {
+			args = append(args, fmt.Sprintf("--%s=%s", flags.FlagPageKey, next))
+		}
+		args = append(args, fmt.Sprintf("--%s=%d", flags.FlagLimit, limit))
+		if total {
+			args = append(args, fmt.Sprintf("--%s", flags.FlagCountTotal))
+		}
+		return args
+	}
+	t.Run("ByOffset", func(t *testing.T) {
+		step := 2
+		for i := 0; i < len(proofs); i += step {
+			args := request(nil, uint64(i), uint64(step), false)
+			out, expectedErr := testcli.ExecTestCLICmd(clientCtx, cli.CmdListProof(), args)
+			require.NoError(t, expectedErr)
+			var resp suppliertypes.QueryAllProofsResponse
+			require.NoError(t, memnet.GetNetwork(t).Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+			require.LessOrEqual(t, len(resp.Proof), step)
+			require.Subset(t,
+				nullify.Fill(proofs),
+				nullify.Fill(resp.Proof),
+			)
+		}
+	})
+	t.Run("ByKey", func(t *testing.T) {
+		step := 2
+		var next []byte
+		for i := 0; i < len(proofs); i += step {
+			args := request(next, 0, uint64(step), false)
+			out, expectedErr := testcli.ExecTestCLICmd(clientCtx, cli.CmdListProof(), args)
+			require.NoError(t, expectedErr)
+			var resp suppliertypes.QueryAllProofsResponse
+			require.NoError(t, memnet.GetNetwork(t).Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+			require.LessOrEqual(t, len(resp.Proof), step)
+			require.Subset(t,
+				nullify.Fill(proofs),
+				nullify.Fill(resp.Proof),
+			)
+			next = resp.Pagination.NextKey
+		}
+	})
+	t.Run("Total", func(t *testing.T) {
+		args := request(nil, 0, uint64(len(proofs)), true)
+		out, expectedErr := testcli.ExecTestCLICmd(clientCtx, cli.CmdListProof(), args)
+		require.NoError(t, expectedErr)
+		var resp suppliertypes.QueryAllProofsResponse
+		require.NoError(t, memnet.GetNetwork(t).Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+		require.NoError(t, expectedErr)
+		require.Equal(t, len(proofs), int(resp.Pagination.Total))
+		require.ElementsMatch(t,
+			nullify.Fill(proofs),
+			nullify.Fill(resp.Proof),
+		)
+	})
+}
