bc55aa2836fbaa7b6da4428825a23563f4c65391
Redouane Lakrache <r3d0ne@gmail.com>
2024-01-04 21:58:04 +0100

    [Configs] Add foundation for RelayMiner operation configs. (#284)
    * feat: Working SDK with AppGateServer integration
    
    * chore: Address change requests
    
    * fix: Chain depinject configs
    
    * chore: Expose the full SessionSuppliers struct
    
    * fix: Avoid nil sessions on GetSessionSupplierEndpoints
    
    * chore: Explain SingleSupplierEndpoint struct usage
    
    * fix: Rename client/event import as per main chainges
    
    * fix: Add missing fmt package
    
    * feat: Enable gRPC client connection
    
    * feat: Have distinct JSON-RPC and gRPC urls
    
    * chore: Trigger e2e tests
    
    * chore: Rename config files entries and enable grpc for off-chain actors
    
    * chore: Revert reviewdog
    
    * chore: Address request changes
    
    * chore: Fix temp assignment of FlagGRPC
    
    * feat: [WIP] Implement new replayminer config
    
    * feat: Adapt tests to new relay miner config
    
    * chore: Add relay miner config and full example
    
    * chore: add valid relay miner config
    
    * chore: Address request changes
    
    * fix: Correct tests error assertions
    
    * chore: Address request changes
    
    * refactor: Split relayminer config parsing and validation logic
    
    * chore: Add TODO to make MarshalAndSend RPC-type agnostic
    
    * chore: Strip-off comments form AppGateServer config, add documented example config
    
    * chore: Strip comments out of relay miner config and reorder entries
    
    * chore: Update flagNode variables and command documentation
    
    * fix: Update sequencer host used in local net
    
    * chore: Address change requests
    
    * chore: Add UDP in TODO comment
    
    * chore: Improve documentation about multiple hosts config
    
    * chore: Add change to trigger e2e test
    
    * chore: e2e bump
    
    * fix: Use RPC url for tx broadcasting

--- pkg/appgateserver/config/appgate_configs_reader.go
+++ pkg/appgateserver/config/appgate_configs_reader.go
@@ -9,18 +9,20 @@ import (
 // YAMLAppGateServerConfig is the structure used to unmarshal the AppGateServer config file
 // TODO_DOCUMENT(@red-0ne): Add proper README documentation for yaml config files.
 type YAMLAppGateServerConfig struct {
-	SelfSigning       bool   `yaml:"self_signing"`
+	QueryNodeRPCUrl   string `yaml:"query_node_rpc_url"`
+	QueryNodeGRPCUrl  string `yaml:"query_node_grpc_url"`
 	SigningKey        string `yaml:"signing_key"`
+	SelfSigning       bool   `yaml:"self_signing"`
 	ListeningEndpoint string `yaml:"listening_endpoint"`
-	QueryNodeUrl      string `yaml:"query_node_url"`
 }
 
 // AppGateServerConfig is the structure describing the AppGateServer config
 type AppGateServerConfig struct {
-	SelfSigning       bool
+	QueryNodeRPCUrl   *url.URL
+	QueryNodeGRPCUrl  *url.URL
 	SigningKey        string
+	SelfSigning       bool
 	ListeningEndpoint *url.URL
-	QueryNodeUrl      *url.URL
 }
 
 // ParseAppGateServerConfigs parses the stake config file into a AppGateConfig
@@ -28,39 +30,53 @@ type AppGateServerConfig struct {
 func ParseAppGateServerConfigs(configContent []byte) (*AppGateServerConfig, error) {
 	var yamlAppGateServerConfig YAMLAppGateServerConfig
 
-	// Unmarshal the stake config file into a yamlAppGateConfig
-	if err := yaml.Unmarshal(configContent, &yamlAppGateServerConfig); err != nil {
-		return nil, ErrAppGateConfigUnmarshalYAML.Wrapf("%s", err)
+	if len(configContent) == 0 {
+		return nil, ErrAppGateConfigEmpty
 	}
 
-	if yamlAppGateServerConfig.SigningKey == "" {
+	// Unmarshal the stake config file into a yamlAppGateConfig
+	if err := yaml.Unmarshal(configContent, &yamlAppGateServerConfig); err != nil {
+		return nil, ErrAppGateConfigUnmarshalYAML.Wrap(err.Error())
+	}
+
+	if len(yamlAppGateServerConfig.SigningKey) == 0 {
 		return nil, ErrAppGateConfigEmptySigningKey
 	}
 
-	if yamlAppGateServerConfig.ListeningEndpoint == "" {
+	if len(yamlAppGateServerConfig.ListeningEndpoint) == 0 {
 		return nil, ErrAppGateConfigInvalidListeningEndpoint
 	}
 
 	listeningEndpoint, err := url.Parse(yamlAppGateServerConfig.ListeningEndpoint)
 	if err != nil {
-		return nil, ErrAppGateConfigInvalidListeningEndpoint.Wrapf("%s", err)
+		return nil, ErrAppGateConfigInvalidListeningEndpoint.Wrap(err.Error())
 	}
 
-	if yamlAppGateServerConfig.QueryNodeUrl == "" {
-		return nil, ErrAppGateConfigInvalidQueryNodeUrl
+	if len(yamlAppGateServerConfig.QueryNodeGRPCUrl) == 0 {
+		return nil, ErrAppGateConfigInvalidQueryNodeGRPCUrl
 	}
 
-	queryNodeUrl, err := url.Parse(yamlAppGateServerConfig.QueryNodeUrl)
+	queryNodeGRPCUrl, err := url.Parse(yamlAppGateServerConfig.QueryNodeGRPCUrl)
 	if err != nil {
-		return nil, ErrAppGateConfigInvalidQueryNodeUrl.Wrapf("%s", err)
+		return nil, ErrAppGateConfigInvalidQueryNodeGRPCUrl.Wrap(err.Error())
+	}
+
+	if len(yamlAppGateServerConfig.QueryNodeRPCUrl) == 0 {
+		return nil, ErrAppGateConfigInvalidQueryNodeRPCUrl
+	}
+
+	queryNodeRPCUrl, err := url.Parse(yamlAppGateServerConfig.QueryNodeRPCUrl)
+	if err != nil {
+		return nil, ErrAppGateConfigInvalidQueryNodeRPCUrl.Wrap(err.Error())
 	}
 
 	// Populate the appGateServerConfig with the values from the yamlAppGateServerConfig
 	appGateServerConfig := &AppGateServerConfig{
-		SelfSigning:       yamlAppGateServerConfig.SelfSigning,
+		QueryNodeRPCUrl:   queryNodeRPCUrl,
+		QueryNodeGRPCUrl:  queryNodeGRPCUrl,
 		SigningKey:        yamlAppGateServerConfig.SigningKey,
+		SelfSigning:       yamlAppGateServerConfig.SelfSigning,
 		ListeningEndpoint: listeningEndpoint,
-		QueryNodeUrl:      queryNodeUrl,
 	}
 
 	return appGateServerConfig, nil
