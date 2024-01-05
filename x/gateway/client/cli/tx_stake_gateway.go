f600f294c20c50cdacd0aa0de7e6ab4da7741aaf
Redouane Lakrache <r3d0ne@gmail.com>
2024-01-05 23:00:24 +0100

    fix: Adapt gateway staking e2e tests

--- x/gateway/client/cli/tx_stake_gateway.go
+++ x/gateway/client/cli/tx_stake_gateway.go
@@ -25,7 +25,7 @@ func CmdStakeGateway() *cobra.Command {
 		Long: `Stake a gateway with the provided parameters. This is a broadcast operation that
 will stake the tokens and associate them with the gateway specified by the 'from' address.
 Example:
-$ poktrolld --home=$(POKTROLLD_HOME) tx gateway stake-gateway --config stake_config --keyring-backend test --from $(GATEWAY) --node $(POCKET_NODE)`,
+$ poktrolld --home=$(POKTROLLD_HOME) tx gateway stake-gateway --config stake_config.yaml --keyring-backend test --from $(GATEWAY) --node $(POCKET_NODE)`,
 		Args: cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, _ []string) (err error) {
 			configContent, err := os.ReadFile(flagStakeConfig)
