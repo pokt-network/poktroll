cc0c8a3dbf582d7cf97b50aef237adbdc748d606
h5law <h5law.curve514@passfwd.com>
2024-01-11 13:36:06 +0000

    feat: implement the add-service CLI command

--- x/service/client/cli/tx_add_service.go
+++ x/service/client/cli/tx_add_service.go
@@ -6,18 +6,27 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	"github.com/pokt-network/poktroll/x/service/types"
 	"github.com/spf13/cobra"
+
+	"github.com/pokt-network/poktroll/x/service/types"
 )
 
 var _ = strconv.Itoa(0)
 
 func CmdAddService() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "add-service",
-		Short: "Broadcast message add-service",
-		Args:  cobra.ExactArgs(0),
+		Use:   "add-service <service_id> <service_name>",
+		Short: "Add a new service to the network",
+		Long: `Add a new service to the network that will be available for applications,
+gateways and suppliers to use. The service id MUST be unique - or the command
+will fail, however the name you use to describe it does not have to be unique.
+
+Example:
+$ poktrolld --home=$(POKTROLLD_HOME) tx service add-service "srv1" "service_one" --keyring-backend test --from $(SUPPLIER) --node $(POCKET_NODE)`,
+		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			serviceIdStr := args[0]
+			serviceNameStr := args[1]
 
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -26,6 +35,8 @@ func CmdAddService() *cobra.Command {
 
 			msg := types.NewMsgAddService(
 				clientCtx.GetFromAddress().String(),
+				serviceIdStr,
+				serviceNameStr,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
