02ce44b87525020a28e592d1d5513be24d29ff0e
h5law <h5law.curve514@passfwd.com>
2024-01-12 16:51:44 +0000

    [Service] Scaffold the AddService command and nothing else (#312)

--- x/service/keeper/msg_server_test.go
+++ x/service/keeper/msg_server_test.go
@@ -5,11 +5,10 @@ import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
-
 	keepertest "github.com/pokt-network/poktroll/testutil/keeper"
 	"github.com/pokt-network/poktroll/x/service/keeper"
 	"github.com/pokt-network/poktroll/x/service/types"
+	"github.com/stretchr/testify/require"
 )
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
