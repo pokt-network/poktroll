03ff71781aa2dc2e6917e6645875657577cc965e
h5law <h5law.curve514@passfwd.com>
2024-01-16 00:36:41 +0000

    feat: replace scaffold lines with correct gci import ordering

--- x/pocket/module.go
+++ x/pocket/module.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
+	// this line is used by starport scaffolding # 1
+	
 	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
