58b74be441857e49d0a530ba91155fc30b5125b9
Eric Nielson <ezeike@gmail.com>
2024-01-19 11:23:05 -0500

    [SDK] GetApplications SDK Method

--- pkg/sdk/errors.go
+++ pkg/sdk/errors.go
@@ -8,4 +8,5 @@ var (
 	ErrSDKInvalidRelayResponseSignature = sdkerrors.Register(codespace, 2, "invalid relay response signature")
 	ErrSDKEmptyRelayResponseSignature   = sdkerrors.Register(codespace, 3, "empty relay response signature")
 	ErrSDKVerifyResponseSignature       = sdkerrors.Register(codespace, 4, "error verifying relay response signature")
+	ErrSDKGetApplications               = sdkerrors.Register(codespace, 5, "error querying on-chain applications")
 )
--- pkg/sdk/interface.go
+++ pkg/sdk/interface.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"net/http"
 
+	apptypes "github.com/pokt-network/poktroll/x/application/types"
 	servicetypes "github.com/pokt-network/poktroll/x/service/types"
 )
 
 // POKTRollSDK is the interface for the POKTRoll SDK. It is used by gateways
 // and/or applications to interact with the Pocket protocol.
 type POKTRollSDK interface {
-	// GetSession returns the suppliers endpoints of the current session for
+	// GetApplications queries a list of all on-chain applications, optionally filtered by
+	// delegated gateway
+	GetApplications(ctx context.Context, options GetApplicationsOptions) ([]apptypes.Application, error)
+
+	// GetSessionSupplierEndpoints returns the suppliers endpoints of the current session for
 	// the given application and service.
 	GetSessionSupplierEndpoints(
 		ctx context.Context,
