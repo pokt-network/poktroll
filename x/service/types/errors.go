267a1328d0c9167b2229a4038839dd4a24c5e05c
Bryan White <bryanchriswhite@gmail.com>
2024-02-06 16:48:09 +0100

    reconcile: module errors

--- x/service/types/errors.go
+++ x/service/types/errors.go
@@ -8,6 +8,14 @@ import (
 
 // x/service module sentinel errors
 var (
-	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
+	ErrServiceDuplicateIndex    = sdkerrors.Register(ModuleName, 1, "duplicate index when adding a new service")
+	ErrServiceInvalidAddress    = sdkerrors.Register(ModuleName, 2, "invalid address when adding a new service")
+	ErrServiceMissingID         = sdkerrors.Register(ModuleName, 3, "missing service ID")
+	ErrServiceMissingName       = sdkerrors.Register(ModuleName, 4, "missing service name")
+	ErrServiceAlreadyExists     = sdkerrors.Register(ModuleName, 5, "service already exists")
+	ErrServiceInvalidServiceFee = sdkerrors.Register(ModuleName, 6, "invalid service fee")
+	ErrServiceAccountNotFound   = sdkerrors.Register(ModuleName, 7, "account not found")
+	ErrServiceNotEnoughFunds    = sdkerrors.Register(ModuleName, 8, "not enough funds to add service")
+	ErrServiceFailedToDeductFee = sdkerrors.Register(ModuleName, 9, "failed to deduct fee")
+	ErrInvalidSigner            = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
 )
