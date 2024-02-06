e98920f09e6b57df89613a1d6c98155da892b629
Bryan White <bryanchriswhite@gmail.com>
2024-02-06 16:53:27 +0100

    scaffold: module service --params add_service_fee:uint --dep bank

---
+++ x/service/module/genesis_test.go
@@ -0,0 +1,29 @@
+package service_test
+
+import (
+	"testing"
+
+	keepertest "github.com/pokt-network/poktroll/testutil/keeper"
+	"github.com/pokt-network/poktroll/testutil/nullify"
+	"github.com/pokt-network/poktroll/x/service/module"
+	"github.com/pokt-network/poktroll/x/service/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ServiceKeeper(t)
+	service.InitGenesis(ctx, k, genesisState)
+	got := service.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
