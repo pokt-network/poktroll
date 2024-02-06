321766bc212c45bc53352a98f8160fd99bf5443b
Bryan White <bryanchriswhite@gmail.com>
2024-02-06 12:47:55 +0100

    chore: temporarily remove `testutil` pkg

--- testutil/testclient/testblock/godoc.go
+++
@@ -1,4 +0,0 @@
-// Package testblock provides helper functions for constructing real (e.g. localnet)
-// and mock BlockClient objects with pre-configured and/or parameterized call
-// arguments, return value(s), and/or expectations thereof. Intended for use in tests.
-package testblock
--- testutil/testclient/testeventsquery/godoc.go
+++
@@ -1,5 +0,0 @@
-// Package testeventsquery provides helper functions for constructing real
-// (e.g. localnet) and mock EventsQueryClient objects with pre-configured and/or
-// parameterized call arguments, return value(s), and/or expectations thereof.
-// Intended for use in tests.
-package testeventsquery
