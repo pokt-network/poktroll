fe497117a5196f8f1c1e47f18383abc47db92139
Bryan White <bryanchriswhite@gmail.com>
2024-02-06 12:48:33 +0100

    chore: temporarily remove `pkg` pkgs

--- pkg/client/query/godoc.go
+++
@@ -1,4 +0,0 @@
-// Package query provides interfaces used to query the state of the blockchain.
-// These interfaces abstract the underlying request/response types and provide
-// a single method to query for a specific piece of information from the chain.
-package query
--- pkg/crypto/godoc.go
+++
@@ -1,5 +0,0 @@
-// Package crypto provides the cryptographic primitives used by the Pocket
-// Network. This includes the RingCache used to build and cache rings used
-// for signing relays, and may expand to include other cryptographic
-// primitives in the future.
-package crypto
