3afa16ef648263c4c5f098959bbf5d3eefeb25d5
Bryan White <bryanchriswhite@gmail.com>
2024-02-07 08:20:40 +0100

    scaffold: message add-service --module service --signer address

--- x/service/types/message_add_service_test.go
+++ x/service/types/message_add_service_test.go
@@ -3,53 +3,35 @@ package types
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/pokt-network/poktroll/testutil/sample"
-	sharedtypes "github.com/pokt-network/poktroll/x/shared/types"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMsgAddService_ValidateBasic(t *testing.T) {
 	tests := []struct {
-		desc        string
-		msg         MsgAddService
-		expectedErr error
+		name string
+		msg  MsgAddService
+		err  error
 	}{
 		{
-			desc: "invalid service supplier address - no service",
+			name: "invalid address",
 			msg: MsgAddService{
 				Address: "invalid_address",
-				// Service: intentionally omitted,
 			},
-			expectedErr: ErrServiceInvalidAddress,
+			err: sdkerrors.ErrInvalidAddress,
 		}, {
-			desc: "valid service supplier address - no service ID",
+			name: "valid address",
 			msg: MsgAddService{
 				Address: sample.AccAddress(),
-				Service: sharedtypes.Service{Name: "service name"}, // ID intentionally omitted
 			},
-			expectedErr: ErrServiceMissingID,
-		}, {
-			desc: "valid service supplier address - no service name",
-			msg: MsgAddService{
-				Address: sample.AccAddress(),
-				Service: sharedtypes.Service{Id: "svc1"}, // Name intentionally omitted
-			},
-			expectedErr: ErrServiceMissingName,
-		}, {
-			desc: "valid service supplier address and service",
-			msg: MsgAddService{
-				Address: sample.AccAddress(),
-				Service: sharedtypes.Service{Id: "svc1", Name: "service name"},
-			},
-			expectedErr: nil,
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.desc, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.expectedErr != nil {
-				require.ErrorIs(t, err, tt.expectedErr)
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
 				return
 			}
 			require.NoError(t, err)
