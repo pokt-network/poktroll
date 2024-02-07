374ee3569a777c33465e4b16dd0f0a953a0eacfb
Bryan White <bryanchriswhite@gmail.com>
2024-02-07 17:13:29 +0100

    reconcile: network testutils

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -1,19 +1,30 @@
 package network
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
+	sdkmath "cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	addresscodec "github.com/cosmos/cosmos-sdk/codec/address"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/pokt-network/poktroll/app"
+	"github.com/pokt-network/poktroll/cmd/poktrolld/cmd"
 )
 
 type (
 	Network = network.Network
 	Config  = network.Config
 )
+
+func init() {
+	cmd.InitSDKConfig()
+}
 
 // New creates instance with fully configured cosmos network.
 // Accepts optional config, that will be used in place of the DefaultConfig() if provided.
@@ -57,6 +68,38 @@ func DefaultConfig() network.Config {
 		cfg.GRPCAddress = fmt.Sprintf("0.0.0.0:%s", ports[2])
 	}
 	return cfg
+}
+
+// InitAccountWithSequence initializes an Account by sending it some funds from
+// the validator in the network to the address provided
+func InitAccountWithSequence(
+	t *testing.T,
+	net *Network,
+	addr sdk.AccAddress,
+	signatureSequencerNumber int,
+) {
+	t.Helper()
+	val := net.Validators[0]
+	signerAccountNumber := 0
+	ctx := val.ClientCtx
+	args := []string{
+		fmt.Sprintf("--%s=true", flags.FlagOffline),
+		fmt.Sprintf("--%s=%d", flags.FlagAccountNumber, signerAccountNumber),
+		fmt.Sprintf("--%s=%d", flags.FlagSequence, signatureSequencerNumber),
+
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdkmath.NewInt(10))).String()),
+	}
+	amount := sdk.NewCoins(sdk.NewCoin("stake", sdkmath.NewInt(200)))
+	addrCodec := addresscodec.NewBech32Codec(app.AccountAddressPrefix)
+	responseRaw, err := clitestutil.MsgSendExec(ctx, val.Address, addr, amount, addrCodec, args...)
+	require.NoError(t, err)
+	var responseJSON map[string]interface{}
+	err = json.Unmarshal(responseRaw.Bytes(), &responseJSON)
+	require.NoError(t, err)
+	require.Equal(t, float64(0), responseJSON["code"], "code is not 0 in the response: %v", responseJSON)
 }
 
 // freePorts return the available ports based on the number of requested ports.
