6392587c72cfb3fe78090608f94f3504b693cf3f
Bryan White <bryanchriswhite@gmail.com>
2024-02-07 11:57:45 +0100

    reconcile: `x/service/keeper.msgServer#AddService()`

--- x/service/types/expected_keepers.go
+++ x/service/types/expected_keepers.go
@@ -16,6 +16,13 @@ type AccountKeeper interface {
 type BankKeeper interface {
 	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
+
+	SendCoinsFromAccountToModule(
+		ctx context.Context,
+		senderAddr sdk.AccAddress,
+		recipientModule string,
+		amt sdk.Coins,
+	) error
 }
 
 // ParamSubspace defines the expected Subspace interface for parameters.
