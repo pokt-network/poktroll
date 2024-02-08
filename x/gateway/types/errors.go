71f3214def0861e2f342fd78597ac66cf3de6458
Redouane Lakrache <r3d0ne@gmail.com>
2024-02-08 10:21:15 +0100

    reconcile: gateway stake

--- x/gateway/types/errors.go
+++ x/gateway/types/errors.go
@@ -8,6 +8,9 @@ import (
 
 // x/gateway module sentinel errors
 var (
-	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
+	ErrInvalidSigner         = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrSample                = sdkerrors.Register(ModuleName, 1101, "sample error")
+	ErrGatewayInvalidAddress = sdkerrors.Register(ModuleName, 1102, "invalid gateway address")
+	ErrGatewayInvalidStake   = sdkerrors.Register(ModuleName, 1103, "invalid gateway stake")
+	ErrGatewayUnauthorized   = sdkerrors.Register(ModuleName, 1104, "unauthorized signer")
 )
