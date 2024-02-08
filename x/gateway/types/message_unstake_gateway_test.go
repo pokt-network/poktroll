f3bd7c7e160db4ca4a0a2fba7f8d30c2e4911ed9
Redouane Lakrache <r3d0ne@gmail.com>
2024-02-08 07:09:54 +0100

    scaffold: message unstake_gateway --module gateway
    ignite scaffold message unstake_gateway --signer address --module gateway

---
+++ x/gateway/types/message_unstake_gateway_test.go
@@ -0,0 +1,40 @@
+package types
+
+import (
+	"testing"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/pokt-network/poktroll/testutil/sample"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgUnstakeGateway_ValidateBasic(t *testing.T) {
+	tests := []struct {
+		name string
+		msg  MsgUnstakeGateway
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgUnstakeGateway{
+				Address: "invalid_address",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		}, {
+			name: "valid address",
+			msg: MsgUnstakeGateway{
+				Address: sample.AccAddress(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
