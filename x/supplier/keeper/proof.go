c8e5fa4d03c83985e912dbf97c6bc8cc7e78a187
Bryan White <bryanchriswhite@gmail.com>
2024-02-08 15:43:14 +0100

    reconcile: `Proof` proto type

--- x/supplier/keeper/proof.go
+++ x/supplier/keeper/proof.go
@@ -6,6 +6,7 @@ import (
 	"cosmossdk.io/store/prefix"
 	storetypes "cosmossdk.io/store/types"
 	"github.com/cosmos/cosmos-sdk/runtime"
+
 	"github.com/pokt-network/poktroll/x/supplier/types"
 )
 
@@ -15,7 +16,7 @@ func (k Keeper) SetProof(ctx context.Context, proof types.Proof) {
 	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.ProofKeyPrefix))
 	b := k.cdc.MustMarshal(&proof)
 	store.Set(types.ProofKey(
-		proof.Index,
+		proof.SupplierAddress,
 	), b)
 }
 
