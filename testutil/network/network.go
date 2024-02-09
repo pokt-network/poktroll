6f0dbffee6d25969dd2173f7c3f51941fb649d1d
Redouane Lakrache <r3d0ne@gmail.com>
2024-02-09 03:24:16 +0100

    reconcile: gateway CLI commands

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -15,6 +15,8 @@ import (
 
 	"github.com/pokt-network/poktroll/app"
 	"github.com/pokt-network/poktroll/cmd/poktrolld/cmd"
+	"github.com/pokt-network/poktroll/testutil/sample"
+	gatewaytypes "github.com/pokt-network/poktroll/x/gateway/types"
 )
 
 type (
@@ -70,6 +72,49 @@ func DefaultConfig() network.Config {
 	return cfg
 }
 
+// DefaultGatewayModuleGenesisState generates a GenesisState object with a given
+// number of gateways. It returns the populated GenesisState object.
+func DefaultGatewayModuleGenesisState(t *testing.T, n int) *gatewaytypes.GenesisState {
+	t.Helper()
+	state := gatewaytypes.DefaultGenesis()
+	for i := 0; i < n; i++ {
+		stake := sdk.NewCoin("upokt", sdkmath.NewInt(int64(i)))
+		gateway := gatewaytypes.Gateway{
+			Address: sample.AccAddress(),
+			Stake:   &stake,
+		}
+		// TODO_CONSIDERATION: Evaluate whether we need `nullify.Fill` or if we should enforce `(gogoproto.nullable) = false` everywhere
+		// nullify.Fill(&gateway)
+		state.GatewayList = append(state.GatewayList, gateway)
+	}
+	return state
+}
+
+// TODO_CLEANUP: Consolidate all of the helpers below to use shared business
+// logic and move into its own helpers file.
+
+// InitAccount initializes an Account by sending it some funds from the validator
+// in the network to the address provided
+func InitAccount(t *testing.T, net *Network, addr sdk.AccAddress) {
+	t.Helper()
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	args := []string{
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdkmath.NewInt(10))).String()),
+	}
+	amount := sdk.NewCoins(sdk.NewCoin("stake", sdkmath.NewInt(200)))
+	addrCodec := addresscodec.NewBech32Codec(app.AccountAddressPrefix)
+	responseRaw, err := clitestutil.MsgSendExec(ctx, val.Address, addr, amount, addrCodec, args...)
+	require.NoError(t, err)
+	var responseJSON map[string]interface{}
+	err = json.Unmarshal(responseRaw.Bytes(), &responseJSON)
+	require.NoError(t, err)
+	require.Equal(t, float64(0), responseJSON["code"], "code is not 0 in the response: %v", responseJSON)
+}
+
 // InitAccountWithSequence initializes an Account by sending it some funds from
 // the validator in the network to the address provided
 func InitAccountWithSequence(
@@ -86,7 +131,6 @@ func InitAccountWithSequence(
 		fmt.Sprintf("--%s=true", flags.FlagOffline),
 		fmt.Sprintf("--%s=%d", flags.FlagAccountNumber, signerAccountNumber),
 		fmt.Sprintf("--%s=%d", flags.FlagSequence, signatureSequencerNumber),
-
 		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
 		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
--- x/gateway/types/message_unstake_gateway.go
+++ x/gateway/types/message_unstake_gateway.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-var _ sdk.Msg = &MsgUnstakeGateway{}
+var _ sdk.Msg = (*MsgUnstakeGateway)(nil)
 
 func NewMsgUnstakeGateway(address string) *MsgUnstakeGateway {
 	return &MsgUnstakeGateway{
