8f6387fc9dd2760ecd909bd1325123d1b1e97afd
Dima Kniazev <okdas@users.noreply.github.com>
2024-02-09 15:03:34 -0800

    [SDK Migration] Switch to celestia-da, bump rollkit, fix CI (#367)

--- app/app.go
+++ app/app.go
@@ -61,7 +61,7 @@ import (
 
 const (
 	AccountAddressPrefix = "pokt"
-	Name                 = "poktroll"
+	Name                 = "pocket"
 	// TODO_CLEANUP: Find a way to centralize the use of `upokt` throughout the codebase
 	DenomuPOKT = "upokt"
 )
