fe090e61f651b8b8a752794290bd5163fbb83ad7
Bryan White <bryanchriswhite@gmail.com>
2024-02-12 10:16:43 +0100

    fix: app name

--- app/app.go
+++ app/app.go
@@ -60,7 +60,7 @@ import (
 
 const (
 	AccountAddressPrefix = "pokt"
-	Name                 = "pocket"
+	Name                 = "poktroll"
 	// TODO_CLEANUP: Find a way to centralize the use of `upokt` throughout the codebase
 	DenomuPOKT = "upokt"
 )
