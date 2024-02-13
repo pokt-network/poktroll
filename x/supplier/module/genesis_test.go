82826dcf3d46d22cf8534a60192e1007e417af8f
Bryan White <bryanchriswhite@gmail.com>
2024-02-13 16:37:09 +0100

    reconcile: module genesis

--- x/supplier/module/genesis_test.go
+++ x/supplier/module/genesis_test.go
@@ -3,23 +3,57 @@ package supplier_test
 import (
 	"testing"
 
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
 	keepertest "github.com/pokt-network/poktroll/testutil/keeper"
 	"github.com/pokt-network/poktroll/testutil/nullify"
+	"github.com/pokt-network/poktroll/testutil/sample"
+	sharedtypes "github.com/pokt-network/poktroll/x/shared/types"
 	"github.com/pokt-network/poktroll/x/supplier/module"
 	"github.com/pokt-network/poktroll/x/supplier/types"
-	"github.com/stretchr/testify/require"
 )
 
 func TestGenesis(t *testing.T) {
 	genesisState := types.GenesisState{
 		Params: types.DefaultParams(),
-
-		SupplierList: []types.Supplier{
+		SupplierList: []sharedtypes.Supplier{
 			{
-				Index: "0",
+				Address: sample.AccAddress(),
+				Stake:   &sdk.Coin{Denom: "upokt", Amount: math.NewInt(100)},
+				Services: []*sharedtypes.SupplierServiceConfig{
+					{
+						Service: &sharedtypes.Service{
+							Id: "svcId1",
+						},
+						Endpoints: []*sharedtypes.SupplierEndpoint{
+							{
+								Url:     "http://localhost:8081",
+								RpcType: sharedtypes.RPCType_JSON_RPC,
+								Configs: make([]*sharedtypes.ConfigOption, 0),
+							},
+						},
+					},
+				},
 			},
 			{
-				Index: "1",
+				Address: sample.AccAddress(),
+				Stake:   &sdk.Coin{Denom: "upokt", Amount: math.NewInt(100)},
+				Services: []*sharedtypes.SupplierServiceConfig{
+					{
+						Service: &sharedtypes.Service{
+							Id: "svcId2",
+						},
+						Endpoints: []*sharedtypes.SupplierEndpoint{
+							{
+								Url:     "http://localhost:8082",
+								RpcType: sharedtypes.RPCType_GRPC,
+								Configs: make([]*sharedtypes.ConfigOption, 0),
+							},
+						},
+					},
+				},
 			},
 		},
 		// this line is used by starport scaffolding # genesis/test/state
