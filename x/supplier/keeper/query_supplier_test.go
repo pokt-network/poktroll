d1ed1f26229e3d1f6b48a23e185b1683702f5820
Bryan White <bryanchriswhite@gmail.com>
2024-02-14 12:39:56 +0100

    scaffold: map supplier --module supplier --no-message address stake:coin services

---
+++ x/supplier/keeper/query_supplier_test.go
@@ -0,0 +1,124 @@
+package keeper_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	keepertest "github.com/pokt-network/poktroll/testutil/keeper"
+	"github.com/pokt-network/poktroll/testutil/nullify"
+	"github.com/pokt-network/poktroll/x/supplier/types"
+)
+
+// Prevent strconv unused error
+var _ = strconv.IntSize
+
+func TestSupplierQuerySingle(t *testing.T) {
+	keeper, ctx := keepertest.SupplierKeeper(t)
+	msgs := createNSupplier(keeper, ctx, 2)
+	tests := []struct {
+		desc     string
+		request  *types.QueryGetSupplierRequest
+		response *types.QueryGetSupplierResponse
+		err      error
+	}{
+		{
+			desc: "First",
+			request: &types.QueryGetSupplierRequest{
+				Index: msgs[0].Index,
+			},
+			response: &types.QueryGetSupplierResponse{Supplier: msgs[0]},
+		},
+		{
+			desc: "Second",
+			request: &types.QueryGetSupplierRequest{
+				Index: msgs[1].Index,
+			},
+			response: &types.QueryGetSupplierResponse{Supplier: msgs[1]},
+		},
+		{
+			desc: "KeyNotFound",
+			request: &types.QueryGetSupplierRequest{
+				Index: strconv.Itoa(100000),
+			},
+			err: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc: "InvalidRequest",
+			err:  status.Error(codes.InvalidArgument, "invalid request"),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			response, err := keeper.Supplier(ctx, tc.request)
+			if tc.err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t,
+					nullify.Fill(tc.response),
+					nullify.Fill(response),
+				)
+			}
+		})
+	}
+}
+
+func TestSupplierQueryPaginated(t *testing.T) {
+	keeper, ctx := keepertest.SupplierKeeper(t)
+	msgs := createNSupplier(keeper, ctx, 5)
+
+	request := func(next []byte, offset, limit uint64, total bool) *types.QueryAllSupplierRequest {
+		return &types.QueryAllSupplierRequest{
+			Pagination: &query.PageRequest{
+				Key:        next,
+				Offset:     offset,
+				Limit:      limit,
+				CountTotal: total,
+			},
+		}
+	}
+	t.Run("ByOffset", func(t *testing.T) {
+		step := 2
+		for i := 0; i < len(msgs); i += step {
+			resp, err := keeper.SupplierAll(ctx, request(nil, uint64(i), uint64(step), false))
+			require.NoError(t, err)
+			require.LessOrEqual(t, len(resp.Supplier), step)
+			require.Subset(t,
+				nullify.Fill(msgs),
+				nullify.Fill(resp.Supplier),
+			)
+		}
+	})
+	t.Run("ByKey", func(t *testing.T) {
+		step := 2
+		var next []byte
+		for i := 0; i < len(msgs); i += step {
+			resp, err := keeper.SupplierAll(ctx, request(next, 0, uint64(step), false))
+			require.NoError(t, err)
+			require.LessOrEqual(t, len(resp.Supplier), step)
+			require.Subset(t,
+				nullify.Fill(msgs),
+				nullify.Fill(resp.Supplier),
+			)
+			next = resp.Pagination.NextKey
+		}
+	})
+	t.Run("Total", func(t *testing.T) {
+		resp, err := keeper.SupplierAll(ctx, request(nil, 0, 0, true))
+		require.NoError(t, err)
+		require.Equal(t, len(msgs), int(resp.Pagination.Total))
+		require.ElementsMatch(t,
+			nullify.Fill(msgs),
+			nullify.Fill(resp.Supplier),
+		)
+	})
+	t.Run("InvalidRequest", func(t *testing.T) {
+		_, err := keeper.SupplierAll(ctx, nil)
+		require.ErrorIs(t, err, status.Error(codes.InvalidArgument, "invalid request"))
+	})
+}
