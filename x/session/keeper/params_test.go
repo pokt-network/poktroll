3e8f7d2a4ae672f2aa5192b04e4b015b195d47d7
Redouane Lakrache <r3d0ne@gmail.com>
2024-02-15 00:38:53 +0100

    chore: remove diffs with main

--- x/session/keeper/params_test.go
+++ x/session/keeper/params_test.go
@@ -5,12 +5,12 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	keepertest "github.com/pokt-network/poktroll/testutil/keeper"
+	testkeeper "github.com/pokt-network/poktroll/testutil/keeper"
 	"github.com/pokt-network/poktroll/x/session/types"
 )
 
 func TestGetParams(t *testing.T) {
-	k, ctx := keepertest.SessionKeeper(t)
+	k, ctx := testkeeper.SessionKeeper(t)
 	params := types.DefaultParams()
 
 	require.NoError(t, k.SetParams(ctx, params))
