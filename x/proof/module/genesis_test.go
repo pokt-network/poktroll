5058ca4c5bfedf1677bb948dfb104c7d5daa8c57
Bryan White <bryanchriswhite@gmail.com>
2024-02-15 10:29:56 +0100

    scaffold: module proof --dep session

---
+++ x/proof/module/genesis_test.go
@@ -0,0 +1,29 @@
+package proof_test
+
+import (
+	"testing"
+
+	keepertest "github.com/pokt-network/poktroll/testutil/keeper"
+	"github.com/pokt-network/poktroll/testutil/nullify"
+	"github.com/pokt-network/poktroll/x/proof/module"
+	"github.com/pokt-network/poktroll/x/proof/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ProofKeeper(t)
+	proof.InitGenesis(ctx, k, genesisState)
+	got := proof.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/proof/types/keys.go
@@ -0,0 +1,20 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "proof"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// MemStoreKey defines the in-memory store key
+	MemStoreKey = "mem_proof"
+)
+
+var (
+	ParamsKey = []byte("p_proof")
+)
+
+func KeyPrefix(p string) []byte {
+	return []byte(p)
+}
