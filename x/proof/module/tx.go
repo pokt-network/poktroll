5d71471d62070898e551b460636d33fdf4863540
Bryan White <bryanchriswhite@gmail.com>
2024-02-15 16:46:42 +0100

    reconcile: create claim CLI command

---
+++ x/proof/module/tx.go
@@ -0,0 +1,27 @@
+package proof
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/spf13/cobra"
+
+	"github.com/pokt-network/poktroll/x/supplier/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func (am AppModule) GetTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(CmdCreateClaim())
+	// cmd.AddCommand(CmdSubmitProof())
+	// this line is used by starport scaffolding # 1
+
+	return cmd
+}
