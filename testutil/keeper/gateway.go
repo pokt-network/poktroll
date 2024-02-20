8f2a1b670ab453c40df831b69e16f01bba65a6b6
Redouane Lakrache <r3d0ne@gmail.com>
2024-02-20 17:47:04 +0100

    fixup! chore: gateway module review improvements

--- testutil/keeper/gateway.go
+++ testutil/keeper/gateway.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"context"
 	"testing"
 
 	"cosmossdk.io/log"
@@ -23,7 +24,8 @@ import (
 	"github.com/pokt-network/poktroll/x/gateway/types"
 )
 
-func GatewayKeeper(t testing.TB) (keeper.Keeper, sdk.Context) {
+func GatewayKeeper(t testing.TB) (keeper.Keeper, context.Context) {
+	t.Helper()
 	storeKey := storetypes.NewKVStoreKey(types.StoreKey)
 
 	db := dbm.NewMemDB()
--- x/gateway/module/genesis.go
+++ x/gateway/module/genesis.go
@@ -1,14 +1,14 @@
 package gateway
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"context"
 
 	"github.com/pokt-network/poktroll/x/gateway/keeper"
 	"github.com/pokt-network/poktroll/x/gateway/types"
 )
 
 // InitGenesis initializes the module's state from a provided genesis state.
-func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+func InitGenesis(ctx context.Context, k keeper.Keeper, genState types.GenesisState) {
 	// Set all the gateway
 	for _, gateway := range genState.GatewayList {
 		k.SetGateway(ctx, gateway)
@@ -18,7 +18,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 }
 
 // ExportGenesis returns the module's exported genesis.
-func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+func ExportGenesis(ctx context.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
 
