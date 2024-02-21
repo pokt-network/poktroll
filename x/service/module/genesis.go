a796fbfe56a2ef489d3074205d9fdcf6322dca04
Bryan White <bryanchriswhite@gmail.com>
2024-02-21 01:00:28 +0100

    fixup! chore: service module review improvements

--- x/service/module/genesis.go
+++ x/service/module/genesis.go
@@ -14,7 +14,9 @@ func InitGenesis(ctx context.Context, k keeper.Keeper, genState types.GenesisSta
 		k.SetService(ctx, service)
 	}
 	// this line is used by starport scaffolding # genesis/module/init
-	k.SetParams(ctx, genState.Params)
+	if err := k.SetParams(ctx, genState.Params); err != nil {
+		panic(err)
+	}
 }
 
 // ExportGenesis returns the module's exported genesis.
