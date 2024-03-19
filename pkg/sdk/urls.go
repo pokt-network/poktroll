9a335bdffb6e648304b57f43d59c755649692874
Eric Nielson <ezeike@gmail.com>
2024-03-19 10:33:30 -0400

    HostToWebsocketURL updated to handle https as well as http URLs

--- pkg/sdk/urls.go
+++ pkg/sdk/urls.go
@@ -1,10 +1,17 @@
 package sdk
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
 // HostToWebsocketURL converts the provided host into a websocket URL that can
 // be used to subscribe to onchain events and query the chain via a client
 // context or send transactions via a tx client context.
-func HostToWebsocketURL(host string) string {
-	return fmt.Sprintf("ws://%s/websocket", host)
+func HostToWebsocketURL(hostUrl *url.URL) string {
+	if hostUrl.Scheme == "https" {
+		return fmt.Sprintf("wss://%s/websocket", hostUrl.Host)
+	} else {
+		return fmt.Sprintf("ws://%s/websocket", hostUrl.Host)
+	}
 }
