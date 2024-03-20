86b2d81093511e6453a8670eac53d0feed0c8245
Eric Nielson <ezeike@gmail.com>
2024-03-20 12:45:10 -0400

    Updated urls_test

--- pkg/sdk/urls_test.go
+++ pkg/sdk/urls_test.go
@@ -5,72 +5,69 @@ import (
 	"testing"
 
 	"github.com/pokt-network/poktroll/pkg/sdk"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHostToWebsocketURL(t *testing.T) {
 	tests := []struct {
-		name    string
+		desc    string
 		hostUrl string
 		want    string
 	}{
 		{
-			name:    "Test HTTPS",
+			desc:    "Test HTTPS",
 			hostUrl: "https://poktroll.com",
 			want:    "wss://poktroll.com/websocket",
 		},
 		{
-			name:    "Test HTTP",
+			desc:    "Test HTTP",
 			hostUrl: "http://poktroll.com",
 			want:    "ws://poktroll.com/websocket",
 		},
 	}
 
 	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
+		t.Run(tc.desc, func(t *testing.T) {
 			u, _ := url.Parse(tc.hostUrl)
 			got := sdk.HostToWebsocketURL(u)
-			if got != tc.want {
-				t.Fatalf("expected %s; got %s", tc.want, got)
-			}
+			require.Equal(t, tc.want, got)
 		})
 	}
 }
 
 func TestHostToGRPCUrl(t *testing.T) {
 	tests := []struct {
-		name    string
+		desc    string
 		hostUrl string
 		want    string
 	}{
 		{
-			name:    "Test HTTPS",
+			desc:    "Test HTTPS",
 			hostUrl: "https://poktroll.com",
 			want:    "grpcs://poktroll.com",
 		},
 		{
-			name:    "Test with port",
+			desc:    "Test with port",
 			hostUrl: "https://poktroll.com:443",
 			want:    "grpcs://poktroll.com:443",
 		},
 		{
-			name:    "Test gRPCs",
+			desc:    "Test gRPCs",
 			hostUrl: "grpcs://poktroll.com",
 			want:    "grpcs://poktroll.com",
 		},
 		{
-			name:    "Test HTTP",
+			desc:    "Test HTTP",
 			hostUrl: "http://poktroll.com",
 			want:    "grpc://poktroll.com",
 		},
 	}
 
 	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
+		t.Run(tc.desc, func(t *testing.T) {
 			u, _ := url.Parse(tc.hostUrl)
 			got := sdk.HostToGRPCUrl(u)
-			if got != tc.want {
-				t.Fatalf("expected %s; got %s", tc.want, got)
-			}
+			require.Equal(t, tc.want, got)
 		})
 	}
 }
