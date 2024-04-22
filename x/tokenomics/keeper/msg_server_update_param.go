07219dec7524bc6efdbcdfd2f3be1aed874c5912
Bryan White <bryanchriswhite@gmail.com>
2024-04-22 13:38:14 +0200

    feat: implement tokenomics update param msg handler

--- x/tokenomics/keeper/msg_server_update_param.go
+++ x/tokenomics/keeper/msg_server_update_param.go
@@ -2,16 +2,36 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/pokt-network/poktroll/x/tokenomics/types"
 )
 
-func (k msgServer) UpdateParam(goCtx context.Context, msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+func (k msgServer) UpdateParam(ctx context.Context, msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
+	if k.GetAuthority() != msg.Authority {
+		return nil, types.ErrTokenomicsInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+	}
 
-	// TODO: Handling the message
-	_ = ctx
+	params := k.GetParams(ctx)
 
-	return &types.MsgUpdateParamResponse{}, nil
+	switch msg.Name {
+	case "compute_units_to_tokens_multiplier":
+		value, ok := msg.AsType.(*types.MsgUpdateParam_AsInt64)
+		if !ok {
+			return nil, fmt.Errorf("unsupported value type for %s param: %T", msg.Name, msg.AsType)
+		}
+
+		params.ComputeUnitsToTokensMultiplier = uint64(value.AsInt64)
+	default:
+		return nil, fmt.Errorf("unsupported param %q", msg.Name)
+	}
+
+	if err := k.SetParams(ctx, params); err != nil {
+		return nil, err
+	}
+
+	updatedParams := k.GetParams(ctx)
+	return &types.MsgUpdateParamResponse{
+		Params: &updatedParams,
+	}, nil
 }
