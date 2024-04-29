f165be13b41f3bb17a830f13f58d353a1699517b
Bryan White <bryanchriswhite@gmail.com>
2024-04-29 16:10:23 +0200

    chore: review feedback improvements & cleanup

--- e2e/tests/update_params.feature
+++ e2e/tests/update_params.feature
@@ -11,33 +11,33 @@ Feature: Params Namespace
     When the user sends an authz exec message to update all "tokenomics" module params
       | name                                 | value | type    |
       | "compute_units_to_tokens_multiplier" | "420" | "int64" |
-    # Then all "tokenomics" module params should be updated
-    #   | name                                 | value | type    |
-    #   | "compute_units_to_tokens_multiplier" | "420" | "int64" |
+     Then all "tokenomics" module params should be updated
+       | name                                 | value | type    |
+       | "compute_units_to_tokens_multiplier" | "420" | "int64" |
 
-  # Scenario: An authorized user updates all "proof" module params
-  #   Given the user has the pocketd binary installed
-  #   And all "proof" module params are set to their default values
-  #   And an authz grant from the "gov" "module" account to the "pnf" "user" account for the "/poktroll.proof.MsgUpdateParams" message
-  #   When the user sends an authz exec message to update all "tokenomics" module params
-  #     | name                        | value | type    |
-  #     | "min_relay_difficulty_bits" | "8"   | "int64" |
-  #   Then all "proof" module params should be updated
-  #     | name                        | value | type    |
-  #     | "min_relay_difficulty_bits" | "8"   | "int64" |
+   Scenario: An authorized user updates all "proof" module params
+     Given the user has the pocketd binary installed
+     And all "proof" module params are set to their default values
+     And an authz grant from the "gov" "module" account to the "pnf" "user" account for the "/poktroll.proof.MsgUpdateParams" message
+     When the user sends an authz exec message to update all "tokenomics" module params
+       | name                        | value | type    |
+       | "min_relay_difficulty_bits" | "8"   | "int64" |
+     Then all "proof" module params should be updated
+       | name                        | value | type    |
+       | "min_relay_difficulty_bits" | "8"   | "int64" |
 
-  # Scenario Outline: An authorized user updates individual <module> module params
-  #   Given the user has the pocketd binary installed
-  #   And all <module> module params are set to their default values
-  #   And an authz grant from the <granter> "module" account to the <grantee> "user" account for the <message_type> message
-  #   When the user sends an authz exec message to update <module> module param <param_name>
-  #     | value         | type         |
-  #     | <param_value> | <param_type> |
-  #   Then the <module> module param <param_name> should be updated
-  #     | value         | type         |
-  #     | <param_value> | <param_type> |
+   Scenario Outline: An authorized user updates individual <module> module params
+     Given the user has the pocketd binary installed
+     And all <module> module params are set to their default values
+     And an authz grant from the <granter> "module" account to the <grantee> "user" account for the <message_type> message
+     When the user sends an authz exec message to update <module> module param <param_name>
+       | value         | type         |
+       | <param_value> | <param_type> |
+     Then the <module> module param <param_name> should be updated
+       | value         | type         |
+       | <param_value> | <param_type> |
 
-  #   Examples:
-  #     | module       | granter | grantee | message_type                          | param_name                           | param_value | param_type |
-  #     | "tokenomics" | "gov"   | "pnf"   | "/poktroll.tokenomics.MsgUpdateParam" | "compute_units_to_tokens_multiplier" | "69"        | "int64"    |
-  #     | "proof"      | "gov"   | "pnf"   | "/poktroll.proof.MsgUpdateParam"      | "min_relay_difficulty_bits"          | "12"        | "int64"    |
+     Examples:
+       | module       | granter | grantee | message_type                          | param_name                           | param_value | param_type |
+       | "tokenomics" | "gov"   | "pnf"   | "/poktroll.tokenomics.MsgUpdateParam" | "compute_units_to_tokens_multiplier" | "69"        | "int64"    |
+       | "proof"      | "gov"   | "pnf"   | "/poktroll.proof.MsgUpdateParam"      | "min_relay_difficulty_bits"          | "12"        | "int64"    |
--- e2e/tests/update_params_test.go
+++ e2e/tests/update_params_test.go
@@ -4,6 +4,7 @@ package e2e
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -11,15 +12,40 @@ import (
 	"text/template"
 
 	cometcli "github.com/cometbft/cometbft/libs/cli"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
 	"github.com/cosmos/gogoproto/proto"
 	"github.com/regen-network/gocuke"
 	"github.com/stretchr/testify/require"
 
+	"github.com/pokt-network/poktroll/testutil/testclient"
 	prooftypes "github.com/pokt-network/poktroll/x/proof/types"
 	tokenomicstypes "github.com/pokt-network/poktroll/x/tokenomics/types"
 )
 
+// authzCLIGrantResponse is the JSON response struct for the authz grants query CLI subcommand.
+type authzCLIGrantResponse struct {
+	Grants []struct {
+		Authorization struct {
+			Type  string `json:"type"`
+			Value struct {
+				Msg string `json:"msg"`
+			} `json:"value"`
+		} `json:"authorization"`
+	} `json:"grants"`
+}
+
+// txTemplateLocals is the template locals struct used to populate the
+// updateParamsTxJSONTemplate template.
+type txTemplateLocals struct {
+	Type      string
+	Authority string
+	Params    string
+}
+
+// updateParamsTxJSONTemplate is a text template for a tx JSON file which is
+// intended to be used with the `authz exec` CLI subcommand.
 var updateParamsTxJSONTemplate = template.Must(
 	template.
 		New("txJSON").
@@ -37,6 +63,8 @@ var updateParamsTxJSONTemplate = template.Must(
 `))
 
 func (s *suite) AllModuleParamsAreSetToTheirDefaultValues(moduleName string) {
+	s.Skip("TODO_TEST: complete step definitions for update_params.feature scenarios")
+
 	argsAndFlags := []string{
 		"query",
 		moduleName,
@@ -93,22 +121,12 @@ func (s *suite) AnAuthzGrantFromTheAccountToTheAccountForTheMessage(granterName,
 	res, err := s.pocketd.RunCommandOnHost("", argsAndFlags...)
 	require.NoError(s, err)
 
+	// Unmarshal the JSON response into the grantsRes struct.
 	// Using s.cdc.UnmarshalJSON() with *authz.QueryGrantsResponse complains about
 	// the "type" field, expecting "@type" for the #Grants[n]#Authorization pb.Any.
-	var grantsRes struct {
-		Grants []struct {
-			Authorization struct {
-				Type  string `json:"type"`
-				Value struct {
-					Msg string `json:"msg"`
-				} `json:"value"`
-			} `json:"authorization"`
-		} `json:"grants"`
-	}
-
-	// Unmarshal the JSON response into the grantsRes struct.
+	grantsRes := new(authzCLIGrantResponse)
 	responseBz := []byte(strings.TrimSpace(res.Stdout))
-	err = json.Unmarshal(responseBz, &grantsRes)
+	err = json.Unmarshal(responseBz, grantsRes)
 	require.NoError(s, err)
 
 	// Check if the expected grant is found in the response.
@@ -128,58 +146,26 @@ func (s *suite) AnAuthzGrantFromTheAccountToTheAccountForTheMessage(granterName,
 }
 
 func (s *suite) TheUserSendsAnAuthzExecMessageToUpdateAllModuleParams(moduleName string, table gocuke.DataTable) {
+	ctx := context.Background()
+
 	// Convert the text table to a map of param names to their types & values.
 	paramsMap := s.parseParamsTable(table)
 
-	// Convert the params map to a MsgUpdateParams message & JSON.
-	msgUpdateParams, paramsJSON := s.paramsMapToMsgUpdateParams(moduleName, paramsMap)
-
-	locals := struct{ Type, Authority, Params string }{
-		Type:      proto.MessageName(msgUpdateParams),
-		Authority: authtypes.NewModuleAddress(s.granterName).String(),
-		Params:    string(paramsJSON),
-	}
-	buf := new(bytes.Buffer)
-	require.NoError(s, updateParamsTxJSONTemplate.Execute(buf, locals))
-
-	tempFile, err := os.CreateTemp("", "exec.json")
-	require.NoError(s, err)
-	s.Cleanup(func() {
-		os.Remove(tempFile.Name())
-	})
-
-	// TODO_IN_THIS_PR: reset the module params to their default values using t.Cleanup()
-	// here so that the test can be run multiple times per localnet boot
-	// (i.e. doesn't require localnet restart).
-
-	// TODO_IN_THIS_PR: find a better and/or more conventional way to programmatically generate tx JSON containing pb.Any messages.
-	// - 👍 s.cdc.MarshalJSON() converts the pb.Any message to a JSON object with a "@type" field.
-	// - 👎 s.cdc.MarshalJSON() the value of the "@type" field is missing a preceding "/" that authz exec CLI is expecting.
-	// - 👎 EOF error currently when calling authz exec CLI with the generated JSON.
-	// - 👎 s.cdc.MarshalJSON() can only be applied to proto.Message implementations (i.e. protobuf types).
-	// - ? is there a tx-level object that can be used to serialize & which can be constructed in this scope?
-	// - 👎 otherwise, must use a template for the tx JSON structure (s.cdc.MarshalJSON() can't be used on a tx.Tx object).
-
-	replacedJSON := bytes.Replace(buf.Bytes(), []byte(`"@type": "`), []byte(`"@type": "/`), 1)
-	_, err = tempFile.Write(replacedJSON)
-	require.NoError(s, err)
-
-	s.Log("tempfile:")
-	fileOut, err := os.ReadFile(tempFile.Name())
-	require.NoError(s, err)
-	s.Log(string(fileOut))
+	// Use the map of params to populate a tx JSON template & write it to a file.
+	txJSONPath := s.newTempTxJSONFile(moduleName, paramsMap)
 
 	argsAndFlags := []string{
 		"tx", "authz", "exec",
-		tempFile.Name(),
+		txJSONPath,
 		"--from", s.granteeName,
 		keyRingFlag,
 		fmt.Sprintf("--%s=json", cometcli.OutputFlag),
 	}
-	res, err := s.pocketd.RunCommandOnHost("", argsAndFlags...)
+	_, err := s.pocketd.RunCommandOnHost("", argsAndFlags...)
 	require.NoError(s, err)
 
-	s.Log(res.Stdout)
+	// Reset all module params to their default values after the test completes.
+	s.Cleanup(func() { s.resetAllModuleParamsToDefaults(ctx) })
 }
 
 func (s *suite) AllModuleParamsShouldBeUpdated(moduleName string, table gocuke.DataTable) {
@@ -281,5 +267,74 @@ func (s *suite) paramsMapToMsgUpdateParams(moduleName string, paramsMap map[stri
 
 	default:
 		s.Fatalf("unexpected module name %q", moduleName)
+		return nil, nil
 	}
+}
+
+// newTempTxJSONFile creates a new temp file with the JSON representation of a tx,
+// intended for use with the `authz exec` CLI subcommand. It returns the file path.
+func (s *suite) newTempTxJSONFile(moduleName string, paramsMap map[string]anyMap) string {
+	buf := new(bytes.Buffer)
+	locals := s.newTxTemplateLocals(moduleName, paramsMap)
+	require.NoError(s, updateParamsTxJSONTemplate.Execute(buf, locals))
+
+	tempFile, err := os.CreateTemp("", "exec.json")
+	require.NoError(s, err)
+
+	// Remove tempFile when the test completes.
+	s.Cleanup(func() {
+		os.Remove(tempFile.Name())
+	})
+
+	// TODO_IMPROVE: find a better and/or more conventional way to programmatically generate tx JSON containing pb.Any messages.
+	// - 👍 s.cdc.MarshalJSON() converts the pb.Any message to a JSON object with a "@type" field.
+	// - 👎 s.cdc.MarshalJSON() the value of the "@type" field is missing a preceding "/" that authz exec CLI is expecting.
+	// - 👎 EOF error currently when calling authz exec CLI with the generated JSON.
+	// - 👎 s.cdc.MarshalJSON() can only be applied to proto.Message implementations (i.e. protobuf types).
+	// - ? is there a tx-level object that can be used to serialize & which can be constructed in this scope?
+	// - 👎 otherwise, must use a template for the tx JSON structure (s.cdc.MarshalJSON() can't be used on a tx.Tx object).
+
+	replacedJSON := bytes.Replace(buf.Bytes(), []byte(`"@type": "`), []byte(`"@type": "/`), 1)
+	_, err = tempFile.Write(replacedJSON)
+	require.NoError(s, err)
+
+	return tempFile.Name()
+}
+
+func (s *suite) newTxTemplateLocals(moduleName string, paramsMap map[string]anyMap) *txTemplateLocals {
+	// Convert the params map to a MsgUpdateParams message & JSON.
+	msgUpdateParams, paramsJSON := s.paramsMapToMsgUpdateParams(moduleName, paramsMap)
+
+	return &txTemplateLocals{
+		Type:      proto.MessageName(msgUpdateParams),
+		Authority: authtypes.NewModuleAddress(s.granterName).String(),
+		Params:    string(paramsJSON),
+	}
+}
+
+// resetAllModuleParamsToDefaults resets all module params to their default values using
+// a single authz exec message. It blocks until the resulting tx has been committed.
+func (s *suite) resetAllModuleParamsToDefaults(ctx context.Context) {
+	flagSet := testclient.NewLocalnetFlagSet(s)
+	clientCtx := testclient.NewLocalnetClientCtx(s, flagSet)
+	authzClient := authz.NewMsgClient(clientCtx)
+
+	proofDefaultParams := prooftypes.DefaultParams()
+	proofDefaultParamsAny, err := codectypes.NewAnyWithValue(&proofDefaultParams)
+	require.NoError(s, err)
+
+	tokenomicsDefaultParams := tokenomicstypes.DefaultParams()
+	tokenomicsDefaultParamsAny, err := codectypes.NewAnyWithValue(&tokenomicsDefaultParams)
+	require.NoError(s, err)
+
+	msgExec := &authz.MsgExec{
+		Grantee: authtypes.NewModuleAddress(s.granteeName).String(),
+		Msgs: []*codectypes.Any{
+			proofDefaultParamsAny,
+			tokenomicsDefaultParamsAny,
+		},
+	}
+
+	_, err = authzClient.Exec(ctx, msgExec)
+	require.NoError(s, err)
 }
