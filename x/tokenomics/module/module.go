1f48fe1eba9a5775ed607f539fa4f462fa4f7aca
Daniel Olshansky <olshansky.daniel@gmail.com>
2024-05-27 15:06:58 -0700

    Fix error type

--- x/tokenomics/module/module.go
+++ x/tokenomics/module/module.go
@@ -74,7 +74,7 @@ func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
 	var genState types.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &genState); err != nil {
-		return types.ErrTokenomicsUnmarshalInvalid.Wrapf("invalid genesis state: %w", err)
+		return types.ErrTokenomicsUnmarshalInvalid.Wrapf("invalid genesis state: %v", err)
 	}
 	return genState.Validate()
 }
