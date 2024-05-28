244ce0bcd408f44faa74a785a590d5d1cf72c1d4
Bryan White <bryanchriswhite@gmail.com>
2024-05-28 17:32:41 +0200

    refactor: relayerSessionsManager to use ClaimWindowOpenOffsetBlocks param

--- testutil/testclient/testqueryclients/sharedquerier.go
+++ testutil/testclient/testqueryclients/sharedquerier.go
@@ -1,15 +1,18 @@
 package testqueryclients
 
 import (
+	"context"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 
 	"github.com/pokt-network/poktroll/testutil/mockclient"
+	"github.com/pokt-network/poktroll/x/shared"
 	sharedtypes "github.com/pokt-network/poktroll/x/shared/types"
 )
 
-// NewTestSharedQueryClient creates a mock of the SharedQueryClient.
+// NewTestSharedQueryClient creates a mock of the SharedQueryClient which uses the
+// default shared module params for its implementation.
 func NewTestSharedQueryClient(
 	t *testing.T,
 ) *mockclient.MockSharedQueryClient {
@@ -18,8 +21,19 @@ func NewTestSharedQueryClient(
 	sharedQuerier := mockclient.NewMockSharedQueryClient(ctrl)
 	params := sharedtypes.DefaultParams()
 
-	sharedQuerier.EXPECT().GetParams(gomock.Any()).
+	sharedQuerier.EXPECT().
+		GetParams(gomock.Any()).
 		Return(&params, nil).
+		AnyTimes()
+
+	sharedQuerier.EXPECT().
+		GetClaimWindowOpenHeight(gomock.Any(), gomock.Any()).
+		DoAndReturn(
+			func(ctx context.Context, queryHeight int64) (int64, error) {
+				sharedParams := sharedtypes.DefaultParams()
+				return shared.GetClaimWindowOpenHeight(&sharedParams, queryHeight), nil
+			},
+		).
 		AnyTimes()
 
 	return sharedQuerier
