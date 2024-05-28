55ba107f28babb8a8d0edfe6e20f6aa2873ff06b
Bryan White <bryanchriswhite@gmail.com>
2024-05-28 14:35:10 +0200

    scaffold: message update-param --module shared --signer authority name as_type --response params

--- x/shared/module/autocli.go
+++ x/shared/module/autocli.go
@@ -28,6 +28,12 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					RpcMethod: "UpdateParams",
 					Skip:      true, // skipped because authority gated
 				},
+				{
+					RpcMethod:      "UpdateParam",
+					Use:            "update-param [name] [as-type]",
+					Short:          "Send a update-param tx",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "name"}, {ProtoField: "asType"}},
+				},
 				// this line is used by ignite scaffolding # autocli/tx
 			},
 		},
