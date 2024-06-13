bb366df3a45685dbb12d64642e50de4aeb4d2b23
Dmitry K. <okdas@pm.me>
2024-06-13 16:40:42 -0700

    more adjustments after moving things around

--- pkg/client/supplier/types.go
+++ pkg/client/supplier/types.go
@@ -1,14 +1,16 @@
 package supplier
 
+import "github.com/pokt-network/poktroll/pkg/client"
+
 // SupplierClientMap is a helper struct needed to depinject multiple supplier clients.
 // The inner structure maps a supplier address to a list of supplier clients for that address.
 // Must be a type to successfully work with depinject.
 type SupplierClientMap struct {
-	SupplierClients map[string]SupplierClient
+	SupplierClients map[string]client.SupplierClient
 }
 
 func NewSupplierClientMap() *SupplierClientMap {
-	m := make(map[string]SupplierClient)
+	m := make(map[string]client.SupplierClient)
 	return &SupplierClientMap{
 		SupplierClients: m,
 	}
