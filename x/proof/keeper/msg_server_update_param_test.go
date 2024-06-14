e29a88b64f6fa4d6e0f6008fd26535d3dbaa0a34
Bryan White <bryanchriswhite@gmail.com>
2024-06-14 09:54:48 +0200

    [Proof Module] Add proof_missing_penalty param (#596)
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- x/proof/keeper/msg_server_update_param_test.go
+++ x/proof/keeper/msg_server_update_param_test.go
@@ -3,10 +3,13 @@ package keeper_test
 import (
 	"testing"
 
+	"cosmossdk.io/math"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/stretchr/testify/require"
 
+	"github.com/pokt-network/poktroll/app/volatile"
 	prooftypes "github.com/pokt-network/poktroll/x/proof/types"
 )
 
@@ -30,7 +33,12 @@ func TestMsgUpdateParam_UpdateMinRelayDifficultyBitsOnly(t *testing.T) {
 	res, err := msgSrv.UpdateParam(ctx, updateParamMsg)
 	require.NoError(t, err)
 
+	require.NotEqual(t, defaultParams.MinRelayDifficultyBits, res.Params.MinRelayDifficultyBits)
 	require.Equal(t, expectedMinRelayDifficultyBits, res.Params.MinRelayDifficultyBits)
+
+	require.Equal(t, defaultParams.ProofRequirementThreshold, res.Params.ProofRequirementThreshold)
+	require.Equal(t, defaultParams.ProofRequestProbability, res.Params.ProofRequestProbability)
+	require.Equal(t, defaultParams.ProofMissingPenalty, res.Params.ProofMissingPenalty)
 }
 
 func TestMsgUpdateParam_UpdateProofRequestProbabilityOnly(t *testing.T) {
@@ -53,7 +61,12 @@ func TestMsgUpdateParam_UpdateProofRequestProbabilityOnly(t *testing.T) {
 	res, err := msgSrv.UpdateParam(ctx, updateParamMsg)
 	require.NoError(t, err)
 
+	require.NotEqual(t, defaultParams.ProofRequestProbability, res.Params.ProofRequestProbability)
 	require.Equal(t, expectedProofRequestProbability, res.Params.ProofRequestProbability)
+
+	require.Equal(t, defaultParams.ProofRequirementThreshold, res.Params.ProofRequirementThreshold)
+	require.Equal(t, defaultParams.MinRelayDifficultyBits, res.Params.MinRelayDifficultyBits)
+	require.Equal(t, defaultParams.ProofMissingPenalty, res.Params.ProofMissingPenalty)
 }
 
 func TestMsgUpdateParam_UpdateProofRequirementThresholdOnly(t *testing.T) {
@@ -76,5 +89,38 @@ func TestMsgUpdateParam_UpdateProofRequirementThresholdOnly(t *testing.T) {
 	res, err := msgSrv.UpdateParam(ctx, updateParamMsg)
 	require.NoError(t, err)
 
+	require.NotEqual(t, defaultParams.ProofRequirementThreshold, res.Params.ProofRequirementThreshold)
 	require.Equal(t, expectedProofRequirementThreshold, res.Params.ProofRequirementThreshold)
+
+	require.Equal(t, defaultParams.ProofMissingPenalty, res.Params.ProofMissingPenalty)
+	require.Equal(t, defaultParams.ProofRequestProbability, res.Params.ProofRequestProbability)
+	require.Equal(t, defaultParams.MinRelayDifficultyBits, res.Params.MinRelayDifficultyBits)
+}
+
+func TestMsgUpdateParam_UpdateProofMissingPenaltyOnly(t *testing.T) {
+	expectedProofMissingPenalty := cosmostypes.NewCoin(volatile.DenomuPOKT, math.NewInt(500))
+
+	// Set the parameters to their default values
+	k, msgSrv, ctx := setupMsgServer(t)
+	defaultParams := prooftypes.DefaultParams()
+	require.NoError(t, k.SetParams(ctx, defaultParams))
+
+	// Ensure the default values are different from the new values we want to set
+	require.NotEqual(t, expectedProofMissingPenalty, defaultParams.ProofMissingPenalty)
+
+	// Update the proof request probability
+	updateParamMsg := &prooftypes.MsgUpdateParam{
+		Authority: authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+		Name:      prooftypes.ParamProofMissingPenalty,
+		AsType:    &prooftypes.MsgUpdateParam_AsCoin{AsCoin: &expectedProofMissingPenalty},
+	}
+	res, err := msgSrv.UpdateParam(ctx, updateParamMsg)
+	require.NoError(t, err)
+
+	require.NotEqual(t, defaultParams.ProofMissingPenalty, res.Params.ProofMissingPenalty)
+	require.Equal(t, &expectedProofMissingPenalty, res.Params.ProofMissingPenalty)
+
+	require.Equal(t, defaultParams.ProofRequirementThreshold, res.Params.ProofRequirementThreshold)
+	require.Equal(t, defaultParams.ProofRequestProbability, res.Params.ProofRequestProbability)
+	require.Equal(t, defaultParams.MinRelayDifficultyBits, res.Params.MinRelayDifficultyBits)
 }
