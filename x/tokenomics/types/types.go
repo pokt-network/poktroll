b8a2edc99ae235e9fd4857250041e10d51106f2f
Bryan White <bryanchriswhite@gmail.com>
2024-07-02 09:17:21 +0200

    refactor: pending claims result & increment claim relays counter

--- x/tokenomics/types/types.go
+++ x/tokenomics/types/types.go
@@ -1,3 +1,16 @@
 package types
 
 // This file is in place to declare the package for dynamically generated protobufs
+
+type PendingClaimsResult struct {
+	NumClaims           uint64
+	NumComputeUnits     uint64
+	NumRelays           uint64
+	RelaysPerServiceMap map[string]uint64
+}
+
+func NewClaimSettlementResult() PendingClaimsResult {
+	return PendingClaimsResult{
+		RelaysPerServiceMap: make(map[string]uint64),
+	}
+}
