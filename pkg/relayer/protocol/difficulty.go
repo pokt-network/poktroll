5fd53f8651d9267b696d9d834616067901586cb5
Bryan White <bryanchriswhite@gmail.com>
2024-07-04 12:57:32 +0200

    chore: improve comments

--- pkg/relayer/protocol/difficulty.go
+++ pkg/relayer/protocol/difficulty.go
@@ -8,10 +8,10 @@ import (
 // CountHashDifficultyBits returns the number of leading zero bits in the given byte
 // slice. It returns an error if the byte slice is all zero bits.
 func CountHashDifficultyBits(bz [32]byte) int {
-	// Using BigEndian for  consistent bit/byte ordering such leading zeros
+	// Using BigEndian for contiguous bit/byte ordering such leading zeros
 	// accumulate across adjacent bytes.
-	// E.g.: 0x00, 0x255, 0x00, 0x00 has 8 leading zero bits. If LittleEndian
-	// were applied instead, it would have 16 leading zeros because it would
-	// look like 0x00, 0x00, 0x255, 0x00.
+	// E.g.: []byte{0, 0b00111111, 0x00, 0x00} has 10 leading zero bits. If
+	// LittleEndian were applied instead, it would have 18 leading zeros because it would
+	// look like []byte{0, 0, 0b00111111, 0}.
 	return bits.LeadingZeros64(binary.BigEndian.Uint64(bz[:]))
 }
