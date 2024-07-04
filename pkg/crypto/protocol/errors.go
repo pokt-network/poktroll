85251bf105814321929eadec9a2f1220bbd14531
Bryan White <bryanchriswhite@gmail.com>
2024-07-04 13:43:43 +0200

    fixup: refactor: CountHashDifficultyBits()

--- pkg/crypto/protocol/errors.go
+++ pkg/crypto/protocol/errors.go
@@ -1,8 +1,5 @@
 package protocol
 
-import sdkerrors "cosmossdk.io/errors"
-
 var (
-	ErrDifficulty = sdkerrors.New(codespace, 1, "difficulty error")
-	codespace     = "relayer/protocol"
+	codespace = "relayer/protocol"
 )
