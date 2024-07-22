304fc70b5a7f969bd02a7fd0c67dc169a6f8fce7
Redouane Lakrache <r3d0ne@gmail.com>
2024-07-22 19:03:50 +0200

    chore: Add IsUnbonding and IsActive godoc

--- x/shared/types/supplier.go
+++ x/shared/types/supplier.go
@@ -4,10 +4,17 @@ package types
 // supplier is not actively in the unbonding period.
 const SupplierNotUnstaking uint64 = 0
 
+// IsUnbonding returns whether the supplier has submitted an unstake message,
+// in which case the supplier has an UnstakeSessionEndHeight set.
 func (s *Supplier) IsUnbonding() bool {
 	return s.UnstakeSessionEndHeight != SupplierNotUnstaking
 }
 
+// IsActive returns whether the supplier is allowed to serve requests at the
+// given query height.
+// A supplier that has not submitted an unstake message is always active.
+// A supplier that has submitted an unstake message is active until the end of
+// the session containing the height at which unstake message was submitted.
 func (s *Supplier) IsActive(queryHeight int64) bool {
 	return !s.IsUnbonding() || uint64(queryHeight) <= s.UnstakeSessionEndHeight
 }
