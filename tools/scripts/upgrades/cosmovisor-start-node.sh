7a81394e68a274d4db4f47f9f1361e862bec3726
Dmitry K. <okdas@pm.me>
2024-08-02 15:45:07 -0700

    change echo statement

--- tools/scripts/upgrades/cosmovisor-start-node.sh
+++ tools/scripts/upgrades/cosmovisor-start-node.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-description="This script is intended to run the full node using cosmovisor, and perform an upgrade after the upgrade plan is \
-submitted on chain. It is helpful to simulate a real network upgrade. If the upgrade includes a consensus-breaking \
-change, make sure the 'old' binary does not have these changes (so the real upgrade can be simulated)."
+cat << EOF
+This script runs the full node using cosmovisor and performs an upgrade after the upgrade plan is submitted on chain. 
+It simulates a real network upgrade. For consensus-breaking changes, ensure the 'old' binary doesn't have these changes.
 
-echo "$description"
+Pre-requisites:
+1. 'Old' binary
+2. 'New' binary
+3. Cosmovisor (Install: 'go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.5.0')
+   Documentation: https://docs.cosmos.network/main/build/tooling/cosmovisor
+4. \`upgrade.Upgrade\` with matching \`POKTROLLD_UPGRADE_PLAN_NAME\` created and included in the new version
+5. LocalNet turned off
+EOF
 
-echo -e "\nPre-requisites:"
-echo "1. 'Old' binary."
-echo "2. 'New' binary."
-echo "3. Cosmovisor. Install with 'go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.5.0'"
-echo "   Documentation: https://docs.cosmos.network/main/build/tooling/cosmovisor"
-echo "4. The \`upgrade.Upgrade\` with matching \`POKTROLLD_UPGRADE_PLAN_NAME\` should be created and included in the new version."
-echo "5. The LocalNet should be turned off."
+# Define paths and upgrade name
+POKTROLLD_OLD_BINARY_PATH=$HOME/pocket/pokrtoll-for-releases/poktrolld
+POKTROLLD_NEW_BINARY_PATH=$HOME/pocket/poktroll/poktrolld
+POKTROLLD_UPGRADE_PLAN_NAME=v0.0.4
 
-echo -e "\nMake sure to adjust POKTROLLD_OLD_BINARY_PATH, POKTROLLD_NEW_BINARY_PATH and POKTROLLD_UPGRADE_PLAN_NAME in the script."
-echo "Binaries can be built using 'ignite chain build' command and moved to a directory not included in git."
+cat << EOF
+
+The script will use the following:
+Old binary: $POKTROLLD_OLD_BINARY_PATH
+New binary: $POKTROLLD_NEW_BINARY_PATH
+Upgrade plan name: $POKTROLLD_UPGRADE_PLAN_NAME
+
+EOF
 
 read -p "Do you want to continue? (y/n): " answer
 if [[ $answer != "y" ]]; then
@@ -23,17 +33,11 @@ if [[ $answer != "y" ]]; then
     exit 0
 fi
 
-# Paths to pre-built binaries.
-export POKTROLLD_OLD_BINARY_PATH=$HOME/pocket/pokrtoll-for-releases/poktrolld
-export POKTROLLD_NEW_BINARY_PATH=$HOME/pocket/poktroll/poktrolld
-export POKTROLLD_UPGRADE_PLAN_NAME=v0.0.4
-
-export DAEMON_HOME=$HOME/.poktroll # `localnet_regenesis` creates new genesis in this directory by default.
-
 # Cosmovisor settings:
 export DAEMON_ALLOW_DOWNLOAD_BINARIES=false 
 export DAEMON_RESTART_AFTER_UPGRADE=true
 export DAEMON_NAME=poktrolld 
+export DAEMON_HOME=$HOME/.poktroll # `localnet_regenesis` creates new genesis in this directory by default.
 
 # Cleans up old upgrade binary and home dir.
 rm -rf $DAEMON_HOME
