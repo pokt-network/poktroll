9f38fb1fa1c6b9650fcec581d77b20d519384742
Bryan White <bryanchriswhite@gmail.com>
2024-09-16 10:53:31 +0200

    chore: add error assertion run option to integration app

--- testutil/integration/options.go
+++ testutil/integration/options.go
@@ -1,13 +1,29 @@
 package integration
 
+var (
+	RunUntilNextBlockOpts = RunOptions{
+		WithAutomaticCommit(),
+		WithAutomaticFinalizeBlock(),
+	}
+)
+
 // RunConfig is the configuration for the testing integration app.
 type RunConfig struct {
 	AutomaticFinalizeBlock bool
 	AutomaticCommit        bool
+	ErrorAssertion         func(error)
 }
 
 // RunOption is a function that can be used to configure the integration app.
 type RunOption func(*RunConfig)
+
+// RunOptions is a list of RunOption. It implements the Append method for convenience.
+type RunOptions []RunOption
+
+// Append returns a new RunOptions with the given RunOptions appended.
+func (runOpts RunOptions) Append(newRunOpts ...RunOption) RunOptions {
+	return append(runOpts, newRunOpts...)
+}
 
 // WithAutomaticFinalizeBlock calls ABCI finalize block.
 func WithAutomaticFinalizeBlock() RunOption {
@@ -21,5 +37,13 @@ func WithAutomaticFinalizeBlock() RunOption {
 func WithAutomaticCommit() RunOption {
 	return func(cfg *RunConfig) {
 		cfg.AutomaticCommit = true
+	}
+}
+
+// WithErrorAssertion registers an error assertion function which is called when
+// RunMsg() encounters an error.
+func WithErrorAssertion(errAssertFn func(error)) RunOption {
+	return func(cfg *RunConfig) {
+		cfg.ErrorAssertion = errAssertFn
 	}
 }
