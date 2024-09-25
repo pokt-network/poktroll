2a9d91b4bddac794886c42cf75ad4251f62daa95
Bryan White <bryanchriswhite@gmail.com>
2024-09-25 16:05:29 +0200

    [Testing] refractor: param update E2E tests as integration tests (#826)
    ## Summary
    
    Ports the `MsgUpdateParams` and `MsgUpdateParam` E2E tests to
    integration tests, improving execution speed and maintainability.
    
    ## Depends on
    
    - #827 
    - #820 
    - #821
    - #815
    
    ## Dependents
    
    - #809 
    
    ## Issue
    
    - #799
    
    ## Type of change
    
    Select one or more from the following:
    
    - [ ] New feature, functionality or library
    - [ ] Consensus breaking; add the `consensus-breaking` label if so. See
    #791 for details
    - [ ] Bug fix
    - [x] Code health or cleanup
    - [ ] Documentation
    - [ ] Other (specify)
    
    ## Testing
    
    - [x] **Documentation**: `make docusaurus_start`; only needed if you
    make doc changes
    - [ ] **Unit Tests**: `make go_develop_and_test`
    - [ ] **LocalNet E2E Tests**: `make test_e2e`
    - [x] **DevNet E2E Tests**: Add the `devnet-test-e2e` label to the PR.
    
    ## Sanity Checklist
    
    - [x] I have tested my changes using the available tooling
    - [x] I have commented my code
    - [x] I have performed a self-review of my own code; both comments &
    source code
    - [x] I create and reference any new tickets, if applicable
    - [x] I have left TODOs throughout the codebase, if applicable
    
    ---------
    
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    Co-authored-by: red-0ne <red-0ne@users.noreply.github.com>

---
+++ tests/integration/params/update_param_test.go
@@ -0,0 +1,142 @@
+package params
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/pokt-network/poktroll/testutil/integration/suites"
+)
+
+// msgUpdateParamSuite is a test suite which exercises the MsgUpdateParam message
+// for each poktroll module via authz, as would be done in a live network in order
+// to update **individual** parameter values for a given module.
+// NB: Not to be confused with MsgUpdateParams (plural), which updates all parameter
+// values for a module.
+type msgUpdateParamSuite struct {
+	suites.ParamsSuite
+
+	unauthorizedAddr cosmostypes.AccAddress
+}
+
+func TestUpdateParamSuite(t *testing.T) {
+	suite.Run(t, new(msgUpdateParamSuite))
+}
+
+func (s *msgUpdateParamSuite) SetupSubTest() {
+	// Create a fresh integration app for each test.
+	s.NewApp(s.T())
+
+	// Initialize the test accounts and create authz grants.
+	s.SetupTestAuthzAccounts()
+	s.SetupTestAuthzGrants()
+
+	// Allocate an address for unauthorized user.
+	nextAcct, ok := s.GetApp().GetPreGeneratedAccounts().Next()
+	require.True(s.T(), ok, "insufficient pre-generated accounts available")
+	s.unauthorizedAddr = nextAcct.Address
+}
+
+func (s *msgUpdateParamSuite) TestUnauthorizedMsgUpdateParamFails() {
+	for _, moduleName := range suites.MsgUpdateParamEnabledModuleNames {
+		moduleCfg := suites.ModuleParamConfigMap[moduleName]
+
+		// Iterate over each field in the current module's MsgUpdateParam, for each
+		// field, send a new MsgUpdateParam which would update the corresponding param
+		// to that field's value.
+		validParamsValue := reflect.ValueOf(moduleCfg.ValidParams)
+		for fieldIdx := 0; fieldIdx < validParamsValue.NumField(); fieldIdx++ {
+			fieldValue := validParamsValue.Field(fieldIdx)
+			fieldName := validParamsValue.Type().Field(fieldIdx).Name
+
+			testName := fmt.Sprintf("%s_%s", moduleName, fieldName)
+			s.T().Run(testName, func(t *testing.T) {
+				// Reset the app state in order to assert that each module
+				// param is updated correctly.
+				s.SetupSubTest()
+
+				// Assert that the module's params are set to their default values.
+				s.RequireModuleHasDefaultParams(t, moduleName)
+
+				updateResBz, err := s.RunUpdateParamAsSigner(t,
+					moduleName,
+					fieldName,
+					fieldValue.Interface(),
+					s.unauthorizedAddr,
+				)
+				require.ErrorContains(t, err, authz.ErrNoAuthorizationFound.Error())
+				require.Nil(t, updateResBz)
+			})
+		}
+	}
+}
+
+func (s *msgUpdateParamSuite) TestAuthorizedMsgUpdateParamSucceeds() {
+	for _, moduleName := range suites.MsgUpdateParamEnabledModuleNames {
+		moduleCfg := suites.ModuleParamConfigMap[moduleName]
+
+		// Iterate over each field in the current module's MsgUpdateParam, for each
+		// field, send a new MsgUpdateParam which would update the corresponding param
+		// to that field's value.
+		validParamsValue := reflect.ValueOf(moduleCfg.ValidParams)
+		for fieldIdx := 0; fieldIdx < validParamsValue.NumField(); fieldIdx++ {
+			fieldExpectedValue := validParamsValue.Field(fieldIdx)
+			fieldName := validParamsValue.Type().Field(fieldIdx).Name
+
+			testName := fmt.Sprintf("%s_%s", moduleName, fieldName)
+			s.T().Run(testName, func(t *testing.T) {
+				// Reset the app state in order to assert that each module
+				// param is updated correctly.
+				s.SetupSubTest()
+
+				// Assert that the module's params are set to their default values.
+				s.RequireModuleHasDefaultParams(t, moduleName)
+
+				updateResBz, err := s.RunUpdateParam(t,
+					moduleName,
+					fieldName,
+					fieldExpectedValue.Interface(),
+				)
+				require.NoError(t, err)
+				require.NotNil(t, updateResBz)
+
+				// TODO_INVESTIGATE(https://github.com/cosmos/cosmos-sdk/issues/21904):
+				// It seems like the response objects are encoded in an unexpected way.
+				// It's unclear whether this is the result of being executed via authz.
+				// Looking at the code, it seems like authz utilizes the sdk.Result#Data
+				// field of the result which is returned from the message handler.
+				// These result byte slices are accumulated for each message in the MsgExec and
+				// set on the MsgExecResponse#Results field.
+				//
+				// - https://github.com/cosmos/cosmos-sdk/blob/v0.50.9/x/authz/keeper/msg_server.go#L120
+				// - https://github.com/cosmos/cosmos-sdk/blob/v0.50.9/x/authz/keeper/keeper.go#L166
+				// - https://github.com/cosmos/cosmos-sdk/blob/v0.50.9/baseapp/msg_service_router.go#L55
+				// - https://github.com/cosmos/cosmos-sdk/blob/v0.50.9/baseapp/msg_service_router.go#L198
+				// - https://github.com/cosmos/cosmos-sdk/blob/v0.50.9/types/result.go#L213
+				//
+				// I (@bryanchriswhite) would've expected the following to work, but it does not:
+				//
+				// updateResValue := reflect.New(reflect.TypeOf(moduleCfg.MsgUpdateParamResponse))
+				// // NB: using proto.Unmarshal here because authz seems to use
+				// // proto.Marshal to serialize each message response.
+				// err = proto.Unmarshal(updateResBz, updateResValue.Interface().(cosmostypes.Msg))
+				// require.NoError(t, err)
+				// updateResParamValue := updateResValue.Elem().FieldByName("Params").Elem().FieldByName(fieldName)
+				// require.Equal(t, fieldExpectedValue.Interface(), updateResParamValue.Interface())
+
+				// Query for the module's params.
+				params, err := s.QueryModuleParams(t, moduleName)
+				require.NoError(t, err)
+
+				paramsValue := reflect.ValueOf(params)
+				paramValue := paramsValue.FieldByName(fieldName)
+				require.Equal(t, fieldExpectedValue.Interface(), paramValue.Interface())
+			})
+		}
+	}
+}
---
+++ testutil/integration/suites/authz.go
@@ -0,0 +1,108 @@
+package suites
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	// poktrollMsgTypeFormat is the format for a poktroll module's message type.
+	// The first %s is the module name, and the second %s is the message name.
+	poktrollMsgTypeFormat = "/poktroll.%s.%s"
+)
+
+var defaultAuthzGrantExpiration = time.Now().Add(time.Hour)
+
+// AuthzIntegrationSuite is an integration test suite that provides helper functions for
+// running authz grant and exec messages. It is intended to be embedded in other integration
+// test suites which are dependent on authz.
+type AuthzIntegrationSuite struct {
+	BaseIntegrationSuite
+}
+
+// RunAuthzGrantMsgForPoktrollModules creates an on-chain authz grant for the given
+// granter and grantee addresses for the specified message name in each of the poktroll
+// modules present in the integration app.
+func (s *AuthzIntegrationSuite) RunAuthzGrantMsgForPoktrollModules(
+	t *testing.T,
+	granterAddr, granteeAddr cosmostypes.AccAddress,
+	msgName string,
+	moduleNames ...string,
+) {
+	t.Helper()
+
+	var foundModuleGrants = make(map[string]int)
+	for _, moduleName := range moduleNames {
+		msgType := fmtPoktrollMsgType(moduleName, msgName)
+		authorization := &authz.GenericAuthorization{Msg: msgType}
+		s.RunAuthzGrantMsg(t, granterAddr, granteeAddr, authorization)
+
+		// Query for the created grant to assert that they were created.
+		authzQueryClient := authz.NewQueryClient(s.app.QueryHelper())
+		queryGrantsReq := &authz.QueryGrantsRequest{
+			Granter:    granterAddr.String(),
+			Grantee:    granteeAddr.String(),
+			MsgTypeUrl: msgType,
+		}
+		queryGrantsRes, err := authzQueryClient.Grants(s.app.GetSdkCtx(), queryGrantsReq)
+		require.NoError(t, err)
+		require.NotNil(t, queryGrantsRes)
+
+		// Count the number of grants found for each module.
+		for range queryGrantsRes.GetGrants() {
+			foundModuleGrants[moduleName]++
+		}
+	}
+
+	// Assert that only one grant was created for each module.
+	for _, foundTimes := range foundModuleGrants {
+		require.Equal(t, 1, foundTimes)
+	}
+}
+
+// RunAuthzGrantMsg creates an on-chain authz grant from the given granter to the
+// grantee addresses for the authorization object provided.
+func (s *AuthzIntegrationSuite) RunAuthzGrantMsg(
+	t *testing.T,
+	granterAddr,
+	granteeAddr cosmostypes.AccAddress,
+	authorization authz.Authorization,
+) {
+	t.Helper()
+
+	grantMsg, err := authz.NewMsgGrant(granterAddr, granteeAddr, authorization, &defaultAuthzGrantExpiration)
+	require.NoError(t, err)
+
+	grantResAny, err := s.app.RunMsg(s.T(), grantMsg)
+	require.NoError(t, err)
+	require.NotNil(t, grantResAny)
+}
+
+// RunAuthzExecMsg executes the given messag(es) using authz. It assumes that an
+// authorization exists for which signerAdder is the grantee.
+func (s *AuthzIntegrationSuite) RunAuthzExecMsg(
+	t *testing.T,
+	signerAddr cosmostypes.AccAddress,
+	msgs ...cosmostypes.Msg,
+) (msgRespsBz [][]byte, err error) {
+	t.Helper()
+
+	execMsg := authz.NewMsgExec(signerAddr, msgs)
+	execResAny, err := s.GetApp().RunMsg(s.T(), &execMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	require.NotNil(t, execResAny)
+	return execResAny.(*authz.MsgExecResponse).Results, nil
+}
+
+// fmtPoktrollMsgType returns the formatted message type for a poktroll module.
+func fmtPoktrollMsgType(moduleName, msgName string) string {
+	return fmt.Sprintf(poktrollMsgTypeFormat, moduleName, msgName)
+}
