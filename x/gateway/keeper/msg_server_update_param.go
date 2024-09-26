899effb2dec8fd207f58c5c70162915db4654156
Bryan White <bryanchriswhite@gmail.com>
2024-09-26 08:57:40 +0200

    feat: add min_stake gateway module param

--- x/gateway/keeper/msg_server_update_param.go
+++ x/gateway/keeper/msg_server_update_param.go
@@ -3,15 +3,45 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/pokt-network/poktroll/x/gateway/types"
 )
 
-func (k msgServer) UpdateParam(goCtx context.Context, msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+func (k msgServer) UpdateParam(ctx context.Context, msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
+	if err := msg.ValidateBasic(); err != nil {
+		return nil, err
+	}
 
-	// TODO: Handling the message
-	_ = ctx
+	if k.GetAuthority() != msg.Authority {
+		return nil, types.ErrGatewayInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+	}
 
-	return &types.MsgUpdateParamResponse{}, nil
+	params := k.GetParams(ctx)
+
+	switch msg.Name {
+	case types.ParamMinStake:
+		value, ok := msg.AsType.(*types.MsgUpdateParam_AsCoin)
+		if !ok {
+			return nil, types.ErrGatewayParamInvalid.Wrapf("unsupported value type for %s param: %T", msg.Name, msg.AsType)
+		}
+
+		params.MinStake = value.AsCoin
+	default:
+		return nil, types.ErrGatewayParamInvalid.Wrapf("unsupported param %q", msg.Name)
+	}
+
+	// Perform a global validation on all params, which includes the updated param.
+	// This is needed to ensure that the updated param is valid in the context of all other params.
+	if err := params.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := k.SetParams(ctx, params); err != nil {
+		return nil, err
+	}
+
+	updatedParams := k.GetParams(ctx)
+
+	return &types.MsgUpdateParamResponse{
+		Params: &updatedParams,
+	}, nil
 }
--- x/gateway/types/message_update_param.go
+++ x/gateway/types/message_update_param.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"fmt"
+
 	errorsmod "cosmossdk.io/errors"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -8,11 +10,20 @@ import (
 
 var _ cosmostypes.Msg = (*MsgUpdateParam)(nil)
 
-func NewMsgUpdateParam(authority string, name string, asType string) *MsgUpdateParam {
+func NewMsgUpdateParam(authority string, name string, value any) *MsgUpdateParam {
+	var valueAsType isMsgUpdateParam_AsType
+
+	switch v := value.(type) {
+	case *cosmostypes.Coin:
+		valueAsType = &MsgUpdateParam_AsCoin{AsCoin: v}
+	default:
+		panic(fmt.Sprintf("unexpected param value type: %T", value))
+	}
+
 	return &MsgUpdateParam{
 		Authority: authority,
 		Name:      name,
-		AsType:    asType,
+		AsType:    valueAsType,
 	}
 }
 
@@ -21,5 +32,29 @@ func (msg *MsgUpdateParam) ValidateBasic() error {
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid authority address (%s)", err)
 	}
+
+	// Parameter value cannot be nil.
+	if msg.AsType == nil {
+		return ErrGatewayParamInvalid.Wrap("missing param AsType")
+	}
+
+	// Parameter name must be supported by this module.
+	switch msg.Name {
+	case ParamMinStake:
+		return msg.paramTypeIsCoin()
+	default:
+		return ErrGatewayParamInvalid.Wrapf("unsupported param %q", msg.Name)
+	}
+}
+
+func (msg *MsgUpdateParam) paramTypeIsCoin() error {
+	_, ok := msg.AsType.(*MsgUpdateParam_AsCoin)
+	if !ok {
+		return ErrGatewayParamInvalid.Wrapf(
+			"invalid type for param %q expected %T type: %T",
+			msg.Name, &MsgUpdateParam_AsCoin{}, msg.AsType,
+		)
+	}
+
 	return nil
 }
