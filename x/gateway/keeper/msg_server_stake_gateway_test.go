7e9f8570824a261e384cba26248cbaf511a22be3
Bryan White <bryanchriswhite@gmail.com>
2024-09-27 10:12:07 +0200

    chore: cleanup comments

--- x/gateway/keeper/msg_server_stake_gateway_test.go
+++ x/gateway/keeper/msg_server_stake_gateway_test.go
@@ -18,38 +18,38 @@ func TestMsgServer_StakeGateway_SuccessfulCreateAndUpdate(t *testing.T) {
 	k, ctx := keepertest.GatewayKeeper(t)
 	srv := keeper.NewMsgServerImpl(k)
 
-	// Generate an address for the gateway
+	// Generate an address for the gateway.
 	addr := sample.AccAddress()
 
-	// Verify that the gateway does not exist yet
+	// Verify that the gateway does not exist yet.
 	_, isGatewayFound := k.GetGateway(ctx, addr)
 	require.False(t, isGatewayFound)
 
-	// Prepare the gateway
+	// Prepare the gateway.
 	initialStake := cosmostypes.NewCoin("upokt", math.NewInt(100))
 	stakeMsg := &gatewaytypes.MsgStakeGateway{
 		Address: addr,
 		Stake:   &initialStake,
 	}
 
-	// Stake the gateway
+	// Stake the gateway.
 	_, err := srv.StakeGateway(ctx, stakeMsg)
 	require.NoError(t, err)
 
-	// Verify that the gateway exists
+	// Verify that the gateway exists.
 	foundGateway, isGatewayFound := k.GetGateway(ctx, addr)
 	require.True(t, isGatewayFound)
 	require.Equal(t, addr, foundGateway.Address)
 	require.Equal(t, initialStake.Amount, foundGateway.Stake.Amount)
 
-	// Prepare an updated gateway with a higher stake
+	// Prepare an updated gateway with a higher stake.
 	updatedStake := cosmostypes.NewCoin("upokt", math.NewInt(200))
 	updateMsg := &gatewaytypes.MsgStakeGateway{
 		Address: addr,
 		Stake:   &updatedStake,
 	}
 
-	// Update the staked gateway
+	// Update the staked gateway.
 	_, err = srv.StakeGateway(ctx, updateMsg)
 	require.NoError(t, err)
 	foundGateway, isGatewayFound = k.GetGateway(ctx, addr)
@@ -61,7 +61,7 @@ func TestMsgServer_StakeGateway_FailLoweringStake(t *testing.T) {
 	k, ctx := keepertest.GatewayKeeper(t)
 	srv := keeper.NewMsgServerImpl(k)
 
-	// Prepare the gateway
+	// Prepare the gateway.
 	addr := sample.AccAddress()
 	initialStake := cosmostypes.NewCoin("upokt", math.NewInt(100))
 	stakeMsg := &gatewaytypes.MsgStakeGateway{
@@ -69,24 +69,24 @@ func TestMsgServer_StakeGateway_FailLoweringStake(t *testing.T) {
 		Stake:   &initialStake,
 	}
 
-	// Stake the gateway & verify that the gateway exists
+	// Stake the gateway & verify that the gateway exists.
 	_, err := srv.StakeGateway(ctx, stakeMsg)
 	require.NoError(t, err)
 	_, isGatewayFound := k.GetGateway(ctx, addr)
 	require.True(t, isGatewayFound)
 
-	// Prepare an updated gateway with a lower stake
+	// Prepare an updated gateway with a lower stake.
 	updatedStake := cosmostypes.NewCoin("upokt", math.NewInt(50))
 	updateMsg := &gatewaytypes.MsgStakeGateway{
 		Address: addr,
 		Stake:   &updatedStake,
 	}
 
-	// Verify that it fails
+	// Verify that it fails.
 	_, err = srv.StakeGateway(ctx, updateMsg)
 	require.Error(t, err)
 
-	// Verify that the gateway stake is unchanged
+	// Verify that the gateway stake is unchanged.
 	gatewayFound, isGatewayFound := k.GetGateway(ctx, addr)
 	require.True(t, isGatewayFound)
 	require.Equal(t, initialStake.Amount, gatewayFound.Stake.Amount)
@@ -107,13 +107,13 @@ func TestMsgServer_StakeGateway_FailBelowMinStake(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	// Prepare the gateway
+	// Prepare the gateway.
 	stakeMsg := &gatewaytypes.MsgStakeGateway{
 		Address: addr,
 		Stake:   &gatewayStake,
 	}
 
-	// Stake the gateway & verify that the gateway exists
+	// Attempt to stake the gateway & verify that the gateway does NOT exist.
 	_, err = srv.StakeGateway(ctx, stakeMsg)
 	require.ErrorContains(t, err, expectedErr.Error())
 	_, isGatewayFound := k.GetGateway(ctx, addr)
