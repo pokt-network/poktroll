a49d69b3b6555ced9c6bf88f04d0ebd3e39a7e24
Bryan White <bryanchriswhite@gmail.com>
2024-09-27 17:47:05 +0200

    feat: implement NewMsgUpdateParam constructor

--- x/application/types/message_update_param.go
+++ x/application/types/message_update_param.go
@@ -14,6 +14,8 @@ func NewMsgUpdateParam(authority string, name string, asType any) *MsgUpdatePara
 	var asTypeIface isMsgUpdateParam_AsType
 
 	switch t := asType.(type) {
+	case uint64:
+		asTypeIface = &MsgUpdateParam_AsUint64{AsUint64: t}
 	case *cosmostypes.Coin:
 		asTypeIface = &MsgUpdateParam_AsCoin{AsCoin: t}
 	default:
@@ -31,6 +33,40 @@ func (msg *MsgUpdateParam) ValidateBasic() error {
 	_, err := cosmostypes.AccAddressFromBech32(msg.Authority)
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid authority address (%s)", err)
+	}
+
+	// Parameter value MUST NOT be nil.
+	if msg.AsType == nil {
+		return ErrAppParamInvalid.Wrapf("missing param AsType for parameter %q", msg.Name)
+	}
+
+	// Parameter name MUST be supported by this module.
+	switch msg.Name {
+	case ParamMaxDelegatedGateways:
+		return msg.paramTypeIsUint64()
+	case ParamMinStake:
+		return msg.paramTypeIsCoin()
+	default:
+		return ErrAppParamInvalid.Wrapf("unsupported param %q", msg.Name)
+	}
+}
+
+func (msg *MsgUpdateParam) paramTypeIsUint64() error {
+	if _, ok := msg.AsType.(*MsgUpdateParam_AsUint64); !ok {
+		return ErrAppParamInvalid.Wrapf(""+
+			"invalid type for param %q; expected %T, got %T",
+			msg.Name, &MsgUpdateParam_AsUint64{}, msg.AsType,
+		)
+	}
+	return nil
+}
+
+func (msg *MsgUpdateParam) paramTypeIsCoin() error {
+	if _, ok := msg.AsType.(*MsgUpdateParam_AsCoin); !ok {
+		return ErrAppParamInvalid.Wrapf(""+
+			"invalid type for param %q; expected %T, got %T",
+			msg.Name, &MsgUpdateParam_AsCoin{}, msg.AsType,
+		)
 	}
 	return nil
 }
