352afa1b8647f87e61375c719a9accfa0bfae1f6
Bryan White <bryanchriswhite@gmail.com>
2024-09-30 11:13:22 +0200

    chore: review feedback improvements
    Co-authored-by: Redouane Lakrache <r3d0ne@gmail.com>

--- x/application/keeper/msg_server_update_param.go
+++ x/application/keeper/msg_server_update_param.go
@@ -3,8 +3,9 @@ package keeper
 import (
 	"context"
 
-    "github.com/pokt-network/poktroll/x/application/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/pokt-network/poktroll/x/application/types"
 )
 
 
