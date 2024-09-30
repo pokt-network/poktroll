6afdaefc629d5f4cff6b3c70213fe26abdd2a735
Bryan White <bryanchriswhite@gmail.com>
2024-09-30 11:51:29 +0200

    fix: linter errors

--- x/application/keeper/msg_server_update_param.go
+++ x/application/keeper/msg_server_update_param.go
@@ -8,12 +8,11 @@ import (
 	"github.com/pokt-network/poktroll/x/application/types"
 )
 
-
-func (k msgServer) UpdateParam(goCtx context.Context,  msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
+func (k msgServer) UpdateParam(goCtx context.Context, msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-    // TODO: Handling the message
-    _ = ctx
+	// TODO: Handling the message
+	_ = ctx
 
 	return &types.MsgUpdateParamResponse{}, nil
 }
