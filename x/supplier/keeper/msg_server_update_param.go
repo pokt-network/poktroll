acb662201aaed0751acc0e5591a0c3f54cb5b207
Bryan White <bryanchriswhite@gmail.com>
2024-10-02 11:47:35 +0200

    chore: update msgServer#UpdateParam()

--- x/supplier/keeper/msg_server_update_param.go
+++ x/supplier/keeper/msg_server_update_param.go
@@ -3,16 +3,33 @@ package keeper
 import (
 	"context"
 
-    "github.com/pokt-network/poktroll/x/supplier/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	suppliertypes "github.com/pokt-network/poktroll/x/supplier/types"
 )
 
+// UpdateParam updates a single parameter in the proof module and returns
+// all active parameters.
+func (k msgServer) UpdateParam(ctx context.Context, msg *suppliertypes.MsgUpdateParam) (*suppliertypes.MsgUpdateParamResponse, error) {
+	if err := msg.ValidateBasic(); err != nil {
+		return nil, err
+	}
 
-func (k msgServer) UpdateParam(goCtx context.Context,  msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	if k.GetAuthority() != msg.Authority {
+		return nil, suppliertypes.ErrSupplierInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+	}
 
-    // TODO: Handling the message
-    _ = ctx
+	params := k.GetParams(ctx)
 
-	return &types.MsgUpdateParamResponse{}, nil
+	switch msg.Name {
+	default:
+		return nil, suppliertypes.ErrSupplierParamInvalid.Wrapf("unsupported param %q", msg.Name)
+	}
+
+	if err := k.SetParams(ctx, params); err != nil {
+		return nil, err
+	}
+
+	updatedParams := k.GetParams(ctx)
+	return &suppliertypes.MsgUpdateParamResponse{
+		Params: &updatedParams,
+	}, nil
 }
