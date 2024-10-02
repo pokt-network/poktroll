5e72e47f398cbbb45d66a0950f976301ea393489
Bryan White <bryanchriswhite@gmail.com>
2024-10-02 17:08:19 +0200

    chore: add min_stake case to MsgUpdateParam handler

--- x/supplier/keeper/msg_server_update_param.go
+++ x/supplier/keeper/msg_server_update_param.go
@@ -17,21 +17,21 @@ func (k msgServer) UpdateParam(ctx context.Context, msg *suppliertypes.MsgUpdate
 		return nil, suppliertypes.ErrSupplierInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
 	}
 
-	// TODO_UPNEXT(@bryanchriswhite, #612): uncomment & add a min_stake case.
-
-	//params := k.GetParams(ctx)
+	params := k.GetParams(ctx)
 
 	switch msg.Name {
+	case suppliertypes.ParamMinStake:
+		params.MinStake = msg.GetAsCoin()
 	default:
 		return nil, suppliertypes.ErrSupplierParamInvalid.Wrapf("unsupported param %q", msg.Name)
 	}
 
-	//if err := k.SetParams(ctx, params); err != nil {
-	//	return nil, err
-	//}
-	//
-	//updatedParams := k.GetParams(ctx)
-	//return &suppliertypes.MsgUpdateParamResponse{
-	//	Params: &updatedParams,
-	//}, nil
+	if err := k.SetParams(ctx, params); err != nil {
+		return nil, err
+	}
+
+	updatedParams := k.GetParams(ctx)
+	return &suppliertypes.MsgUpdateParamResponse{
+		Params: &updatedParams,
+	}, nil
 }
