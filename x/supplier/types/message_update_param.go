000c3304fd5225ba46318cd418aed6bde0102dac
Bryan White <bryanchriswhite@gmail.com>
2024-10-02 16:42:25 +0200

    chore: add min_stake defaults and validation

--- x/supplier/types/message_update_param.go
+++ x/supplier/types/message_update_param.go
@@ -27,6 +27,10 @@ func NewMsgUpdateParam(authority string, name string, asType any) *MsgUpdatePara
 	}
 }
 
+// ValidateBasic performs a basic validation of the MsgUpdateParam fields. It ensures:
+// 1. The parameter name is supported.
+// 2. The parameter type matches the expected type for a given parameter name.
+// 3. The parameter value is valid (according to its respective validation function).
 func (msg *MsgUpdateParam) ValidateBasic() error {
 	_, err := cosmostypes.AccAddressFromBech32(msg.Authority)
 	if err != nil {
@@ -40,10 +44,24 @@ func (msg *MsgUpdateParam) ValidateBasic() error {
 
 	// Parameter name MUST be supported by this module.
 	switch msg.Name {
-	// TODO_UPNEXT(@bryanchriswhite, #612): replace with min_stake param name and call validation function.
-	case "":
-		return nil
+	case ParamMinStake:
+		if err := msg.paramTypeIsCoin(); err != nil {
+			return err
+		}
+		return ValidateMinStake(msg.GetAsCoin())
 	default:
 		return ErrSupplierParamInvalid.Wrapf("unsupported param %q", msg.Name)
 	}
+}
+
+// paramTypeIsCoin checks if the parameter type is a Coin, returning an error if not.
+func (msg *MsgUpdateParam) paramTypeIsCoin() error {
+	if _, ok := msg.AsType.(*MsgUpdateParam_AsCoin); !ok {
+		return ErrSupplierParamInvalid.Wrapf(
+			"invalid type for param %q expected %T, got %T",
+			msg.Name, &MsgUpdateParam_AsCoin{},
+			msg.AsType,
+		)
+	}
+	return nil
 }
