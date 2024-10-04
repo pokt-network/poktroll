bb0ec852ea8bf2340903b07516ee1c7d20d1da0c
Bryan White <bryanchriswhite@gmail.com>
2024-10-04 19:17:19 +0200

    [Application] chore: add `MsgUpdateParam` to application module  (#844)
    ## Summary
    
    ```bash
    ignite scaffold message update-param --module gateway --signer authority name as_type --response params
    ```
    
    Adds the `MsgUpdateParam` message so that the application module may
    update individual parameters. The application module's min_stake param
    will be added in a subsequent PR.
    
    ## Dependencies
    
    - #809
    - #843 
    
    ## Dependents
    
    - #845 
    - #847 
    - #848 
    - #849
    - #850
    - #857
    
    ## Issue
    
    Add individual param updates support to the application module
    
    - #612
    
    ## Type of change
    
    Select one or more from the following:
    
    - [x] New feature, functionality or library
    - [x] Consensus breaking; add the `consensus-breaking` label if so. See
    #791 for details
    - [ ] Bug fix
    - [ ] Code health or cleanup
    - [ ] Documentation
    - [ ] Other (specify)
    
    ## Testing
    
    - [ ] **Documentation**: `make docusaurus_start`; only needed if you
    make doc changes
    - [ ] **Unit Tests**: `make go_develop_and_test`
    - [ ] **LocalNet E2E Tests**: `make test_e2e`
    - [ ] **DevNet E2E Tests**: Add the `devnet-test-e2e` label to the PR.
    
    ## Sanity Checklist
    
    - [x] I have tested my changes using the available tooling
    - [ ] I have commented my code
    - [x] I have performed a self-review of my own code; both comments &
    source code
    - [ ] I create and reference any new tickets, if applicable
    - [ ] I have left TODOs throughout the codebase, if applicable
    
    ---------
    
    Co-authored-by: Redouane Lakrache <r3d0ne@gmail.com>
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    Co-authored-by: red-0ne <red-0ne@users.noreply.github.com>

---
+++ x/application/types/message_update_param.go
@@ -0,0 +1,36 @@
+package types
+
+import (
+	"fmt"
+
+	errorsmod "cosmossdk.io/errors"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+var _ cosmostypes.Msg = (*MsgUpdateParam)(nil)
+
+func NewMsgUpdateParam(authority string, name string, asType any) *MsgUpdateParam {
+	var asTypeIface isMsgUpdateParam_AsType
+
+	switch t := asType.(type) {
+	case *cosmostypes.Coin:
+		asTypeIface = &MsgUpdateParam_AsCoin{AsCoin: t}
+	default:
+		panic(fmt.Sprintf("unexpected param value type: %T", asType))
+	}
+
+	return &MsgUpdateParam{
+		Authority: authority,
+		Name:      name,
+		AsType:    asTypeIface,
+	}
+}
+
+func (msg *MsgUpdateParam) ValidateBasic() error {
+	_, err := cosmostypes.AccAddressFromBech32(msg.Authority)
+	if err != nil {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid authority address (%s)", err)
+	}
+	return nil
+}
