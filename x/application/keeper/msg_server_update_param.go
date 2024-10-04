2a331272cacd424f8d5f5883e618f9a74ecab16f
Bryan White <bryanchriswhite@gmail.com>
2024-10-04 10:14:53 +0200

    chore: improvements:
    - Ensure gRPC status error returns from application module UpdateParam handler
    - Push validation down into MsgUpdateParam#ValidateBasic()
    - Validate params before setting when handling MsgUpdateParam
    - Fix param name argument in MsgUpdateParam test

--- x/application/keeper/msg_server_update_param.go
+++ x/application/keeper/msg_server_update_param.go
@@ -3,6 +3,9 @@ package keeper
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	apptypes "github.com/pokt-network/poktroll/x/application/types"
 )
 
@@ -12,7 +15,13 @@ func (k msgServer) UpdateParam(ctx context.Context, msg *apptypes.MsgUpdateParam
 	}
 
 	if k.GetAuthority() != msg.Authority {
-		return nil, apptypes.ErrAppInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+		return nil, status.Error(
+			codes.InvalidArgument,
+			apptypes.ErrAppInvalidSigner.Wrapf(
+				"invalid authority; expected %s, got %s",
+				k.GetAuthority(), msg.Authority,
+			).Error(),
+		)
 	}
 
 	params := k.GetParams(ctx)
@@ -20,27 +29,21 @@ func (k msgServer) UpdateParam(ctx context.Context, msg *apptypes.MsgUpdateParam
 	switch msg.Name {
 	// TODO_IMPROVE: Add a Uint64 asType instead of using int64 for uint64 params.
 	case apptypes.ParamMaxDelegatedGateways:
-		if _, ok := msg.AsType.(*apptypes.MsgUpdateParam_AsInt64); !ok {
-			return nil, apptypes.ErrAppParamInvalid.Wrapf("unsupported value type for %s param: %T", msg.Name, msg.AsType)
-		}
-		maxDelegatedGateways := uint64(msg.GetAsInt64())
-
-		if err := apptypes.ValidateMaxDelegatedGateways(maxDelegatedGateways); err != nil {
-			return nil, apptypes.ErrAppParamInvalid.Wrapf("maxdelegegated_gateways (%d): %v", maxDelegatedGateways, err)
-		}
-		params.MaxDelegatedGateways = maxDelegatedGateways
+		params.MaxDelegatedGateways = uint64(msg.GetAsInt64())
 	case apptypes.ParamMinStake:
-		if _, ok := msg.AsType.(*apptypes.MsgUpdateParam_AsCoin); !ok {
-			return nil, apptypes.ErrAppParamInvalid.Wrapf("unsupported value type for %s param: %T", msg.Name, msg.AsType)
-		}
-		minStake := msg.GetAsCoin()
-
-		if err := apptypes.ValidateMinStake(minStake); err != nil {
-			return nil, err
-		}
-		params.MinStake = minStake
+		params.MinStake = msg.GetAsCoin()
 	default:
-		return nil, apptypes.ErrAppParamInvalid.Wrapf("unsupported param %q", msg.Name)
+		return nil, status.Error(
+			codes.InvalidArgument,
+			apptypes.ErrAppParamInvalid.Wrapf("unsupported param %q", msg.Name).Error(),
+		)
+	}
+
+	// Perform a global validation on all params, which includes the updated param.
+	// This is needed to ensure that the updated param is valid in the context of all other params.
+	if err := params.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+
 	}
 
 	if err := k.SetParams(ctx, params); err != nil {
