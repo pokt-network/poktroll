3280142714a82e6070ac568955f2b435700ee48a
Bryan White <bryanchriswhite@gmail.com>
2024-10-04 10:33:17 +0200

    chore: improve logging & ensure gRPC status error returns

--- x/gateway/keeper/msg_server_update_param.go
+++ x/gateway/keeper/msg_server_update_param.go
@@ -2,6 +2,10 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/pokt-network/poktroll/x/gateway/types"
 )
@@ -9,31 +13,48 @@ import (
 // UpdateParam updates a single parameter in the proof module and returns
 // all active parameters.
 func (k msgServer) UpdateParam(ctx context.Context, msg *types.MsgUpdateParam) (*types.MsgUpdateParamResponse, error) {
+	logger := k.logger.With(
+		"method", "UpdateParam",
+		"param_name", msg.Name,
+	)
+
 	if err := msg.ValidateBasic(); err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	if k.GetAuthority() != msg.Authority {
-		return nil, types.ErrGatewayInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+		return nil, status.Error(
+			codes.InvalidArgument,
+			types.ErrGatewayInvalidSigner.Wrapf(
+				"invalid authority; expected %s, got %s",
+				k.GetAuthority(), msg.Authority,
+			).Error(),
+		)
 	}
 
 	params := k.GetParams(ctx)
 
 	switch msg.Name {
 	case types.ParamMinStake:
+		logger = logger.With("param_value", msg.GetAsCoin())
 		params.MinStake = msg.GetAsCoin()
 	default:
-		return nil, types.ErrGatewayParamInvalid.Wrapf("unsupported param %q", msg.Name)
+		return nil, status.Error(
+			codes.InvalidArgument,
+			types.ErrGatewayParamInvalid.Wrapf("unsupported param %q", msg.Name).Error(),
+		)
 	}
 
 	// Perform a global validation on all params, which includes the updated param.
 	// This is needed to ensure that the updated param is valid in the context of all other params.
 	if err := params.Validate(); err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	if err := k.SetParams(ctx, params); err != nil {
-		return nil, err
+		err = fmt.Errorf("unable to set params: %w", err)
+		logger.Error(err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	updatedParams := k.GetParams(ctx)
