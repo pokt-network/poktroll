955b28fb38d9c5e606107705ac618b837dd612d2
Bryan White <bryanchriswhite@gmail.com>
2024-10-04 10:42:48 +0200

    chore: improve logging, validate params in MsgUpdateParam handler, & ensure gRPC status error returns

--- x/supplier/keeper/msg_server_update_param.go
+++ x/supplier/keeper/msg_server_update_param.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -12,6 +13,11 @@ import (
 // UpdateParam updates a single parameter in the proof module and returns
 // all active parameters.
 func (k msgServer) UpdateParam(ctx context.Context, msg *suppliertypes.MsgUpdateParam) (*suppliertypes.MsgUpdateParamResponse, error) {
+	logger := k.logger.With(
+		"method", "UpdateParam",
+		"param_name", msg.Name,
+	)
+
 	if err := msg.ValidateBasic(); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
@@ -30,6 +36,7 @@ func (k msgServer) UpdateParam(ctx context.Context, msg *suppliertypes.MsgUpdate
 
 	switch msg.Name {
 	case suppliertypes.ParamMinStake:
+		logger = logger.With("min_stake", msg.GetAsCoin())
 		params.MinStake = msg.GetAsCoin()
 	default:
 		return nil, status.Error(
@@ -38,11 +45,20 @@ func (k msgServer) UpdateParam(ctx context.Context, msg *suppliertypes.MsgUpdate
 		)
 	}
 
-	if err := k.SetParams(ctx, params); err != nil {
+	// Perform a global validation on all params, which includes the updated param.
+	// This is needed to ensure that the updated param is valid in the context of all other params.
+	if err := params.Validate(); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	if err := k.SetParams(ctx, params); err != nil {
+		err = fmt.Errorf("unable to set params: %v", err)
+		logger.Error(err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
 	updatedParams := k.GetParams(ctx)
+
 	return &suppliertypes.MsgUpdateParamResponse{
 		Params: &updatedParams,
 	}, nil
