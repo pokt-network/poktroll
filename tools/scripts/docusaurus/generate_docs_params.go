d5620d7c6393f7139467e9ddc2c36e0a0e37e75e
Bryan White <bryanchriswhite@gmail.com>
2024-10-07 18:07:32 +0200

    refactor: param docs generator command

--- tools/scripts/docusaurus/generate_docs_params.go
+++ tools/scripts/docusaurus/generate_docs_params.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
+
+	"github.com/jhump/protoreflect/desc/protoparse"
+	"github.com/jhump/protoreflect/desc/protoparse/ast"
 
 	"github.com/pokt-network/poktroll/pkg/polylog"
 	_ "github.com/pokt-network/poktroll/pkg/polylog/polyzero"
@@ -27,17 +30,29 @@ type ProtoMessage struct {
 
 const (
 	destinationFile = "docusaurus/docs/protocol/governance/params.md"
+	tableRowFmt     = "| `%-10s` | `%-10s` | `%-10s` | %-7s |\n"
 )
 
-var paramsDocsTemplateStr string
+var (
+	paramsDocsTemplateStr string
+	protoParser           = protoparse.Parser{
+		IncludeSourceCodeInfo: true,
+	}
+	logger = polylog.DefaultContextLogger
+
+	flagProtoRootPathValue string
+)
 
 func init() {
+	flag.StringVar(&flagProtoRootPathValue, "proto-root", "./proto", "path to the proto files root directory; this directory will be walked, looking for Params.proto files")
+	flag.Parse()
+
 	paramsTempalteFile, err := os.ReadFile("./tools/scripts/docusaurus/params_template.md")
 	if err != nil {
 		polylog.DefaultContextLogger.Error().Err(err).Send()
 		os.Exit(1)
 	}
-	paramsDocsTemplateStr = string(paramsTempalteFile)
+	paramsDocsTemplateStr = string(paramsTempalteFile) + "\n"
 }
 
 // writeContentToFile writes the given content to the specified file.
@@ -57,118 +72,104 @@ func writeContentToFile(file_path, content string) error {
 	return nil
 }
 
-// findProtoFiles returns a slice of file paths that contain the specified pattern within the given base directory.
-func findProtoFiles(baseDir, pattern string) (protoFilePaths []string, err error) {
-	err = filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+func prepareGovernanceParamsDocs(protoFilesRootDir string, template string) (string, error) {
+	paramsFieldNodesByModule := make(map[string][]*ast.FieldNode)
 
-		// Check if the file name contains the specified pattern
-		if !info.IsDir() && strings.Contains(info.Name(), pattern) {
-			protoFilePaths = append(protoFilePaths, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
+	if pathWalkErr := filepath.Walk(
+		protoFilesRootDir,
+		forEachMatchingFileWalkFn(
+			"params.proto",
+			newCollectParamsDescsFn(paramsFieldNodesByModule),
+		),
+	); pathWalkErr != nil {
+		logger.Error().Err(pathWalkErr)
+		os.Exit(1)
 	}
 
-	return protoFilePaths, nil
+	for moduleName, fieldNodes := range paramsFieldNodesByModule {
+		for _, fieldNode := range fieldNodes {
+			// Uncomment and concatenate the field's comment lines.
+			comment := ""
+			for commentIdx, commentLine := range fieldNode.LeadingComments() {
+				var commentFmt = " %s"
+				if commentIdx == 0 {
+					commentFmt = "%s"
+				}
+
+				comment += fmt.Sprintf(commentFmt, strings.Trim(commentLine.Text, " /"))
+			}
+			logger.Info().Str("comment", comment).Send()
+
+			// Extract the field's type information.
+			var fieldType string
+			ast.Walk(fieldNode, func(n ast.Node) (bool, ast.VisitFunc) {
+				switch node := n.(type) {
+				case *ast.IdentNode:
+					fieldType = node.Val
+					return false, nil
+				case *ast.CompoundIdentNode:
+					fieldType = node.Val
+					return false, nil
+				}
+				return true, nil
+			})
+
+			template += fmt.Sprintf(tableRowFmt, moduleName, fieldType, fieldNode.Name.Val, comment)
+		}
+	}
+
+	return template, nil
 }
 
-var (
-	messageParamPattern = regexp.MustCompile(`^message\s+(Params)\s*{`)
-	fieldPattern        = regexp.MustCompile(`^\s*(\w+)\s+(\w+)\s*=\s*(\d+)\s*\[(.*?)\];`)
-	commentPattern      = regexp.MustCompile(`^//\s*(.*)`)
-)
-
-// prepareGovernanceParamsDocs parses the given .proto files and prepares the governance parameters documentation.
-func prepareGovernanceParamsDocs(protoFilePaths []string, template string) (string, error) {
-	docs := template
-	for _, filePath := range protoFilePaths {
-		fmt.Println("Parsing .proto file:", filePath)
-		module := strings.Split(filePath, "/")[2]
-
-		protoFile, err := os.Open(filePath)
-		if err != nil {
-			fmt.Println("Error opening .proto file:", err)
-			return "", err
+func newCollectParamsDescsFn(paramsFieldNodesByModule map[string][]*ast.FieldNode) func(protoFilePath string) {
+	return func(protoFilePath string) {
+		logger.Debug().Str("protoFilePath", protoFilePath).Send()
+		protoFileNodes, parseErr := protoParser.ParseToAST(protoFilePath)
+		if parseErr != nil {
+			logger.Error().Err(parseErr).Msgf("Unable to parse proto file: %q", protoFilePath)
 		}
-		defer protoFile.Close()
 
-		var paramsMessages []ProtoMessage
-		var currentParamMessage *ProtoMessage
-		var currentComment string
-
-		scanner := bufio.NewScanner(protoFile)
-		for scanner.Scan() {
-			line := strings.TrimSpace(scanner.Text())
-
-			// Check if the line is a comment
-			if matches := commentPattern.FindStringSubmatch(line); matches != nil {
-				currentComment += matches[1] + " "
+		var moduleName string
+		pathParts := strings.Split(protoFilePath, string(filepath.Separator))
+		for idx, pathPart := range pathParts {
+			// Module name is ALWAYS the child directory of "proto/poktroll".
+			if pathPart != "poktroll" {
 				continue
 			}
+			moduleName = pathParts[idx+1]
+			break
+		}
 
-			// Check if the line defines a new message
-			if matches := messageParamPattern.FindStringSubmatch(line); matches != nil {
-				if currentParamMessage != nil {
-					paramsMessages = append(paramsMessages, *currentParamMessage)
-				}
-				currentComment = "" // Reset comment after associating it with a message
-				currentParamMessage = &ProtoMessage{Name: matches[1]}
-				continue
-			}
-
-			// Check if the line defines a field within a message
-			if matches := fieldPattern.FindStringSubmatch(line); matches != nil {
-				if currentParamMessage != nil {
-					field := ProtoField{
-						Type:        matches[1],
-						Name:        matches[2],
-						Tag:         matches[3],
-						Options:     matches[4],
-						Description: strings.TrimSpace(currentComment),
+		// Iterate through the file node and walk its AST.
+		for _, fileNode := range protoFileNodes {
+			ast.Walk(fileNode, func(node ast.Node) (bool, ast.VisitFunc) {
+				// Search for Message nodes:
+				if msgNode, ok := node.(*ast.MessageNode); ok {
+					// Filter out messages with names other than "Params".
+					if msgNode.Name.Val == "Params" {
+						// TODO_IN_THIS_COMMIT: factor out...
+						return true, func(node ast.Node) (bool, ast.VisitFunc) {
+							if fieldNode, ok := node.(*ast.FieldNode); ok {
+								moduleFieldNodes := append(paramsFieldNodesByModule[moduleName], fieldNode)
+								paramsFieldNodesByModule[moduleName] = moduleFieldNodes
+							}
+							return false, nil
+						}
 					}
-					currentParamMessage.Fields = append(currentParamMessage.Fields, field)
-					currentComment = "" // Reset comment after associating it with a field
+					return false, nil
 				}
-			}
-		}
-
-		// Add the last message to the list
-		if currentParamMessage != nil {
-			paramsMessages = append(paramsMessages, *currentParamMessage)
-		}
-
-		// Print the parsed messages and their fields as a table
-		for _, message := range paramsMessages {
-			for _, field := range message.Fields {
-				new_line := fmt.Sprintf("| `%-10s` | `%-10s` | `%-10s` | %-7s |\n", module, field.Type, field.Name, field.Description)
-				docs += new_line
-			}
-		}
-
-		if err := scanner.Err(); err != nil {
-			return "", err
+				return true, nil
+			})
 		}
 	}
-	return docs, nil
 }
 
 func main() {
-	protoFilePaths, err := findProtoFiles(".", "params.proto")
-	if err != nil {
-		fmt.Println("Error finding .proto files:", err)
-		return
-	}
-
 	// This is necessary because multiline strings in golang do not support embedded backticks.
 	template := fmt.Sprintf(paramsDocsTemplateStr, "```", "```")
 
-	docs, err := prepareGovernanceParamsDocs(protoFilePaths, template)
+	// TODO_IN_THIS_COMMIT: register flagRootValue...
+	docs, err := prepareGovernanceParamsDocs(flagProtoRootPathValue, template)
 	if err != nil {
 		fmt.Println("Error preparing governance params docs:", err)
 		return
@@ -180,4 +181,33 @@ func main() {
 		return
 	}
 
+}
+
+// forEachMatchingFileWalkFn returns a filepath.WalkFunc which does the following:
+// 1. Iterates over files matching fileNamePattern against each file name.
+// 2. For matching files, it calls fileMatchedFn with the respective path.
+func forEachMatchingFileWalkFn(
+	fileNamePattern string,
+	fileMatchedFn func(path string),
+) filepath.WalkFunc {
+	return func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Ignore directories
+		if info.IsDir() {
+			return nil
+		}
+
+		matched, matchErr := filepath.Match(fileNamePattern, info.Name())
+		if matchErr != nil {
+			return matchErr
+		}
+
+		if matched {
+			fileMatchedFn(path)
+		}
+		return nil
+	}
 }
