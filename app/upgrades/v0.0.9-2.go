9d0fb17ca305b9acb4d843a5eaf35991172ea03e
Dmitry K. <okdas@pm.me>
2024-10-09 16:43:51 -0700

    v0.0.9-2 upgrade

---
+++ app/upgrades/v0.0.9-2.go
@@ -0,0 +1,11 @@
+package upgrades
+
+import storetypes "cosmossdk.io/store/types"
+
+// Upgrade_v0_0_9_2 is an upgrade on Beta TestNet.
+var Upgrade_v0_0_9_2 = Upgrade{
+	// the transaction needs to have a plan with the same name.
+	PlanName:             "v0.0.9-2",
+	CreateUpgradeHandler: defaultUpgradeHandler,
+	StoreUpgrades:        storetypes.StoreUpgrades{},
+}
