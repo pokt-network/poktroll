2d90c41f40281367e4618ef4eec794236a62f889
Daniel Olshansky <olshansky.daniel@gmail.com>
2024-11-04 14:21:54 -0500

    Updated list of required TODO_BETAs

--- x/tokenomics/keeper/settle_pending_claims.go
+++ x/tokenomics/keeper/settle_pending_claims.go
@@ -140,8 +140,8 @@ func (k Keeper) SettlePendingClaims(ctx sdk.Context) (
 
 			// If the proof is missing or invalid -> expire it
 			if expirationReason != tokenomicstypes.ClaimExpirationReason_EXPIRATION_REASON_UNSPECIFIED {
-				// TODO_BETA(@red-0ne): Slash the supplier in proportion
-				// to their stake. Consider allowing suppliers to RemoveClaim via a new
+				// TODO_BETA(@red-0ne): Slash the supplier in proportion to their stake.
+				// TODO_POST_MAINNET: Consider allowing suppliers to RemoveClaim via a new
 				// message in case it was sent by accident
 
 				// Proof was required but is invalid or not found.
