90e3f3277cb870c408a54105466d7bad088fcf44
Dima K. <okdas@users.noreply.github.com>
2024-11-15 14:07:10 -0800

    Update app/upgrades/types.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- app/upgrades/types.go
+++ app/upgrades/types.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pokt-network/poktroll/app/keepers"
 )
 
-// TODO_CONSIDER: different networks should have the same gov module address, but might have different DAO addresses,
+// TODO_MAINNET_DISCUSSION(@Olshansk): different networks should have the same gov module address, but might have different DAO addresses,
 // unless we specifically write in these addresses in the genesis file.
 // Should we use the same address/wallet for DAO or find a way to detect the network the upgrade is being applied to,
 // to pick different addresses depending on the name of the network? (e.g chain-id)
