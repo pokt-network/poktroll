9259f6a8066547114b9f7acb8eb852ed8d7f6b2c
Bryan White <bryanchriswhite@gmail.com>
2024-11-22 11:58:18 +0100

    fix: gateway module gRPC status error returns

--- x/gateway/keeper/msg_update_params.go
+++ x/gateway/keeper/msg_update_params.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/pokt-network/poktroll/x/gateway/types"
 )
@@ -13,16 +15,21 @@ func (k msgServer) UpdateParams(
 	req *types.MsgUpdateParams,
 ) (*types.MsgUpdateParamsResponse, error) {
 	if err := req.ValidateBasic(); err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 	if k.GetAuthority() != req.Authority {
-		return nil, types.ErrGatewayInvalidSigner.Wrapf("invalid authority; expected %s, got %s", k.GetAuthority(), req.Authority)
+		return nil, status.Error(
+			codes.PermissionDenied,
+			types.ErrGatewayInvalidSigner.Wrapf(
+				"invalid authority; expected %s, got %s", k.GetAuthority(), req.Authority,
+			).Error(),
+		)
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	// NOTE(#322): Omitted parameters will be set to their zero value.
 	if err := k.SetParams(ctx, req.Params); err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	return &types.MsgUpdateParamsResponse{}, nil
