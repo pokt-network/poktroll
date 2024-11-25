36148300b012a69227ddc022027b428b498a4369
Bryan White <bryanchriswhite@gmail.com>
2024-11-25 16:37:09 +0100

    fix: outstanding tokenomics module non-status (gRPC) errors

--- x/tokenomics/keeper/msg_update_params.go
+++ x/tokenomics/keeper/msg_update_params.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/pokt-network/poktroll/x/tokenomics/types"
 )
 
@@ -11,21 +14,25 @@ func (k msgServer) UpdateParams(ctx context.Context, msg *types.MsgUpdateParams)
 	logger := k.Logger()
 
 	if err := msg.ValidateBasic(); err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	if msg.Authority != k.GetAuthority() {
-		return nil, types.ErrTokenomicsInvalidSigner.Wrapf(
-			"invalid authority; expected %s, got %s",
-			k.GetAuthority(),
-			msg.Authority,
+		return nil, status.Error(
+			codes.PermissionDenied,
+			types.ErrTokenomicsInvalidSigner.Wrapf(
+				"invalid authority; expected %s, got %s",
+				k.GetAuthority(),
+				msg.Authority,
+			).Error(),
 		)
 	}
 
 	logger.Info(fmt.Sprintf("About to update params from [%v] to [%v]", k.GetParams(ctx), msg.Params))
 
 	if err := k.SetParams(ctx, msg.Params); err != nil {
-		return nil, err
+		err = status.Error(codes.Internal, err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	logger.Info("Done updating params")
