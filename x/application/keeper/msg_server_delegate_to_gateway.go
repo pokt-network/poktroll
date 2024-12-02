f35e87943124ddc2965fc1ffa4c2bba4ce4139ee
Bryan White <bryanchriswhite@gmail.com>
2024-12-02 10:16:18 +0100

    [Code Health] fix: application module gRPC status error returns (#954)
    ## Summary
    
    Ensure all application message and query handlers return gRPC status
    errors.
    
    ## Issue
    
    - #860 
    
    ## Type of change
    
    Select one or more from the following:
    
    - [ ] New feature, functionality or library
    - [x] Consensus breaking; add the `consensus-breaking` label if so. See
    #791 for details
    - [ ] Bug fix
    - [x] Code health or cleanup
    - [ ] Documentation
    - [ ] Other (specify)
    
    ## Testing
    
    - [ ] **Documentation**: `make docusaurus_start`; only needed if you
    make doc changes
    - [x] **Unit Tests**: `make go_develop_and_test`
    - [ ] **LocalNet E2E Tests**: `make test_e2e`
    - [x] **DevNet E2E Tests**: Add the `devnet-test-e2e` label to the PR.
    
    ## Sanity Checklist
    
    - [x] I have tested my changes using the available tooling
    - [ ] I have commented my code
    - [x] I have performed a self-review of my own code; both comments &
    source code
    - [ ] I create and reference any new tickets, if applicable
    - [ ] I have left TODOs throughout the codebase, if applicable

--- x/application/keeper/msg_server_delegate_to_gateway.go
+++ x/application/keeper/msg_server_delegate_to_gateway.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/pokt-network/poktroll/telemetry"
 	apptypes "github.com/pokt-network/poktroll/x/application/types"
@@ -20,44 +22,66 @@ func (k msgServer) DelegateToGateway(ctx context.Context, msg *apptypes.MsgDeleg
 	)
 
 	logger := k.Logger().With("method", "DelegateToGateway")
-	logger.Info(fmt.Sprintf("About to delegate application to gateway with msg: %v", msg))
+	logger.Info(fmt.Sprintf("About to delegate application to gateway with msg: %+v", msg))
 
 	if err := msg.ValidateBasic(); err != nil {
-		logger.Error(fmt.Sprintf("Delegation Message failed basic validation: %v", err))
-		return nil, err
+		logger.Info(fmt.Sprintf("Delegation Message failed basic validation: %s", err))
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	// Retrieve the application from the store
-	app, found := k.GetApplication(ctx, msg.AppAddress)
+	app, found := k.GetApplication(ctx, msg.GetAppAddress())
 	if !found {
 		logger.Info(fmt.Sprintf("Application not found with address [%s]", msg.AppAddress))
-		return nil, apptypes.ErrAppNotFound.Wrapf("application not found with address: %s", msg.AppAddress)
+		return nil, status.Error(
+			codes.NotFound,
+			apptypes.ErrAppNotFound.Wrapf(
+				"application with address: %s", msg.GetAppAddress(),
+			).Error(),
+		)
 	}
 	logger.Info(fmt.Sprintf("Application found with address [%s]", msg.AppAddress))
 
 	// Check if the gateway is staked
-	if _, found := k.gatewayKeeper.GetGateway(ctx, msg.GatewayAddress); !found {
-		logger.Info(fmt.Sprintf("Gateway not found with address [%s]", msg.GatewayAddress))
-		return nil, apptypes.ErrAppGatewayNotFound.Wrapf("gateway not found with address: %s", msg.GatewayAddress)
+	if _, found := k.gatewayKeeper.GetGateway(ctx, msg.GetGatewayAddress()); !found {
+		logger.Info(fmt.Sprintf("Gateway not found with address [%s]", msg.GetGatewayAddress()))
+		return nil, status.Error(
+			codes.NotFound,
+			apptypes.ErrAppGatewayNotFound.Wrapf(
+				"gateway with address: %q", msg.GetGatewayAddress(),
+			).Error(),
+		)
 	}
 
 	// Ensure the application is not already delegated to the maximum number of gateways
 	maxDelegatedParam := k.GetParams(ctx).MaxDelegatedGateways
 	if uint64(len(app.DelegateeGatewayAddresses)) >= maxDelegatedParam {
 		logger.Info(fmt.Sprintf("Application already delegated to maximum number of gateways: %d", maxDelegatedParam))
-		return nil, apptypes.ErrAppMaxDelegatedGateways.Wrapf("application already delegated to %d gateways", maxDelegatedParam)
+		return nil, status.Error(
+			codes.FailedPrecondition,
+			apptypes.ErrAppMaxDelegatedGateways.Wrapf(
+				"application with address %q already delegated to %d (max) gateways",
+				msg.GetAppAddress(), maxDelegatedParam,
+			).Error(),
+		)
 	}
 
 	// Check if the application is already delegated to the gateway
 	for _, gatewayAddr := range app.DelegateeGatewayAddresses {
-		if gatewayAddr == msg.GatewayAddress {
+		if gatewayAddr == msg.GetGatewayAddress() {
 			logger.Info(fmt.Sprintf("Application already delegated to gateway with address [%s]", msg.GatewayAddress))
-			return nil, apptypes.ErrAppAlreadyDelegated.Wrapf("application already delegated to gateway with address: %s", msg.GatewayAddress)
+			return nil, status.Error(
+				codes.AlreadyExists,
+				apptypes.ErrAppAlreadyDelegated.Wrapf(
+					"application with address %q already delegated to gateway with address: %q",
+					msg.GetAppAddress(), msg.GetGatewayAddress(),
+				).Error(),
+			)
 		}
 	}
 
 	// Update the application with the new delegatee public key
-	app.DelegateeGatewayAddresses = append(app.DelegateeGatewayAddresses, msg.GatewayAddress)
+	app.DelegateeGatewayAddresses = append(app.DelegateeGatewayAddresses, msg.GetGatewayAddress())
 	logger.Info("Successfully added delegatee public key to application")
 
 	// Update the application store with the new delegation
@@ -72,12 +96,13 @@ func (k msgServer) DelegateToGateway(ctx context.Context, msg *apptypes.MsgDeleg
 		Application:      &app,
 		SessionEndHeight: sessionEndHeight,
 	}
-	logger.Info(fmt.Sprintf("Emitting application redelegation event %v", event))
+	logger.Info(fmt.Sprintf("Emitting application redelegation event %+v", event))
 
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	if err := sdkCtx.EventManager().EmitTypedEvent(event); err != nil {
-		logger.Error(fmt.Sprintf("Failed to emit application redelegation event: %v", err))
-		return nil, err
+		err = fmt.Errorf("failed to emit application redelegation event: %w", err)
+		logger.Error(err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	isSuccessful = true
