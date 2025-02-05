0cc0a3270c8f2ddf42a7980df9205ed4ee5cd2cc
Redouane Lakrache <r3d0ne@gmail.com>
2025-02-06 00:21:35 +0100

    fix: Test cache missing parameter

--- testutil/integration/suites/application.go
+++ testutil/integration/suites/application.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pokt-network/poktroll/pkg/client"
 	"github.com/pokt-network/poktroll/pkg/client/query"
 	"github.com/pokt-network/poktroll/pkg/client/query/cache"
+	"github.com/pokt-network/poktroll/pkg/polylog"
 	apptypes "github.com/pokt-network/poktroll/x/application/types"
 	sharedtypes "github.com/pokt-network/poktroll/x/shared/types"
 )
@@ -26,8 +27,9 @@ type ApplicationModuleSuite struct {
 // GetAppQueryClient constructs and returns a query client for the application
 // module of the integration app.
 func (s *ApplicationModuleSuite) GetAppQueryClient() client.ApplicationQueryClient {
-	appCache := cache.NewKeyValueCache[apptypes.Application]()
-	appParamsCache := cache.NewParamsCache[apptypes.Params]()
+	logger := polylog.Ctx(s.GetApp().QueryHelper().Ctx)
+	appCache := cache.NewKeyValueCache[apptypes.Application](logger)
+	appParamsCache := cache.NewParamsCache[apptypes.Params](logger)
 	deps := depinject.Supply(s.GetApp().QueryHelper(), appCache, appParamsCache)
 	appQueryClient, err := query.NewApplicationQuerier(deps)
 	require.NoError(s.T(), err)
