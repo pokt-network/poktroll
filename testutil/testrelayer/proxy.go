d05287e98ec163b9de7c528da4fc3cb10884e2f3
eddyzags <zagabe.ed@gmail.com>
2025-02-15 18:29:40 +0100

    improve comment for testrelayer proxy withping

--- testutil/testrelayer/proxy.go
+++ testutil/testrelayer/proxy.go
@@ -37,10 +37,10 @@ func NewMockOneTimeRelayerProxy(
 	return relayerProxyMock
 }
 
-// NewMockOneTimeRelayerProxyWithPing creates a new mock RelayerProxy. This mock
-// RelayerProxy will expect a call to ServedRelays with the given context, and
-// when that call is made, returnedRelaysObs is returned. It also expects a call
-// to Start, Ping, and Stop with the given context.
+// NewMockOneTimeRelayerProxyWithPing creates a new mock RelayerProxy that:
+// - Expects a call to ServedRelays with the given context
+// - Returns returnedRelaysObs when ServedRelays is called
+// - Expects one call each to Start, Ping, and Stop with the given context
 func NewMockOneTimeRelayerProxyWithPing(
 	ctx context.Context,
 	t *testing.T,
