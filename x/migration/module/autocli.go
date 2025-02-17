5af7f01b9135db3988227b449b85a83ebecedebb
Bryan White <bryanchriswhite@gmail.com>
2025-02-17 14:51:26 +0100

    chore: self review improvements

--- x/migration/module/autocli.go
+++ x/migration/module/autocli.go
@@ -56,6 +56,8 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Short:          "Claim the account balance of the given Morse account address",
 					Long:           "Claim the account balance of the given Morse account address, by signing the message with the private key of the Morse account.",
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "morse_src_address"}, {ProtoField: "morse_signature"}},
+					Skip:           true, // skipped because autoCLI cannot handle signing
+					// TODO_UPNEXT(@bryanchriswhite#1034): Add morse account claiming CLI.
 				},
 				// this line is used by ignite scaffolding # autocli/tx
 			},
