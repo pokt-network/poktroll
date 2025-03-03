5411e25d8f0def08efc7b70f2997ea948f8c5b6e
Bryan White <bryanchriswhite@gmail.com>
2025-03-03 13:16:30 +0100

    feat: implement `MsgClaimMorseGateway`

--- x/migration/keeper/msg_server_claim_morse_gateway.go
+++ x/migration/keeper/msg_server_claim_morse_gateway.go
@@ -2,17 +2,133 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
-	"github.com/pokt-network/poktroll/x/migration/types"
+	"github.com/pokt-network/poktroll/app/volatile"
+	gatewaytypes "github.com/pokt-network/poktroll/x/gateway/types"
+	migrationtypes "github.com/pokt-network/poktroll/x/migration/types"
 )
 
-func (k msgServer) ClaimMorseGateway(goCtx context.Context, msg *types.MsgClaimMorseGateway) (*types.MsgClaimMorseGatewayResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+func (k msgServer) ClaimMorseGateway(ctx context.Context, msg *migrationtypes.MsgClaimMorseGateway) (*migrationtypes.MsgClaimMorseGatewayResponse, error) {
+	sdkCtx := cosmostypes.UnwrapSDKContext(ctx)
+	logger := k.Logger().With("method", "ClaimMorseGateway")
 
-	// TODO_UPNEXT(@bryanchriswhite, #1034): Handling the message
-	_ = ctx
+	if err := msg.ValidateBasic(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
 
-	return &types.MsgClaimMorseGatewayResponse{}, nil
+	// DEV_NOTE: It is safe to use MustAccAddressFromBech32 here because the
+	// shannonDestAddress is validated in MsgClaimMorseGateway#ValidateBasic().
+	shannonAccAddr := cosmostypes.MustAccAddressFromBech32(msg.ShannonDestAddress)
+
+	// Ensure that a MorseClaimableAccount exists for the given morseSrcAddress.
+	morseClaimableAccount, isFound := k.GetMorseClaimableAccount(
+		sdkCtx,
+		msg.MorseSrcAddress,
+	)
+	if !isFound {
+		return nil, status.Error(
+			codes.NotFound,
+			migrationtypes.ErrMorseGatewayClaim.Wrapf(
+				"no morse claimable account exists with address %q",
+				msg.MorseSrcAddress,
+			).Error(),
+		)
+	}
+
+	// Ensure that the given MorseClaimableAccount has not already been claimed.
+	if morseClaimableAccount.IsClaimed() {
+		return nil, status.Error(
+			codes.FailedPrecondition,
+			migrationtypes.ErrMorseGatewayClaim.Wrapf(
+				"morse address %q has already been claimed at height %d by shannon address %q",
+				msg.MorseSrcAddress,
+				morseClaimableAccount.ClaimedAtHeight,
+				morseClaimableAccount.ShannonDestAddress,
+			).Error(),
+		)
+	}
+
+	// Mint the totalTokens to the shannonDestAddress account balance.
+	// The gateway stake is subsequently escrowed from the shannonDestAddress account balance.
+	if err := k.MintClaimedMorseTokens(ctx, shannonAccAddr, morseClaimableAccount.TotalTokens()); err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	// Set ShannonDestAddress & ClaimedAtHeight (claim).
+	morseClaimableAccount.ShannonDestAddress = shannonAccAddr.String()
+	morseClaimableAccount.ClaimedAtHeight = sdkCtx.BlockHeight()
+
+	// Update the MorseClaimableAccount.
+	k.SetMorseClaimableAccount(
+		sdkCtx,
+		morseClaimableAccount,
+	)
+
+	msgStakeGateway := gatewaytypes.NewMsgStakeGateway(
+		shannonAccAddr.String(),
+		msg.Stake,
+	)
+
+	initialGatewayStake := cosmostypes.NewInt64Coin(volatile.DenomuPOKT, 0)
+	foundGateway, isFound := k.gatewayKeeper.GetGateway(ctx, shannonAccAddr.String())
+	if isFound {
+		initialGatewayStake = *foundGateway.Stake
+	}
+
+	gateway, err := k.gatewayKeeper.StakeGateway(ctx, logger, msgStakeGateway)
+	if err != nil {
+		// DEV_NOTE: StakeGateway SHOULD ALWAYS return a gRPC status error.
+		return nil, err
+	}
+
+	// DEV_NOTE: While "down-staking" isn't currently supported for gateways,
+	// it MAY be in the future. When BOTH:
+	// - the claimed Shannon account is already staked as a gateway
+	// - the MsgClaimMorseGateway stake amount ("default" or otherwise)
+	//   is less than the current gateway stake amount
+	// then, claimedGatewayStake is set to zero as it would otherwise result in a negative amount.
+	// This value is only used in event(s) and the msg response.
+	claimedGatewayStake, err := gateway.Stake.SafeSub(initialGatewayStake)
+	if err != nil {
+		if !strings.Contains(err.Error(), "negative coin amount") {
+			return nil, err
+		}
+		claimedGatewayStake = cosmostypes.NewInt64Coin(volatile.DenomuPOKT, 0)
+	}
+
+	claimedUnstakedTokens := morseClaimableAccount.TotalTokens().Sub(claimedGatewayStake)
+
+	// Emit an event which signals that the morse account has been claimed.
+	event := migrationtypes.EventMorseGatewayClaimed{
+		ShannonDestAddress:  msg.ShannonDestAddress,
+		MorseSrcAddress:     msg.MorseSrcAddress,
+		ClaimedBalance:      claimedUnstakedTokens,
+		ClaimedGatewayStake: claimedGatewayStake,
+		ClaimedAtHeight:     sdkCtx.BlockHeight(),
+		Gateway:             gateway,
+	}
+	if err = sdkCtx.EventManager().EmitTypedEvent(&event); err != nil {
+		return nil, status.Error(
+			codes.Internal,
+			migrationtypes.ErrMorseGatewayClaim.Wrapf(
+				"failed to emit event type %T: %v",
+				&event,
+				err,
+			).Error(),
+		)
+	}
+
+	// Return the response.
+	return &migrationtypes.MsgClaimMorseGatewayResponse{
+		MorseSrcAddress:     msg.MorseSrcAddress,
+		ClaimedBalance:      claimedUnstakedTokens,
+		ClaimedGatewayStake: claimedGatewayStake,
+		ClaimedAtHeight:     sdkCtx.BlockHeight(),
+		Gateway:             gateway,
+	}, nil
 }
--- x/migration/types/message_claim_morse_gateway.go
+++ x/migration/types/message_claim_morse_gateway.go
@@ -2,29 +2,105 @@ package types
 
 import (
 	errorsmod "cosmossdk.io/errors"
+	cometcrypto "github.com/cometbft/cometbft/crypto"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/gogoproto/proto"
+
+	"github.com/pokt-network/poktroll/app/volatile"
 )
 
 var _ sdk.Msg = &MsgClaimMorseGateway{}
 
-func NewMsgClaimMorseGateway(shannonDestAddress string, morseSrcAddress string, morseSignature string, stake sdk.Coin) *MsgClaimMorseGateway {
-	// TODO_UPNEXT(@bryanchriswhite, #1034): Receive an optional private key (instead of the signature); if present, sign the message.
-
-	return &MsgClaimMorseGateway{
+// NewMsgClaimMorseGateway creates a new MsgClaimMorseGateway.
+// If morsePrivateKey is provided (i.e. not nil), it is used to sign the message.
+func NewMsgClaimMorseGateway(
+	shannonDestAddress string,
+	morseSrcAddress string,
+	morsePrivateKey cometcrypto.PrivKey,
+	stake sdk.Coin,
+) (*MsgClaimMorseGateway, error) {
+	msg := &MsgClaimMorseGateway{
 		ShannonDestAddress: shannonDestAddress,
 		MorseSrcAddress:    morseSrcAddress,
-		MorseSignature:     morseSignature,
 		Stake:              stake,
 	}
+
+	if morsePrivateKey != nil {
+		if err := msg.SignMorseSignature(morsePrivateKey); err != nil {
+			return nil, err
+		}
+	}
+
+	return msg, nil
 }
 
+// ValidateBasic ensures that:
+// - The morseSignature length is valid (signature validation performed elsewhere).
+// - The morseSrcAddress is valid (i.e. it is a valid hex-encoded address).
+// - The shannonDestAddress is valid (i.e. it is a valid bech32 address).
 func (msg *MsgClaimMorseGateway) ValidateBasic() error {
-	// TODO_UPNEXT(@bryanchriswhite, #1034): Add validation.
+	if len(msg.MorseSignature) != MorseSignatureLengthBytes {
+		return ErrMorseGatewayClaim.Wrapf(
+			"invalid morse signature length; expected %d, got %d",
+			MorseSignatureLengthBytes, len(msg.MorseSignature),
+		)
+	}
 
-	_, err := sdk.AccAddressFromBech32(msg.ShannonDestAddress)
-	if err != nil {
+	if len(msg.MorseSrcAddress) != MorseAddressHexLengthBytes {
+		return ErrMorseGatewayClaim.Wrapf("invalid morseSrcAddress length (%d)", len(msg.MorseSrcAddress))
+	}
+
+	if _, err := sdk.AccAddressFromBech32(msg.ShannonDestAddress); err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid shannonDestAddress address (%s)", err)
 	}
+
+	// If msg.Stake is nil, it will default to the sake amount recorded in the corresponding MorseClaimableAccount.
+	if msg.Stake.Denom != volatile.DenomuPOKT {
+		return ErrMorseGatewayClaim.Wrapf("invalid stake denom (%s)", msg.Stake.Denom)
+	}
+
+	if msg.Stake.IsValid() && msg.Stake.IsZero() {
+		return ErrMorseGatewayClaim.Wrapf("invalid stake amount (%s)", msg.Stake.String())
+	}
+
 	return nil
+}
+
+// SignMorseSignature signs the given MsgClaimMorseGateway with the given Morse private key.
+func (msg *MsgClaimMorseGateway) SignMorseSignature(morsePrivKey cometcrypto.PrivKey) (err error) {
+	signingMsgBz, err := msg.getSigningBytes()
+	if err != nil {
+		return err
+	}
+
+	msg.MorseSignature, err = morsePrivKey.Sign(signingMsgBz)
+	return err
+}
+
+// ValidateMorseSignature validates the signature of the given MsgClaimMorseGateway
+// matches the given Morse public key.
+func (msg *MsgClaimMorseGateway) ValidateMorseSignature(morsePublicKey cometcrypto.PubKey) error {
+	signingMsgBz, err := msg.getSigningBytes()
+	if err != nil {
+		return err
+	}
+
+	// Validate the morse signature.
+	if !morsePublicKey.VerifySignature(signingMsgBz, msg.MorseSignature) {
+		return ErrMorseAccountClaim.Wrapf("morseSignature is invalid")
+	}
+
+	return nil
+}
+
+// getSigningBytes returns the canonical byte representation of the MsgClaimMorseGateway
+// which is used for signing and/or signature validation.
+func (msg *MsgClaimMorseGateway) getSigningBytes() ([]byte, error) {
+	// Copy msg and clear the morse signature field (ONLY on the copy) to prevent
+	// it from being included in the signature validation.
+	signingMsg := *msg
+	signingMsg.MorseSignature = nil
+
+	return proto.Marshal(&signingMsg)
 }
