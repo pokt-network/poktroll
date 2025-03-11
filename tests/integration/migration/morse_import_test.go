9513d6b9a909c5015b02872966fbe9d4e2b84b7a
Bryan White <bryanchriswhite@gmail.com>
2025-03-11 11:54:14 +0100

    [Morse->Shannon Migration] implement `MsgClaimMorseApplication` handler (#1082)
    ## Summary
    
    Implement `MsgClaimMorseApplication` message handler. It should stake,
    or upstake and replace the service config, a Shannon application, given
    a previously imported (and unclaimed) `MorseClaimableAccount`.
    
    ## Issue
    
    - Issue: #1034
    
    ## Type of change
    
    Select one or more from the following:
    
    - [x] New feature, functionality or library
    - [ ] Consensus breaking; add the `consensus-breaking` label if so. See
    #791 for details
    - [ ] Bug fix
    - [ ] Code health or cleanup
    - [ ] Documentation
    - [ ] Other (specify)
    
    ## Sanity Checklist
    
    - [x] I have updated the GitHub Issue `assignees`, `reviewers`,
    `labels`, `project`, `iteration` and `milestone`
    - [ ] For docs, I have run `make docusaurus_start`
    - [x] For code, I have run `make go_develop_and_test` and `make
    test_e2e`
    - [ ] For code, I have added the `devnet-test-e2e` label to run E2E
    tests in CI
    - [ ] For configurations, I have update the documentation
    - [ ] I added TODOs where applicable
    
    ---------
    
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    Co-authored-by: red-0ne <red-0ne@users.noreply.github.com>

---
+++ tests/integration/migration/morse_import_test.go
@@ -0,0 +1,140 @@
+package migration
+
+import (
+	"testing"
+
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/pokt-network/poktroll/app/volatile"
+	"github.com/pokt-network/poktroll/testutil/integration/suites"
+	"github.com/pokt-network/poktroll/testutil/sample"
+	"github.com/pokt-network/poktroll/testutil/testmigration"
+	apptypes "github.com/pokt-network/poktroll/x/application/types"
+	migrationtypes "github.com/pokt-network/poktroll/x/migration/types"
+	sharedtypes "github.com/pokt-network/poktroll/x/shared/types"
+	suppliertypes "github.com/pokt-network/poktroll/x/supplier/types"
+)
+
+const (
+	testServiceId                    = "svc1"
+	mockMorseAccountStateHash        = "d7469245aabadc98330f79eef9fb544aa3df0c7cbeabfc3f994fd419b2661633"
+	defaultNumMorseClaimableAccounts = 10
+)
+
+var defaultTestMinStake = cosmostypes.NewInt64Coin(volatile.DenomuPOKT, 100)
+
+type MigrationModuleTestSuite struct {
+	suites.MigrationModuleSuite
+
+	// numMorseClaimableAccounts is the number of morse claimable accounts to
+	// generate when calling #GenerateMorseAccountState.
+	numMorseClaimableAccounts int
+
+	// minStake is used to set the on-chain min stake for the application & supplier modules.
+	minStake cosmostypes.Coin
+
+	// AppServiceConfig is the service config to be used when claiming morse accounts.
+	// It is assigned in the #SetupTest method.
+	appServiceConfig *sharedtypes.ApplicationServiceConfig
+}
+
+func (s *MigrationModuleTestSuite) SetupTest() {
+	s.ResetTestApp(defaultNumMorseClaimableAccounts, defaultTestMinStake)
+}
+
+// ResetTestApp re-runs the #SetupTest logic with the given parameters.
+func (s *MigrationModuleTestSuite) ResetTestApp(
+	numMorseClaimableAccounts int,
+	minStake cosmostypes.Coin,
+) {
+	s.minStake = minStake
+
+	// Set the default application min stake.
+	// DEV_NOTE: This is simpler than modifying genesis or on-chain params.
+	apptypes.DefaultMinStake = s.minStake
+	suppliertypes.DefaultMinStake = s.minStake
+
+	// Initialize a new integration app for the suite.
+	s.NewApp(s.T())
+
+	s.numMorseClaimableAccounts = numMorseClaimableAccounts
+	s.appServiceConfig = &sharedtypes.ApplicationServiceConfig{ServiceId: testServiceId}
+
+	// Assign the app to nested suites.
+	s.AppSuite.SetApp(s.GetApp())
+}
+
+func TestMigrationModuleSuite(t *testing.T) {
+	suite.Run(t, &MigrationModuleTestSuite{})
+}
+
+// TestImportMorseClaimableAccounts exercises importing and persistence of morse claimable accounts.
+func (s *MigrationModuleTestSuite) TestImportMorseClaimableAccounts() {
+	s.GenerateMorseAccountState(s.T(), s.numMorseClaimableAccounts, testmigration.RoundRobinAllMorseAccountActorTypes)
+	msgImportRes := s.ImportMorseClaimableAccounts(s.T())
+	morseAccountState := s.GetAccountState(s.T())
+	morseAccountStateHash, err := morseAccountState.GetHash()
+	require.NoError(s.T(), err)
+
+	expectedMsgImportRes := &migrationtypes.MsgImportMorseClaimableAccountsResponse{
+		StateHash:   morseAccountStateHash,
+		NumAccounts: uint64(s.numMorseClaimableAccounts),
+	}
+	require.Equal(s.T(), expectedMsgImportRes, msgImportRes)
+
+	foundMorseClaimableAccounts := s.QueryAllMorseClaimableAccounts(s.T())
+	require.Equal(s.T(), s.numMorseClaimableAccounts, len(foundMorseClaimableAccounts))
+
+	for _, expectedMorseClaimableAccount := range morseAccountState.Accounts {
+		isFound := false
+		for _, foundMorseClaimableAccount := range foundMorseClaimableAccounts {
+			if foundMorseClaimableAccount.GetMorseSrcAddress() == expectedMorseClaimableAccount.GetMorseSrcAddress() {
+				require.Equal(s.T(), *expectedMorseClaimableAccount, foundMorseClaimableAccount)
+				isFound = true
+				break
+			}
+		}
+		require.True(s.T(), isFound)
+	}
+}
+
+// TestImportMorseClaimableAccounts_ErrorInvalidAuthority tests the error case when the authority address is invalid.
+func (s *MigrationModuleTestSuite) TestImportMorseClaimableAccounts_ErrorInvalidAuthority() {
+	s.GenerateMorseAccountState(s.T(), s.numMorseClaimableAccounts, testmigration.RoundRobinAllMorseAccountActorTypes)
+
+	// random authority address
+	invalidAuthority := sample.AccAddress()
+	msgImport, err := migrationtypes.NewMsgImportMorseClaimableAccounts(
+		invalidAuthority,
+		*s.GetAccountState(s.T()),
+	)
+	require.NoError(s.T(), err)
+
+	// Import Morse claimable accounts.
+	_, err = s.GetApp().RunMsg(s.T(), msgImport)
+
+	expectedErr := migrationtypes.ErrInvalidSigner.Wrapf("invalid authority address (%s)", invalidAuthority)
+	require.ErrorContains(s.T(), err, expectedErr.Error())
+}
+
+// TestImportMorseClaimableAccounts_ErrorInvalidHash tests the error case when the hash is invalid.
+func (s *MigrationModuleTestSuite) TestImportMorseClaimableAccounts_ErrorInvalidHash() {
+	s.GenerateMorseAccountState(s.T(), s.numMorseClaimableAccounts, testmigration.RoundRobinAllMorseAccountActorTypes)
+
+	msgImport, err := migrationtypes.NewMsgImportMorseClaimableAccounts(
+		sample.AccAddress(), // random authority address
+		*s.GetAccountState(s.T()),
+	)
+	require.NoError(s.T(), err)
+
+	// Set an invalid hash.
+	msgImport.MorseAccountStateHash = []byte(mockMorseAccountStateHash)
+
+	// Import Morse claimable accounts.
+	_, err = s.GetApp().RunMsg(s.T(), msgImport)
+
+	expectedErr := migrationtypes.ErrMorseAccountsImport.Wrapf("invalid MorseAccountStateHash size")
+	require.ErrorContains(s.T(), err, expectedErr.Error())
+}
--- x/migration/types/message_claim_morse_application_test.go
+++ x/migration/types/message_claim_morse_application_test.go
@@ -1,29 +1,88 @@
-package types
+package types_test
 
 import (
 	"testing"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/pokt-network/poktroll/testutil/sample"
 	"github.com/stretchr/testify/require"
+
+	"github.com/pokt-network/poktroll/testutil/sample"
+	migrationtypes "github.com/pokt-network/poktroll/x/migration/types"
+	sharedtypes "github.com/pokt-network/poktroll/x/shared/types"
 )
+
+const testServiceId = "svc1"
 
 func TestMsgClaimMorseApplication_ValidateBasic(t *testing.T) {
 	tests := []struct {
 		name string
-		msg  MsgClaimMorseApplication
+		msg  migrationtypes.MsgClaimMorseApplication
 		err  error
 	}{
 		{
-			name: "invalid address",
-			msg: MsgClaimMorseApplication{
+			name: "invalid ShannonDestAddress",
+			msg: migrationtypes.MsgClaimMorseApplication{
 				ShannonDestAddress: "invalid_address",
+				MorseSrcAddress:    sample.MorseAddressHex(),
+				MorseSignature:     mockMorseSignature,
+				ServiceConfig: &sharedtypes.ApplicationServiceConfig{
+					ServiceId: testServiceId,
+				},
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
-			name: "valid address",
-			msg: MsgClaimMorseApplication{
+			name: "invalid MorseSrcAddress",
+			msg: migrationtypes.MsgClaimMorseApplication{
 				ShannonDestAddress: sample.AccAddress(),
+				MorseSrcAddress:    "invalid_address",
+				MorseSignature:     mockMorseSignature,
+				ServiceConfig: &sharedtypes.ApplicationServiceConfig{
+					ServiceId: testServiceId,
+				},
+			},
+			err: migrationtypes.ErrMorseApplicationClaim,
+		}, {
+			name: "invalid service ID (empty)",
+			msg: migrationtypes.MsgClaimMorseApplication{
+				ShannonDestAddress: sample.AccAddress(),
+				MorseSrcAddress:    sample.MorseAddressHex(),
+				MorseSignature:     mockMorseSignature,
+				ServiceConfig: &sharedtypes.ApplicationServiceConfig{
+					ServiceId: "",
+				},
+			},
+			err: migrationtypes.ErrMorseApplicationClaim,
+		}, {
+			name: "invalid service ID (too long)",
+			msg: migrationtypes.MsgClaimMorseApplication{
+				ShannonDestAddress: sample.AccAddress(),
+				MorseSrcAddress:    sample.MorseAddressHex(),
+				MorseSignature:     mockMorseSignature,
+				ServiceConfig: &sharedtypes.ApplicationServiceConfig{
+					ServiceId: "xxxxxxxxxxxxxxxxxxxx",
+				},
+			},
+			err: migrationtypes.ErrMorseApplicationClaim,
+		}, {
+			name: "invalid empty MorseSignature",
+			msg: migrationtypes.MsgClaimMorseApplication{
+				ShannonDestAddress: sample.AccAddress(),
+				MorseSrcAddress:    sample.MorseAddressHex(),
+				MorseSignature:     nil,
+				ServiceConfig: &sharedtypes.ApplicationServiceConfig{
+					ServiceId: testServiceId,
+				},
+			},
+			err: migrationtypes.ErrMorseApplicationClaim,
+		}, {
+			name: "valid claim message",
+			msg: migrationtypes.MsgClaimMorseApplication{
+				ShannonDestAddress: sample.AccAddress(),
+				MorseSrcAddress:    sample.MorseAddressHex(),
+				MorseSignature:     mockMorseSignature,
+				ServiceConfig: &sharedtypes.ApplicationServiceConfig{
+					ServiceId: testServiceId,
+				},
 			},
 		},
 	}
