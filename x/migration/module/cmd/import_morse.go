3a42ce195111ffd7b4a65dd089a820a1f06c09eb
Bryan White <bryanchriswhite@gmail.com>
2025-03-14 14:11:37 +0100

    moveme: feat: poktrolld tx migration import-morse-accounts

---
+++ x/migration/module/cmd/import_morse.go
@@ -0,0 +1,163 @@
+package cmd
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"cosmossdk.io/depinject"
+	cmtjson "github.com/cometbft/cometbft/libs/json"
+	cosmosclient "github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	cosmostx "github.com/cosmos/cosmos-sdk/client/tx"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/spf13/cobra"
+
+	"github.com/pokt-network/poktroll/pkg/client/tx"
+	txtypes "github.com/pokt-network/poktroll/pkg/client/tx/types"
+	"github.com/pokt-network/poktroll/pkg/deps/config"
+	migrationtypes "github.com/pokt-network/poktroll/x/migration/types"
+)
+
+// We're `explicitly omitting default` so the relayer crashes if these aren't specified.
+const omittedDefaultFlagValue = "explicitly omitting default"
+
+// TODO_IN_THIS_COMMIT: godoc...
+func importMorseAccountsCmd() *cobra.Command {
+	importMorseAcctsCmd := &cobra.Command{
+		Use: "import-morse-accounts [morse-account-state-json-path]",
+		// TODO_IN_THIS_COMMIT:
+		// Short: ,
+		// Long: ,
+		Args: cobra.ExactArgs(1),
+		RunE: runImportMorseAccounts,
+	}
+
+	flags.AddTxFlagsToCmd(importMorseAcctsCmd)
+
+	// DEV_NOTE: This is required by the TxClient. Despite this being a "tx" command,
+	// the TxClient still "queries" for its own TxResult events.
+	importMorseAcctsCmd.Flags().String(flags.FlagGRPC, omittedDefaultFlagValue, "Register the default Cosmos node grpc flag, which is needed to initialize the Cosmos query context with grpc correctly. It can be used to override the `QueryNodeGRPCURL` field in the config file if specified.")
+	// TODO_IN_THIS_COMMIT: explain...
+	importMorseAcctsCmd.Flags().Bool(flags.FlagGRPCInsecure, true, "Used to initialize the Cosmos query context with grpc security options. It can be used to override the `QueryNodeGRPCInsecure` field in the config file if specified.")
+
+	return importMorseAcctsCmd
+}
+
+func runImportMorseAccounts(cmd *cobra.Command, args []string) error {
+	ctx := cmd.Context()
+	// Ensure the morse account state file exists.
+	accountStatePath := args[0]
+	if _, err := os.Stat(accountStatePath); err != nil {
+		return err
+	}
+
+	// Read and deserialize it.
+	morseAccountStateBz, err := os.ReadFile(accountStatePath)
+	if err != nil {
+		return err
+	}
+
+	morseAccountState := new(migrationtypes.MorseAccountState)
+	err = cmtjson.Unmarshal(morseAccountStateBz, morseAccountState)
+	if err != nil {
+		return err
+	}
+
+	queryNodeRPCUrlString, err := cmd.Flags().GetString(flags.FlagNode)
+	if err != nil {
+		return err
+	}
+
+	queryNodeRPCUrl, err := url.Parse(queryNodeRPCUrlString)
+	if err != nil {
+		return err
+	}
+
+	deps, err := config.SupplyConfig(ctx, cmd, []config.SupplierFn{
+		config.NewSupplyEventsQueryClientFn(queryNodeRPCUrl),
+		config.NewSupplyBlockQueryClientFn(queryNodeRPCUrl),
+		config.NewSupplyBlockClientFn(queryNodeRPCUrl),
+	})
+	if err != nil {
+		return err
+	}
+	//deps, err := supplyImportMorseAccountsDeps(ctx, cmd)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Extract the cosmos-sdk client context from the depinject config.
+	//var txClientCtx txtypes.Context
+	//if err = depinject.Inject(deps, &txClientCtx); err != nil {
+	//	return err
+	//}
+	//clientCtx := cosmosclient.Context(txClientCtx)
+
+	// Conventionally derive a cosmos-sdk client context from the cobra command.
+	fmt.Println("ONE: >>>>>>>>>>")
+	clientCtx, err := cosmosclient.GetClientTxContext(cmd)
+	if err != nil {
+		return err
+	}
+
+	// Conventionally construct a txClient and its dependencies.
+	fmt.Println("TWO: >>>>>>>>>>")
+	clientFactory, err := cosmostx.NewFactoryCLI(clientCtx, cmd.Flags())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("THREE: >>>>>>>>>>")
+	deps = depinject.Configs(deps, depinject.Supply(
+		txtypes.Context(clientCtx),
+		clientFactory,
+	))
+	txCtx, err := tx.NewTxContext(deps)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("FOUR: >>>>>>>>>>")
+	deps = depinject.Configs(deps, depinject.Supply(txCtx))
+	txClient, err := tx.NewTxClient(ctx, deps, tx.WithSigningKeyName(clientCtx.FromName))
+	if err != nil {
+		return err
+	}
+
+	// Construct a MsgImportMorseAccountState message.
+	msgImportMorseAccountState, err := migrationtypes.NewMsgImportMorseClaimableAccounts(
+		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+		*morseAccountState,
+	)
+	if err != nil {
+		return err
+	}
+
+	// Package the MsgImportMorseAccountState message into a MsgAuthzExec message.
+	msgAuthzExec := authz.NewMsgExec(
+		clientCtx.FromAddress,
+		[]cosmostypes.Msg{msgImportMorseAccountState},
+	)
+
+	// Sign and broadcast the claim Morse account message.
+	eitherErr := txClient.SignAndBroadcast(ctx, &msgAuthzExec)
+	err, errCh := eitherErr.SyncOrAsyncError()
+	if err != nil {
+		return err
+	}
+
+	// Wait for an async error, timeout, or the errCh to close on success.
+	return <-errCh
+}
+
+//// TODO_IN_THIS_COMMIT: godoc & move...
+//func supplyImportMorseAccountsDeps(
+//	ctx context.Context,
+//	cmd *cobra.Command,
+//) (depinject.Config, error) {
+//
+//}
