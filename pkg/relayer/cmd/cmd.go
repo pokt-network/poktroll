403b26baf64d955bb23835f8b891bc38f049446a
Bryan White <bryanchriswhite@gmail.com>
2025-03-20 13:06:14 +0100

    chore: self-review improvements

--- pkg/relayer/cmd/cmd.go
+++ pkg/relayer/cmd/cmd.go
@@ -79,7 +79,6 @@ for such operations.`,
 	cmd.Flags().String(cosmosflags.FlagKeyringBackend, "", "Select keyring's backend (os|file|kwallet|pass|test)")
 	// We're `explicitly omitting default` so the relayer crashes if these aren't specified.
 	cmd.Flags().StringVar(&flagNodeRPCURL, cosmosflags.FlagNode, flags.OmittedDefaultFlagValue, "Register the default Cosmos node flag, which is needed to initialize the Cosmos query and tx contexts correctly. It can be used to override the `QueryNodeRPCURL` and `TxNodeRPCURL` fields in the config file if specified.")
-	// We're `explicitly omitting default` so the relayer crashes if these aren't specified.
 	cmd.Flags().StringVar(&flagNodeGRPCURL, cosmosflags.FlagGRPC, flags.OmittedDefaultFlagValue, "Register the default Cosmos node grpc flag, which is needed to initialize the Cosmos query context with grpc correctly. It can be used to override the `QueryNodeGRPCURL` field in the config file if specified.")
 	cmd.Flags().Bool(cosmosflags.FlagGRPCInsecure, true, "Used to initialize the Cosmos query context with grpc security options. It can be used to override the `QueryNodeGRPCInsecure` field in the config file if specified.")
 	cmd.Flags().String(cosmosflags.FlagChainID, "poktroll", "The network chain ID")
