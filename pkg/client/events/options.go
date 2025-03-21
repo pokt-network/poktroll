00df380933ef5700b7cfcfcabf370705bc281185
Dmitry K <okdas@pm.me>
2025-03-21 16:37:52 -0700

    quick review

--- pkg/client/events/options.go
+++ pkg/client/events/options.go
@@ -14,19 +14,6 @@ func WithDialer(dialer client.Dialer) client.EventsQueryClientOption {
 // of times the replay client should retry in the event that it encounters
 // an error or its connection is interrupted.
 // If connRetryLimit is < 0, it will retry indefinitely.
-//
-// For production environments, it's recommended to use a negative value
-// (e.g., -1) to enable unlimited retries with exponential backoff.
-// Example usage:
-//
-//	client, err := events.NewEventsReplayClient(
-//		ctx,
-//		deps,
-//		queryString,
-//		newEventFn,
-//		replayObsBufferSize,
-//		events.WithConnRetryLimit[MyEventType](-1), // Unlimited retries
-//	)
 func WithConnRetryLimit[T any](limit int) client.EventsReplayClientOption[T] {
 	return func(client client.EventsReplayClient[T]) {
 		// Ignore the zero value because limit may be provided via a partially
