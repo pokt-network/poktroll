acc2bae3e4aec0be2ad5cbe6f19a949a9892b0fa
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-03-25 23:54:47 -0700

    Finished reviewing all cosmos default configs in config.yml

--- cmd/poktrolld/cmd/config.go
+++ cmd/poktrolld/cmd/config.go
@@ -37,24 +37,29 @@ import (
 // - GRPC.Enable: true
 // - GRPCWeb.Enable: true
 //
+// For other default configurations used, see:
+// - cmtcfg.DefaultConfig()
+// - serverconfig.DefaultConfig()
+// - telemetry.DefaultConfig()
+// - serverconfig.DefaultConfigTemplate
+//
 // ### DO NOT DELETE - DEFAULT GENESIS CONFIGS FOR REFERENCE ###
 
 var once sync.Once
 
 // PoktrollAppConfig represents a poktroll-specific part of `app.toml` file.
-// Checkout `customAppConfigTemplate()` for additional information about each setting.
 type PoktrollAppConfig struct {
 	Telemetry telemetry.PoktrollTelemetryConfig `mapstructure:"telemetry"`
 }
 
 // poktrollAppConfigDefaults sets default values to render in `app.toml`.
-// Checkout `customAppConfigTemplate()` for additional information about each config parameter.
 func poktrollAppConfigDefaults() PoktrollAppConfig {
 	return PoktrollAppConfig{
 		Telemetry: telemetry.DefaultConfig(),
 	}
 }
 
+// InitSDKConfig initializes the SDK configuration with the appropriate parameters via a singleton.
 func InitSDKConfig() {
 	once.Do(func() {
 		checkOrInitSDKConfig()
@@ -63,25 +68,23 @@ func InitSDKConfig() {
 
 // checkOrInitSDKConfig updates the prefixes for all account types and seals the config.
 // DEV_NOTE: Due to the separation of this repo and the SDK, where the config is also sealed,
-// we have an added check to return early in case the config has already been set to the expected
-// value.
+// we have an added check to return early in case the config has already been set to the expected value.
 func checkOrInitSDKConfig() {
 	config := sdk.GetConfig()
 
 	// Check if the config is already set with the correct prefixes
 	if config.GetBech32AccountAddrPrefix() == app.AccountAddressPrefix {
-		// Config is already initialized, return early
-		return
+		return // config is already initialized, return early
 	}
 
-	// Set prefixes
+	// Set prefixes for all account types (validator, consensus, public key, etc.)
 	accountPubKeyPrefix := app.AccountAddressPrefix + "pub"
 	validatorAddressPrefix := app.AccountAddressPrefix + "valoper"
 	validatorPubKeyPrefix := app.AccountAddressPrefix + "valoperpub"
 	consNodeAddressPrefix := app.AccountAddressPrefix + "valcons"
 	consNodePubKeyPrefix := app.AccountAddressPrefix + "valconspub"
 
-	// Set and seal config
+	// Set and seal config (enforce the pokt network's prefixes)
 	config.SetBech32PrefixForAccount(app.AccountAddressPrefix, accountPubKeyPrefix)
 	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
 	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
@@ -106,12 +109,8 @@ func checkOrInitSDKConfig() {
 func initCometBFTConfig() *cmtcfg.Config {
 	cfg := cmtcfg.DefaultConfig()
 
-	// these values put a higher strain on node memory
-	// cfg.P2P.MaxNumInboundPeers = 100
-	// cfg.P2P.MaxNumOutboundPeers = 40
-
-	// BlockTime configuration
-	// BlockTime is 1 minute.
+	// BlockTime configuration: 1 minute.
+	//
 	// BlockTime is a function of all of these parameters but ultimately dictated by TimeoutCommit.
 	// TimeoutCommit is the **minimum** time between blocks establishing a baseline.
 	cfg.Consensus.TimeoutPropose = 60 * time.Second
@@ -122,6 +121,13 @@ func initCometBFTConfig() *cmtcfg.Config {
 	cfg.Consensus.TimeoutPrecommitDelta = 5 * time.Second
 	cfg.Consensus.TimeoutCommit = 60 * time.Second
 
+	// P2P configuration values.
+	//
+	// These values put a higher strain on node memory
+	// cfg.P2P.MaxNumInboundPeers = 100
+	// cfg.P2P.MaxNumOutboundPeers = 40
+
+	// CometBFT instrumentation and logging configuration.
 	cfg.Instrumentation.Prometheus = true
 	cfg.LogLevel = "info"
 
@@ -130,42 +136,52 @@ func initCometBFTConfig() *cmtcfg.Config {
 
 // initAppConfig helps to override default appConfig (app.toml) template and configs.
 // These values are going to be rendered into the config file on `poktrolld init`.
-// return "", nil if no custom configuration is required for the application.
-// TODO_MAINNET: Reconsider values - check `app.toml` for possible options.
+// Return "", nil if no custom configuration is required for the application.
 func initAppConfig() (string, interface{}) {
 	type CustomAppConfig struct {
 		serverconfig.Config `mapstructure:",squash"`
 		Poktroll            PoktrollAppConfig `mapstructure:"poktroll"`
 	}
 
-	// Optionally allow the chain developer to overwrite the SDK's default server config.
+	// Optionally, allow the chain developer to overwrite the SDK's default server config.
 	srvCfg := serverconfig.DefaultConfig()
 
 	// The SDK's default minimum gas price is set to "" (empty value) inside app.toml.
+	//
 	// If left empty by validators, the node will halt on startup.
 	// However, the chain developer can set a default app.toml value for their validators here.
 	//
 	// In summary:
 	// - if you leave srvCfg.MinGasPrices = "", all validators MUST tweak their own app.toml config
-	// - if you set srvCfg.MinGasPrices non-empty, validators CAN tweak their own app.toml to override,
-	//   or use this default value.
-	//
-	// In tests, we set the min gas prices to 0.
-	// srvCfg.MinGasPrices = "0stake"
-	// srvCfg.BaseConfig.IAVLDisableFastNode = true // disable fastnode by default
+	// - if you set srvCfg.MinGasPrices non-empty, validators CAN tweak their own app.toml to override OR use this default value.
 
-	// Default configuration settings
-	srvCfg.MinGasPrices = "0.000000001upokt" // Also adjust ignite's `config.yml`.
-	srvCfg.Mempool.MaxTxs = 10000
-	srvCfg.Telemetry.Enabled = true
+	// Default Application configuration settings
+	srvCfg.MinGasPrices = "0.000000001upokt"
+	srvCfg.Mempool.MaxTxs = 100000 // 100k max transactions per block
+
 	// Positive non-zero value turns on Prometheus support.
 	// Prometheus metrics are removed from the exporter when retention time is reached.
 	srvCfg.Telemetry.PrometheusRetentionTime = 60 * 60 * 24 // in seconds.
+	srvCfg.Telemetry.Enabled = true
 	srvCfg.Telemetry.MetricsSink = "mem"
+
 	srvCfg.Pruning = "nothing" // archiving node by default
+
+	// TODO_IN_THIS_PR: Align `RPCMaxBodyBytes` with Beta TestNet
+	// Controls how large any single RPC request accepted by the CometBFT server (offchain) can be.
+	srvCfg.API.RPCMaxBodyBytes = 100000000 // 95MB
+	srvCfg.API.RPCReadTimeout = 60         // 1 minute
+	srvCfg.API.RPCReadTimeout = 60         // 1 minute
 	srvCfg.API.Enable = true
+	srvCfg.API.Swagger = true
+
 	srvCfg.GRPC.Enable = true
+	srvCfg.GRPC.MaxRecvMsgSize = 100000000 // 95MB
 	srvCfg.GRPCWeb.Enable = true
+
+	// For testing only: set the min gas prices to 0.
+	// srvCfg.MinGasPrices = "0stake"
+	// srvCfg.BaseConfig.IAVLDisableFastNode = true // disable fastnode mode
 
 	// Create the custom config with both server and poktroll configs
 	customAppConfig := CustomAppConfig{
@@ -193,14 +209,19 @@ func customPoktrollAppConfigTemplate() string {
 
 		# Cardinality level for telemetry metrics collection
 		# This controls the level of detail (number of unique labels) in metrics.
+		#
 		# Options:
+		#
 		#   - "low":    Collects basic metrics with low cardinality.
-		#              Suitable for production environments with tight performance constraints.
+		#               Suitable for production environments with tight performance constraints.
+		#
 		#   - "medium": Collects a moderate number of labels, balancing detail and performance.
-		#              Suitable for moderate workloads or staging environments.
+		#               Suitable for moderate workloads or staging environments.
+		#
 		#   - "high":   WARNING: WILL CAUSE STRESS TO YOUR MONITORING ENVIRONMENT! Collects detailed metrics with high
-		#              cardinality, including labels with many unique values (e.g., application_id, session_id).
-		#              Recommended for debugging or testing environments.
+		#               cardinality, including labels with many unique values (e.g., application_id, session_id).
+		#               Recommended for debugging or testing environments.
+		#
 		cardinality-level = "{{ .Poktroll.Telemetry.CardinalityLevel }}"
 		`
 }
