a80bc207ec87890cb6665e1d1e736ad824d2260e
Bryan White <bryanchriswhite@gmail.com>
2025-03-27 13:18:26 +0100

    chore: cachebust migration E2E tests

--- makefiles/migrate.mk
+++ makefiles/migrate.mk
@@ -38,11 +38,11 @@ claim_morse_account: check_go_version check_from_key_name check_morse_private_ke
 
 .PHONY: test_e2e_migration_fixture
 test_e2e_migration_fixture: test_e2e_env ## Run only the E2E suite that exercises the migration module using fixture data.
-	go test -v ./e2e/tests/... -tags=e2e,oneshot --run=MigrationWithFixtureData
+	go test -v ./e2e/tests/... -count=1 -tags=e2e,oneshot --run=MigrationWithFixtureData
 
 .PHONY: test_e2e_migration_snapshot
 test_e2e_migration_snapshot: test_e2e_env ## Run only the E2E suite that exercises the migration module using local snapshot data.
-	go test -v ./e2e/tests/... -tags=e2e,oneshot,manual --run=MigrationWithSnapshotData
+	go test -v ./e2e/tests/... -count=1 -tags=e2e,oneshot,manual --run=MigrationWithSnapshotData
 
 #########################
 ### Migration Helpers ###
