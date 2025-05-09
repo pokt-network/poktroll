6203eb350ee4d77e38d559e5208aaeceea40aede
Bryan White <bryanchriswhite@gmail.com>
2025-05-09 22:51:54 +0200

    [Morse -> Shannon Migration] chore: prepare v0.1.11 upgrade for submission (#1306)
    ## Summary
    
    Prepares v0.1.11 onchain network upgrade for application.
    
    - Add authz grant for `MsgRecoverMorseAccount`
    - Add `allow_morse_import_override` migration module param
      - Set it to `true` for Alpha and Beta TestNets
    
    ## Issue
    
    - #1294 
    
    ## Type of change
    
    Select one or more from the following:
    
    - [ ] New feature, functionality or library
    - [ ] Bug fix
    - [ ] Code health or cleanup
    - [ ] Documentation
    - [x] Other (specify): upgrade
    
    ## Sanity Checklist
    
    - [ ] I have updated the GitHub Issue Metadata: `assignees`,
    `reviewers`, `labels`, `project`, `iteration` and `milestone`
    - [ ] For docs: `make docusaurus_start`
    - [ ] For small changes: `make go_develop_and_test` and `make test_e2e`
    - [ ] For major changes: `devnet-test-e2e` label to run E2E tests in CI
    - [ ] For migration changes: `make test_e2e_oneshot`
    - [ ] 'TODO's, configurations and other docs

--- app/upgrades.go
+++ app/upgrades.go
@@ -58,9 +58,8 @@ var allUpgrades = []upgrades.Upgrade{
 	// v0.1.10 - upgrade to fix chain halts caused by the previous upgrade.
 	// upgrades.Upgrade_0_1_10,
 
-	// TODO_MAINNET_MIGRATION(@bryanchriswhite): Update the upgrade version number.
-	// v0.1.x - upgrade to fix chain halts caused by the previous upgrade.
-	upgrades.Upgrade_0_1_x,
+	// v0.1.11 - upgrade to add allow_morse_account_import_overwrite param.
+	upgrades.Upgrade_0_1_11,
 }
 
 // setUpgrades sets upgrade handlers for all upgrades and executes KVStore migration if an upgrade plan file exists.
---
+++ app/upgrades/v0.1.11.go
@@ -0,0 +1,95 @@
+package upgrades
+
+import (
+	"context"
+
+	cosmoslog "cosmossdk.io/log"
+	storetypes "cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/pokt-network/poktroll/app/keepers"
+)
+
+const (
+	Upgrade_0_1_11_PlanName = "v0.1.11"
+)
+
+// Upgrade_0_1_11 handles the upgrade to release `v0.1.11`.
+// This upgrade adds:
+// - the `allow_morse_account_import_overwrite` migration module param
+//   - Set to `false` by default
+//   - Set to `true` on Alpha and Beta TestNets
+//
+// - a corresponding authz grant
+// - new `MsgRecoverMorseAccount` message with empty message handlers (scaffolding)
+// https://github.com/pokt-network/poktroll/compare/v0.1.10..v0.1.11
+var Upgrade_0_1_11 = Upgrade{
+	PlanName: Upgrade_0_1_11_PlanName,
+	// No migrations in this upgrade.
+	StoreUpgrades: storetypes.StoreUpgrades{},
+
+	// Upgrade Handler
+	CreateUpgradeHandler: func(
+		mm *module.Manager,
+		keepers *keepers.Keepers,
+		configurator module.Configurator,
+	) upgradetypes.UpgradeHandler {
+		// Add new parameters by:
+		// 1. Inspecting the diff between v0.1.10...v0.1.11
+		// 2. Manually inspect changes in ignite's config.yml
+		// 3. Update the upgrade handler here accordingly
+		// Ref: https://github.com/pokt-network/poktroll/compare/v0.1.10...v0.1.11
+		applyNewParameters := func(ctx context.Context, logger cosmoslog.Logger) (err error) {
+			logger.Info("Starting parameter updates", "upgrade_plan_name", Upgrade_0_1_11_PlanName)
+
+			// Get the current migration module params
+			migrationParams := keepers.MigrationKeeper.GetParams(ctx)
+
+			// Set allow_morse_account_import_overwrite to:
+			// - True for Alpha and Beta TestNets
+			// - False for ALL other chain IDs (e.g. MainNet)
+			switch cosmostypes.UnwrapSDKContext(ctx).ChainID() {
+			case AlphaTestNetChainId, BetaTestNetChainId:
+				migrationParams.AllowMorseAccountImportOverwrite = true
+			default:
+				migrationParams.AllowMorseAccountImportOverwrite = false
+			}
+
+			// Ensure that the new parameters are valid
+			if err = migrationParams.Validate(); err != nil {
+				logger.Error("Failed to validate migration params", "error", err)
+				return err
+			}
+
+			// ALL parameters in the migration module must be specified when
+			// setting parameters, even if just one is being CRUDed.
+			err = keepers.MigrationKeeper.SetParams(ctx, migrationParams)
+			if err != nil {
+				logger.Error("Failed to set migration params", "error", err)
+				return err
+			}
+			logger.Info("Successfully updated migration params", "new_params", migrationParams)
+
+			return nil
+		}
+
+		return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+			logger := cosmostypes.UnwrapSDKContext(ctx).Logger()
+
+			// Adds new authz authorizations from the diff:
+			// https://github.com/pokt-network/poktroll/compare/v0.1.10...v0.1.11
+			grantAuthorizationMessages := []string{"/pocket.migration.MsgRecoverMorseAccount"}
+			if err := applyNewAuthorizations(ctx, keepers, logger, grantAuthorizationMessages); err != nil {
+				return vm, err
+			}
+
+			if err := applyNewParameters(ctx, logger); err != nil {
+				return vm, err
+			}
+
+			return vm, nil
+		}
+	},
+}
