cb65e657b684e9468078ab945eaef1cc60b9769e
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-05-16 17:26:35 -0400

    Fix compilation errors

--- pkg/relayer/cmd/deps.go
+++ pkg/relayer/cmd/deps.go
@@ -6,23 +6,15 @@ import (
 	"net/url"
 
 	"cosmossdk.io/depinject"
-	cosmosclient "github.com/cosmos/cosmos-sdk/client"
 	cosmosflags "github.com/cosmos/cosmos-sdk/client/flags"
-	cosmostx "github.com/cosmos/cosmos-sdk/client/tx"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
 
 	"github.com/pokt-network/poktroll/cmd/flags"
 	"github.com/pokt-network/poktroll/pkg/client/query"
 	"github.com/pokt-network/poktroll/pkg/client/query/cache"
-	"github.com/pokt-network/poktroll/pkg/client/tx"
-	txtypes "github.com/pokt-network/poktroll/pkg/client/tx/types"
 	"github.com/pokt-network/poktroll/pkg/deps/config"
 	relayerconfig "github.com/pokt-network/poktroll/pkg/relayer/config"
-	"github.com/pokt-network/poktroll/pkg/relayer/miner"
-	"github.com/pokt-network/poktroll/pkg/relayer/proxy"
-	"github.com/pokt-network/poktroll/pkg/relayer/relay_authenticator"
-	"github.com/pokt-network/poktroll/pkg/relayer/session"
 	apptypes "github.com/pokt-network/poktroll/x/application/types"
 	prooftypes "github.com/pokt-network/poktroll/x/proof/types"
 	servicetypes "github.com/pokt-network/poktroll/x/service/types"
@@ -108,220 +100,22 @@ func setupRelayerDependencies(
 		config.NewSupplyServiceQueryClientFn(),
 		config.NewSupplyApplicationQuerierFn(),
 		config.NewSupplySessionQuerierFn(),
-		supplyRelayMeter,
-		supplyMiner,
+		config.SupplyRelayMeter,
+		config.SupplyMiner,
 		config.NewSupplyAccountQuerierFn(),
 		config.NewSupplyBankQuerierFn(),
 		config.NewSupplySupplierQuerierFn(),
 		config.NewSupplyProofQueryClientFn(),
 		config.NewSupplyRingClientFn(),
-		supplyTxFactory,
-		supplyTxContext,
+		config.SupplyTxFactory,
+		config.SupplyTxContext,
 		// RelayMiner always uses tx simulation for gas estimation (variable by tx).
 		// Always use "auto" gas setting for RelayMiner.
 		config.NewSupplySupplierClientsFn(signingKeyNames, cosmosflags.GasFlagAuto),
-		newSupplyRelayAuthenticatorFn(signingKeyNames),
-		newSupplyRelayerProxyFn(servicesConfigMap),
-		newSupplyRelayerSessionsManagerFn(smtStorePath),
+		config.NewSupplyRelayAuthenticatorFn(signingKeyNames),
+		config.NewSupplyRelayerProxyFn(servicesConfigMap),
+		config.NewSupplyRelayerSessionsManagerFn(smtStorePath),
 	}
 
 	return config.SupplyConfig(ctx, cmd, supplierFuncs)
-}
-
-// supplyMiner constructs a Miner instance and returns a new depinject.Config with it supplied.
-//
-// - Supplies Miner to the dependency injection config
-// - Returns updated config and error if any
-//
-// Parameters:
-//   - ctx: Context for the function
-//   - deps: Dependency injection config
-//   - cmd: Cobra command
-//
-// Returns:
-//   - depinject.Config: Updated dependency injection config
-//   - error: Error if setup fails
-func supplyMiner(
-	_ context.Context,
-	deps depinject.Config,
-	_ *cobra.Command,
-) (depinject.Config, error) {
-	mnr, err := miner.NewMiner(deps)
-	if err != nil {
-		return nil, err
-	}
-
-	return depinject.Configs(deps, depinject.Supply(mnr)), nil
-}
-
-// supplyRelayMeter constructs a RelayMeter instance and returns a new depinject.Config with it supplied.
-//
-// - Supplies RelayMeter to the dependency injection config
-// - Returns updated config and error if any
-//
-// Parameters:
-//   - ctx: Context for the function
-//   - deps: Dependency injection config
-//   - cmd: Cobra command
-//
-// Returns:
-//   - depinject.Config: Updated dependency injection config
-//   - error: Error if setup fails
-func supplyRelayMeter(
-	_ context.Context,
-	deps depinject.Config,
-	_ *cobra.Command,
-) (depinject.Config, error) {
-	rm, err := proxy.NewRelayMeter(deps)
-	if err != nil {
-		return nil, err
-	}
-
-	return depinject.Configs(deps, depinject.Supply(rm)), nil
-}
-
-// supplyTxFactory constructs a cosmostx.Factory instance and returns a new depinject.Config with it supplied.
-//
-// - Supplies TxFactory to the dependency injection config
-// - Returns updated config and error if any
-//
-// Parameters:
-//   - ctx: Context for the function
-//   - deps: Dependency injection config
-//   - cmd: Cobra command
-//
-// Returns:
-//   - depinject.Config: Updated dependency injection config
-//   - error: Error if setup fails
-func supplyTxFactory(
-	_ context.Context,
-	deps depinject.Config,
-	cmd *cobra.Command,
-) (depinject.Config, error) {
-	var txClientCtx txtypes.Context
-	if err := depinject.Inject(deps, &txClientCtx); err != nil {
-		return nil, err
-	}
-
-	clientCtx := cosmosclient.Context(txClientCtx)
-	clientFactory, err := cosmostx.NewFactoryCLI(clientCtx, cmd.Flags())
-	if err != nil {
-		return nil, err
-	}
-
-	return depinject.Configs(deps, depinject.Supply(clientFactory)), nil
-}
-
-// supplyTxContext constructs a transaction context and returns a new depinject.Config with it supplied.
-//
-// - Supplies TxContext to the dependency injection config
-// - Returns updated config and error if any
-//
-// Parameters:
-//   - ctx: Context for the function
-//   - deps: Dependency injection config
-//   - cmd: Cobra command
-//
-// Returns:
-//   - depinject.Config: Updated dependency injection config
-//   - error: Error if setup fails
-func supplyTxContext(
-	_ context.Context,
-	deps depinject.Config,
-	_ *cobra.Command,
-) (depinject.Config, error) {
-	txContext, err := tx.NewTxContext(deps)
-	if err != nil {
-		return nil, err
-	}
-
-	return depinject.Configs(deps, depinject.Supply(txContext)), nil
-}
-
-// newSupplyRelayAuthenticatorFn returns a function which constructs a RelayAuthenticator and returns a new depinject.Config with it supplied.
-//
-// - Accepts signingKeyNames for authenticator setup
-// - Returns a SupplierFn for dependency injection
-//
-// Parameters:
-//   - signingKeyNames: List of signing key names
-//
-// Returns:
-//   - config.SupplierFn: Supplier function for dependency injection
-func newSupplyRelayAuthenticatorFn(
-	signingKeyNames []string,
-) config.SupplierFn {
-	return func(
-		ctx context.Context,
-		deps depinject.Config,
-		_ *cobra.Command,
-	) (depinject.Config, error) {
-		relayAuthenticator, err := relay_authenticator.NewRelayAuthenticator(
-			deps,
-			relay_authenticator.WithSigningKeyNames(signingKeyNames),
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		return depinject.Configs(deps, depinject.Supply(relayAuthenticator)), nil
-	}
-}
-
-// newSupplyRelayerProxyFn returns a function which constructs a RelayerProxy and returns a new depinject.Config with it supplied.
-//
-// - Accepts servicesConfigMap for proxy setup
-// - Returns a SupplierFn for dependency injection
-//
-// Parameters:
-//   - servicesConfigMap: Map of services configuration
-//
-// Returns:
-//   - config.SupplierFn: Supplier function for dependency injection
-func newSupplyRelayerProxyFn(
-	servicesConfigMap map[string]*relayerconfig.RelayMinerServerConfig,
-) config.SupplierFn {
-	return func(
-		_ context.Context,
-		deps depinject.Config,
-		_ *cobra.Command,
-	) (depinject.Config, error) {
-		relayerProxy, err := proxy.NewRelayerProxy(
-			deps,
-			proxy.WithServicesConfigMap(servicesConfigMap),
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		return depinject.Configs(deps, depinject.Supply(relayerProxy)), nil
-	}
-}
-
-// newSupplyRelayerSessionsManagerFn returns a function which constructs a RelayerSessionsManager and returns a new depinject.Config with it supplied.
-//
-// - Accepts smtStorePath for sessions manager setup
-// - Returns a SupplierFn for dependency injection
-//
-// Parameters:
-//   - smtStorePath: Path to the sessions store
-//
-// Returns:
-//   - config.SupplierFn: Supplier function for dependency injection
-func newSupplyRelayerSessionsManagerFn(smtStorePath string) config.SupplierFn {
-	return func(
-		ctx context.Context,
-		deps depinject.Config,
-		_ *cobra.Command,
-	) (depinject.Config, error) {
-		relayerSessionsManager, err := session.NewRelayerSessions(
-			deps,
-			session.WithStoresDirectory(smtStorePath),
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		return depinject.Configs(deps, depinject.Supply(relayerSessionsManager)), nil
-	}
 }
