1cb171399a343599b6a7166204987ff47085e3bd
Jorge S. Cuesta <jckpo2@gmail.com>
2025-05-28 18:46:42 -0400

    Refactor and enhance a Morse supplier claim process
    Improved the claim process to support custodial and non-custodial modes. Enhanced revenue share calculations to ensure 100% distribution and dynamically include owner shares. Added detailed documentation and error handling for key functions, improving maintainability and usability.

--- x/migration/module/cmd/claim_morse_supplier_bulk.go
+++ x/migration/module/cmd/claim_morse_supplier_bulk.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	cosmossdk "github.com/cosmos/cosmos-sdk/types"
@@ -36,28 +37,54 @@ func ClaimSupplierBulkCmd() *cobra.Command {
 	claimSuppliersCmd := &cobra.Command{
 		Use:   "claim-suppliers --from [shannon_dest_key_name]",
 		Args:  cobra.ExactArgs(0),
-		Short: "Claim many onchain MorseClaimableAccount as a staked supplier accounts> Claim multiple on-chain MorseClaimableAccounts as staked supplier accounts.",
+		Short: "Claim many onchain MorseClaimableAccount as a staked supplier accounts.",
 		Long: `Claim multiple on-chain MorseClaimableAccounts as staked supplier accounts.
+Pre-Requisites:
+- If Morse node is non-custodial, the output address needs to be claimed on Shannon with MsgClaimMorseAccount.
+
+What it does:
+- Reads Morse node keys and stake template from files.
+- Migrates/claims each Node account to a new Shannon supplier account (custodial or non-custodial).
+- Generates and stores keys for each supplier.
+- Supports simulation mode—doesn't broadcast the transaction if enabled.
+- Outputs migration results to a JSON file.
+- Optional: Can export unarmored (plain) JSON output with sensitive keys, if flagged as unsafe.
 
 Flags:
 --nodes - Operator keys json list
 --stake-template-file - Path to a stake template file detailing services, reward shares, etc. The owner, operator, and stake amount are automatically sourced from ClaimableAccounts.
 --simulate - (Default: false) If true, the transaction will be simulated but not broadcasted.
 --output-file - (Default: migration_output.json) Path to a file where the migration result will be written.
---unsafe
---unarmored-json 
+--unsafe - (Default: false) allow the usage of --unarmored-json flag
+--unarmored-json - (Default: false) allow JSON marshalling of the migration result to include morse/shannon private keys on it.
 
 {
-	"migrated_map": [
-		{
-			morse: {address:"", private_key:""},
-			shannon: {address:"", private_key:""}
-		}
-	],
-	"owner_address_not_claimed_yet": ["address1", "address2", "...", "addressN"],
-	"error": "",
-	"tx_hash": "",
-	"tx_code": 0,
+  "mappings": [
+    {
+      "morse": {
+        "address": "<MORSE_ADDRESS>",
+		"private_key": ".... only if --unsafe --unarmored-json"
+      },
+      "shannon": {
+        "address": "<SHANNON_ADDRESS>",
+		"private_key": ".... only if --unsafe --unarmored-json"
+      },
+      "migration_msg": {
+        "shannon_signing_address": "<SHANNON_SIGNING_ADDRESS>",
+        "shannon_owner_address": "SHANNON_CLAIMED_ADDRESS_OF_MORSE_OUTPUT_ADDRESS",
+        "shannon_operator_address": "SHANNON_GENERATED_ADDRESS",
+        "morse_node_address": "MORSE_NODE_ADDRESS_OF_PRIVATE_KEY_READ_FROM_FILE",
+        "morse_public_key": "MORSE_NODE_PUBLIC_KEY_OF_PRIVATE_KEY_READ_FROM_FILE",
+        "morse_signature": "....",
+		"signer_is_output_address": false,
+		"services": [...STAKE_SERVICES_FROM_YAML_TEMPLATE_FILE...]
+      },
+      "error": ""
+    }
+  ],
+  "error": "",
+  "tx_hash": "<TX_HASH>",
+  "tx_code": <TX_CODE>
 }
 
 // I need to generate the shannon private keys for every operator - why? because olshansky fuck on me about me previous cli cmd :'(
@@ -68,7 +95,7 @@ operator_address: [DEDUCTED SHANNON NODE ADDRESS] - no need to set
 stake_amount: [DEDUCTED SHANNON NODE STAKE AMOUNT] - no need to set
 default_rev_share_percent:
   <DELEGATOR_REWARDS_SHANNON_ADDRESS>: 75
-  [DEDUCTED SHANNON OWNER ADDRESS]: 100 - $DELEGATOR_REWARDS_SHANNON_ADDRESS = 25
+  [DEDUCTED SHANNON OWNER ADDRESS]: [DEDUCTED OWNER SHARE - NO NEED TO INCLUDE] -> 100 - $DELEGATOR_REWARDS_SHANNON_ADDRESS = 25
 services:
   - service_id: "anvil"
     endpoints:
@@ -222,6 +249,7 @@ func runClaimSuppliers(cmd *cobra.Command, _ []string) error {
 	claimMessages := make([]cosmossdk.Msg, 0)
 
 	for i := range nodeMorseKeys {
+		custodial := false
 		morseNode := nodeMorseKeys[i]
 		// this will ensure the morse output address is not empty, which means is a node
 		claimableMorseNode, morseNodeError := loadMorseClaimableAccount(
@@ -250,6 +278,7 @@ func runClaimSuppliers(cmd *cobra.Command, _ []string) error {
 				ownerAddressMap[claimableMorseNode.MorseOutputAddress] = claimableMorseAccount
 			} else {
 				// custodial
+				custodial = true
 				ownerAddressMap[claimableMorseNode.MorseOutputAddress] = claimableMorseNode
 			}
 		}
@@ -273,12 +302,15 @@ func runClaimSuppliers(cmd *cobra.Command, _ []string) error {
 			},
 		}
 
-		// TODO: add owner dynamically here somehow to the default_rev_share or per service rev_share...
-		//  I dont like the idea of need to set the owner because that restrict to only import nodes from specific output_address
-		//  so I want to add them to complete the requirement of 100%
+		ownerAddress := ownerAddressMap[claimableMorseNode.MorseOutputAddress].ShannonDestAddress
+
+		if custodial {
+			ownerAddress = morseShannonMapping.ShannonAccount.Address.String()
+		}
+
 		supplierStakeConfig, supplierStakeConfigErr := buildSupplierStakeConfig(
 			// this needs to be claimed as a protocol rule
-			ownerAddressMap[claimableMorseNode.MorseOutputAddress].ShannonDestAddress,
+			ownerAddress,
 			shannonOperatorAddress,
 			templateSupplierStakeConfig,
 		)
@@ -301,7 +333,6 @@ func runClaimSuppliers(cmd *cobra.Command, _ []string) error {
 		}
 
 		// Import a Shannon private key into a keyring.
-
 		keyringErr := clientCtx.Keyring.ImportPrivKeyHex(
 			shannonOperatorAddress,
 			hex.EncodeToString(morseShannonMapping.ShannonAccount.PrivateKey.Key),
@@ -312,7 +343,7 @@ func runClaimSuppliers(cmd *cobra.Command, _ []string) error {
 			return keyringErr
 		}
 
-		// For debugging: record migration message attempted.
+		// For debugging: a record migration message attempted.
 		morseShannonMapping.MigrationMsg = msgClaimMorseSupplier
 		// append to the list of messages that will be delivery on the transaction
 		claimMessages = append(claimMessages, msgClaimMorseSupplier)
@@ -373,7 +404,10 @@ func runClaimSuppliers(cmd *cobra.Command, _ []string) error {
 	return nil
 }
 
-// readMorseNodesPrivateKeysFile reads Morse private keys from the nodes-file, validates their claimable status, and his output address is claimed.
+// readMorseNodesPrivateKeysFile reads a file containing Morse private keys and parses them into MorseAccountInfo objects.
+// nodesFile is the path to the input file containing Morse private keys in JSON format.
+// Returns a slice of MorseAccountInfo containing parsed private keys and their corresponding addresses, or an error.
+// Errors are returned if the file does not exist, is improperly formatted, or contains invalid data.
 func readMorseNodesPrivateKeysFile(nodesFile string) ([]MorseAccountInfo, error) {
 	// Prepare a new morse private keys slice.
 	var morsePrivateKeys []string
@@ -441,14 +475,9 @@ func loadTemplateSupplierStakeConfigYAML(configYAMLPath string) (*config.YAMLSta
 	return &yamlStakeConfig, nil
 }
 
-// loadMorseClaimableAccount loads and validates the MorseClaimableAccount at the
-// provided address.
-//
-// If the MorseClaimableAccount is not claimed yet, this function will return an
-// error.
-//
-// If the MorseClaimableAccount is claimed, this function will return the
-// MorseClaimableAccount.
+// loadMorseClaimableAccount retrieves a MorseClaimableAccount based on the provided address and validates its state.
+// It uses the given context and client context to interact with the blockchain.
+// If shouldBeNode is true, the function ensures the MorseClaimableAccount is associated with a node account.
 func loadMorseClaimableAccount(
 	ctx context.Context,
 	clientCtx cosmosclient.Context,
@@ -478,6 +507,13 @@ func loadMorseClaimableAccount(
 	return &res.MorseClaimableAccount, nil
 }
 
+// buildSupplierStakeConfig creates and validates a SupplierStakeConfig based on given parameters and a YAML template.
+// Parameters:
+// • owner: The owner address (must not be empty).
+// • operator: The operator address (if empty, defaults to the owner address).
+// • templateSupplierStakeConfig: The YAMLStakeConfig template to start from.
+// • default_rev_share|service.*.rev_share: fulfills share to fit 100% with the owner address because it is required.
+// Returns a validated SupplierStakeConfig instance or an error if any validation fails.
 func buildSupplierStakeConfig(
 	owner string,
 	operator string,
@@ -506,6 +542,27 @@ func buildSupplierStakeConfig(
 		return nil, err
 	}
 
+	if len(yamlStakeConfig.DefaultRevSharePercent) != 0 {
+		yamlStakeConfig.DefaultRevSharePercent, err = enforce100Percent(owner, defaultRevShareMap)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for i := range yamlStakeConfig.Services {
+		service := yamlStakeConfig.Services[i]
+		if len(service.RevSharePercent) == 0 {
+			// set the same rev share per service as default which already includes the owner and enforce 100%
+			service.RevSharePercent = defaultRevShareMap
+			continue
+		}
+
+		service.RevSharePercent, err = enforce100Percent(owner, service.RevSharePercent)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	// Validate and parse the service configs.
 	supplierServiceConfigs, err := yamlStakeConfig.ValidateAndParseServiceConfigs(defaultRevShareMap)
 	if err != nil {
@@ -520,4 +577,28 @@ func buildSupplierStakeConfig(
 		// The stake amount is determined by the sum of any existing supplier stake
 		// and the supplier stake amount of the associated MorseClaimableAccount.
 	}, nil
+}
+
+// enforce100Percent ensures that the sum of revenue shares in revShareMap equals 100%,
+// adjusting the owner's share if necessary.
+// If the owner isn't present, it's added to the map with a share that balances the total to 100%.
+// Returns the updated map or an error.
+func enforce100Percent(owner string, revShareMap map[string]uint64) (map[string]uint64, error) {
+	totalShare := uint64(0)
+	ownerFound := false
+
+	for address, share := range revShareMap {
+		totalShare += share
+		if strings.ToLower(address) == strings.ToLower(owner) {
+			ownerFound = true
+		}
+	}
+
+	// if the owner is not part of the list, add it with the difference.
+	// this avoids the need for the user to add it.
+	if !ownerFound && totalShare < 100 && totalShare > 0 {
+		revShareMap[owner] = 100 - totalShare
+	}
+
+	return revShareMap, nil
 }
