ad479e319e1214b0f6d03c1ca26e60c242a100ca
Bryan White <bryanchriswhite@gmail.com>
2025-05-29 14:04:40 +0200

    fix: example recipient address

--- cmd/faucet/fund.go
+++ cmd/faucet/fund.go
@@ -16,7 +16,7 @@ import (
 // The placeholders are intended to be interpolated in the following order:
 //   - baseURL: Fully-qualified URL to the faucet server (e.g. https://shannon-testnet-grove-faucet.beta.poktroll.com)
 //   - denom: the denom to fund (e.g. upokt)
-//   - recipientAddress: the recipient address to fund (e.g. 1A0BB8623F40D2A9BEAC099A0BAFDCAE3C5D8288)
+//   - recipientAddress: the recipient address to fund (e.g. pokt1mrqt5f7qh8uxs27cjm9t7v9e74a9vvdnq5jva4)
 const fundURLFmt = "%s/%s/%s"
 
 var faucetBaseURL string
