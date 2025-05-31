7a9040a154b9ad5015de591ef7fa580f905ec680
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-05-31 15:43:20 -0700

    Checkpoint

--- tools/scripts/params/state_shift_params/update.sh
+++ tools/scripts/params/state_shift_params/update.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# Beta TestNet
+PARAM_DIR="tools/scripts/params/state_shift_params"
+PARAM_FILES=(
+  application_params.json
+  gateway_params.json
+  mint_params.json
+  proof_params.json
+  service_params.json
+  session_params.json
+  shared_params.json
+  slashing_params.json
+  staking_params.json
+  supplier_params.json
+  tokenomics_params.json
+)
 
-SHANNON_BETA_URL=https://shannon-testnet-grove-rpc.beta.poktroll.com
-pocketd tx authz exec ./slashing_params_beta_20250531_153448.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./application_params_beta_20250531_153122.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./gateway_params_beta_20250531_153128.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./mint_params_beta_20250531_153002.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./mint_params_beta_20250531_153315.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./proof_params_beta_20250531_153144.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./service_params_beta_20250531_153131.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./session_params_beta_20250531_153140.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./shared_params_beta_20250531_153203.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./staking_params_beta_20250531_153324.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./supplier_params_beta_20250531_153136.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-pocketd tx authz exec ./tokenomics_params_beta_20250531_152843.json --from=pnf_beta --keyring-backend=test --chain-id=pocket-beta --node=${SHANNON_BETA_URL} --yes --home=~/.pocket_prod --fees=200upokt
-
-# MainNet
-
-pocketd tx authz exec ./slashing_params_beta_20250531_153448.json --from=grove_mainnet_genesis --keyring-backend=test --chain-id=pocket --node=https://shannon-testnet-grove-rpc.poktroll.com --yes --home=~/.pocket_prod --fees=200upokt
+for file in "${PARAM_FILES[@]}"; do
+  pocketd tx authz exec "$PARAM_DIR/$file" \
+    --from=pnf_beta \
+    --keyring-backend=test \
+    --chain-id=pocket-beta \
+    --node="https://shannon-testnet-grove-rpc.beta.poktroll.com \
+    --yes \
+    --home=~/.pocket_prod \
+    --fees=200upokt
+done
