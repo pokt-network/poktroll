ea0136afcab85bdf592a81eaeb290e64f749e3b2
eddyzags <zagabe.ed@gmail.com>
2025-06-02 16:40:21 +0200

    change Token to AuthToken

--- pkg/relayer/cmd/cmd_start.go
+++ pkg/relayer/cmd/cmd_start.go
@@ -158,7 +158,7 @@ func runRelayer(cmd *cobra.Command, _ []string) error {
 	// enable the forward authenticated server to send request to a supplier for a given service name. It can be useful for operational purposes.
 	// More information available here:  https://dev.poktroll.com/operate/configs/relayminer_config#forward
 	if relayMinerConfig.Forward.Enabled {
-		if err = relayMiner.ServeForward(ctx, "tcp", relayMinerConfig.Forward.Addr, relayMinerConfig.Forward.Token); err != nil {
+		if err = relayMiner.ServeForward(ctx, "tcp", relayMinerConfig.Forward.Addr, relayMinerConfig.Forward.AuthToken); err != nil {
 			return fmt.Errorf("serve forward: %w", err)
 		}
 	}
