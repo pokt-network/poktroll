1f4ca50cf812acfba44429272a64a3c23c5c3487
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-06-04 10:51:43 -0700

    Minor edits to x/migration/module/cmd/recover_morse_account.go

--- x/migration/module/cmd/recover_morse_account.go
+++ x/migration/module/cmd/recover_morse_account.go
@@ -19,27 +19,38 @@ func RecoverMorseAccountCmd() *cobra.Command {
 	recoverCmd := &cobra.Command{
 		Use:   "recover-account [morse-src-address] [shannon-dest-address-or-key-name]",
 		Args:  cobra.ExactArgs(2),
-		Short: "Recover a morse account which is unclaimable and on the recoverable accounts allowlist",
-		Long: `Recover a morse account which is unclaimable and on the recoverable accounts allowlist.
+		Short: "Recover a Morse account which is BOTH unclaimable AND on the recoverable accounts allowlist",
+		Long: `Recover a Morse account that is BOTH unclaimable AND on the recoverable accounts allowlist.
 
-The morse account recovery process is an authority-gated operation which may be invoked via authz or governance proposal.
-This CLI command utilizes authz and therefore requires that an authorization exist onchain for the pocket.migration.MsgRecoverMorseAccount message, where the grantee is the message signer (the --from account).
+The morse account recovery process is authority-gated and can be invoked via:
+  - Authz (authorization)
+  - Governance proposal
 
-To check which authz authorizations exists, see: pocketd query authz -h.`,
-		Example: `# Recover the dao module account by Shannon destination key name
+This CLI command uses authz, so YOU MUST have an onchain authorization for the following message: pocket.migration.MsgRecoverMorseAccount.
+
+The authorization grantee (the --from account) MUST be the message signer.
+
+To check existing authz authorizations, run:
+	pocketd query authz -h
+`,
+		Example: `Examples:
+
+# Recover the dao module account by Shannon destination key name
 pocketd tx migration recover-account dao pnf --from=pnf --network=beta
 
 # Recover the dao module account by Shannon destination address
 pocketd tx migration recover-account dao pokt1eeeksh2tvkh7wzmfrljnhw4wrhs55lcuvmekkw --from=pnf
 
 # Recover the dao module account by key name on MainNet with OS keyring backend
-pocketd tx migration recover-account dao pnf --from=pnf --network=main --keyring-backend=os`,
+pocketd tx migration recover-account dao pnf --from=pnf --network=main --keyring-backend=os
+`,
 		RunE: runRecover,
 	}
 
-	// Add Cosmos SDK standard flags to the command
+	// Add standard Cosmos SDK transaction flags
 	cosmosflags.AddTxFlagsToCmd(recoverCmd)
 
+	// Add common pocket specific flags
 	recoverCmd.Flags().String(flags.FlagLogLevel, flags.DefaultLogLevel, flags.FlagLogLevelUsage)
 	recoverCmd.Flags().String(flags.FlagLogOutput, flags.DefaultLogOutput, flags.FlagLogOutputUsage)
 
@@ -72,6 +83,7 @@ func runRecover(cmd *cobra.Command, args []string) error {
 		}
 	}
 
+	// Create the MsgRecoverMorseAccount message.
 	msgRecoverMorseAccount := migrationtypes.NewMsgRecoverMorseAccount(
 		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 		shannonDestAddress.String(),
