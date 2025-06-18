672a1631b0714820edd72abe09d267bc336c1234
Pascal van Leeuwen <pascal@grove.city>
2025-06-18 18:43:00 +0100

    add autogenerated API docs for docusaurus

---
+++ docusaurus/docs/5_api/sidebar.ts
@@ -0,0 +1,7 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [],
+};
+
+export default sidebar.apisidebar;
--- makefiles/docs.mk
+++ makefiles/docs.mk
@@ -14,3 +14,10 @@ docusaurus_start: check_yarn check_node ## Start the Docusaurus server
 .PHONY: docs_update_gov_params_page
 docs_update_gov_params_page: ## Update the page in Docusaurus documenting all the governance parameters
 	go run tools/scripts/docusaurus/generate_docs_params.go
+
+# Uses https://github.com/PaloAltoNetworks/docusaurus-openapi-docs to generate OpenAPI docs.
+# This is a custom plugin for Docusaurus that allows us to embed the OpenAPI spec into the docs.
+# Outputs docs files to docusaurus/docs/4_develop/developer_guide/api/*.mdx
+.PHONY: docusaurus_gen_api_docs
+docusaurus_gen_api_docs: ## Generate docusaurus OpenAPI docs
+	(cd docusaurus && yarn install && yarn docusaurus clean-api-docs pocket && yarn docusaurus gen-api-docs pocket)
