e01d72354ef72fc50175ca2cdbe3f5ef0ce0aba0
Ramiro Rodriguez Colmeiro <rawthil@gmail.com>
2025-06-24 16:26:00 -0300

    Add stream support for CLI relayminer relay cmd

---
+++ pkg/relayer/proxy/http_stream.go
@@ -0,0 +1,148 @@
+package proxy
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/pokt-network/poktroll/pkg/polylog"
+	"github.com/pokt-network/poktroll/x/service/types"
+	sdktypes "github.com/pokt-network/shannon-sdk/types"
+	"google.golang.org/protobuf/proto"
+)
+
+// Target streaming types
+var httpStreamingTypes = []string{
+	"text/event-stream",
+	"application/x-ndjson",
+}
+
+var streamDelimitter = "||POKT_STREAM||"
+
+// Custom split function for POKT events.
+// The POKT streams contains a signature and the body of the request to the
+// backend, so we need a custom delimiter for streaming responses.
+// This functions is a custom delimiter for the Stream functionality
+func ScanEvents(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	// Look for the POKT_STREAM delimiter
+	if i := strings.Index(string(data), streamDelimitter); i >= 0 {
+		// Return chunk without the delimiter
+		return i + len(streamDelimitter), data[0:i], nil
+	}
+
+	// If we're at EOF, return whatever we have
+	if atEOF {
+		return len(data), data, nil
+	}
+
+	// Request more data
+	return 0, nil, nil
+}
+
+// This function looks for the supported streams.
+// We should be able to handle any cunked stream, but we have limited this
+// to the content types specified in the variable `httpStreamingTypes`
+//
+// Returns:
+//   - boolean whether the backend response should be streamed or not
+func IsStreamingResponse(response *http.Response) (isStreaming bool) {
+	// Check if this is a streaming response
+	contentType := response.Header.Get("Content-Type")
+	for _, streamType := range httpStreamingTypes {
+		if strings.Contains(strings.ToLower(contentType), streamType) {
+			isStreaming = true
+		}
+	}
+
+	return
+}
+
+// Handles the streaming backend responses.
+// This function takes a streaming body and:
+// - Reads each chunk
+// - Converts the chunk into a POKT HTTP response structure
+// - Signs each chunk
+// - Writes and flush to the requesting app
+//
+// This will handle any stream where the delimiter is a newline (`\n`)
+func (server *relayMinerHTTPServer) HandleHttpStream(response *http.Response,
+	writer http.ResponseWriter,
+	meta types.RelayRequestMetadata,
+	logger polylog.Logger) (relayResponse *types.RelayResponse, responseSize float64, err error) {
+
+	// Set headers
+	writer.Header().Set("Connection", "close")
+	// Copy the response back to the original request
+	for k, v := range response.Header {
+		writer.Header()[k] = v
+	}
+	writer.WriteHeader(response.StatusCode)
+
+	// Instance the flusher
+	flusher, ok := writer.(http.Flusher)
+	if !ok {
+		logger.Error().Msg("Streaming not supported.")
+		return nil, 0, fmt.Errorf("❌ failed to open stream request")
+	} else {
+		// Create scanner with default delimiter (\n)
+		scanner := bufio.NewScanner(response.Body)
+		// Scan for chunks
+		for scanner.Scan() {
+			line := scanner.Bytes()
+			// Add back the newline that we stripped at Scan
+			line = append(line, '\n')
+
+			// logger.Debug().
+			// 	Int("lineSize", len(line)).
+			// 	// Str("line", string(line)).
+			// 	Msg("Recieved stream chunk.")
+
+			// Create the POKT HTTP response structure
+			poktHTTPResponse := &sdktypes.POKTHTTPResponse{
+				StatusCode: uint32(http.StatusOK),
+				Header:     make(map[string]*sdktypes.Header, 0),
+				BodyBz:     line,
+			}
+			// Deterministic marshaling of response
+			marshalOpts := proto.MarshalOptions{Deterministic: true}
+			poktHTTPResponseBz, err := marshalOpts.Marshal(poktHTTPResponse)
+			if err != nil {
+				return nil, 0, fmt.Errorf("❌ failed to marshal POKT HTTP response: %w", err)
+			}
+
+			// Sign response
+			relayResponse, err = server.newRelayResponse(poktHTTPResponseBz, meta.SessionHeader, meta.SupplierOperatorAddress)
+			if err != nil {
+				return nil, 0, err
+			}
+			// Marshal response
+			signedLine, err := relayResponse.Marshal()
+			if err != nil {
+				return nil, 0, err
+			}
+
+			// track size, the sum of all chunks
+			responseSize += float64(relayResponse.Size())
+
+			// Append custom delimiter (used by app to detect POKT streaming)
+			signedLine = append(signedLine, []byte(streamDelimitter)...)
+
+			// Write to client
+			writer.Write(signedLine)
+
+			// Flush to ensure the stream goes asap to the app
+			flusher.Flush()
+
+			// logger.Debug().
+			// 	Int("responseSize", relayResponse.Size()).
+			// 	Msg("Stream chunk relayed.")
+		}
+	}
+
+	return
+}
