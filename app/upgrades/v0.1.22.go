e798e0d88b77425be824d63b80c2088b0fd1db7a
Bryan White <bryanchriswhite@gmail.com>
2025-06-24 10:12:53 +0200

    [Upgrade] v0.1.22 (#1552)
    ## Summary
    
    Onchain upgrade v0.1.22 
    
    ## Issue
    
    - #1542 
    - #1543
    - #1549 
    - #1550
    
    ## Type of change
    
    Select one or more from the following:
    
    - [ ] New feature, functionality or library
    - [ ] Bug fix
    - [ ] Code health or cleanup
    - [ ] Documentation
    - [x] Other (specify): onchain upgrade
    
    ## Sanity Checklist
    
    - [ ] I have updated the GitHub Issue Metadata: `assignees`,
    `reviewers`, `labels`, `project`, `iteration` and `milestone`
    - [ ] For docs: `make docusaurus_start`
    - [ ] For small changes: `make go_develop_and_test` and `make test_e2e`
    - [ ] For major changes: `devnet-test-e2e` label to run E2E tests in CI
    - [ ] For migration changes: `make test_e2e_oneshot`
    - [ ] 'TODO's, configurations and other docs

--- app/upgrades/v0.1.22.go
+++ app/upgrades/v0.1.22.go
@@ -10,18 +10,15 @@ import (
 	"github.com/pokt-network/poktroll/app/keepers"
 )
 
-// TODO_NEXT_UPGRADE: Rename NEXT with the appropriate next
-// upgrade version number and update comment versions.
-
 const (
-	Upgrade_NEXT_PlanName = "vNEXT"
+	Upgrade_0_1_22_PlanName = "v0.1.22"
 )
 
-// Upgrade_NEXT handles the upgrade to release `vNEXT`.
+// Upgrade_0_1_22 handles the upgrade to release `v0_1_22`.
 // This upgrade adds:
-// - ...
-var Upgrade_NEXT = Upgrade{
-	PlanName: Upgrade_NEXT_PlanName,
+// - Updates to the Morse account recovery allowlist
+var Upgrade_0_1_22 = Upgrade{
+	PlanName: Upgrade_0_1_22_PlanName,
 	// No KVStore migrations in this upgrade.
 	StoreUpgrades: storetypes.StoreUpgrades{},
 
@@ -32,10 +29,10 @@ var Upgrade_NEXT = Upgrade{
 		configurator module.Configurator,
 	) upgradetypes.UpgradeHandler {
 		// Add new parameters by:
-		// 1. Inspecting the diff between vPREV..vNEXT
+		// 1. Inspecting the diff between v0.1.21..v0.1.22
 		// 2. Manually inspect changes in ignite's config.yml
 		// 3. Update the upgrade handler here accordingly
-		// Ref: https://github.com/pokt-network/poktroll/compare/vPREV..vNEXT
+		// Ref: https://github.com/pokt-network/poktroll/compare/v0.1.21..v0.1.22
 
 		return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 			return vm, nil
