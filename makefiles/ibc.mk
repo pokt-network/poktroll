fe1fe9534c1bcc837562aba8ef4df8db999c61ce
Bryan White <bryanchriswhite@gmail.com>
2025-06-25 10:07:05 +0200

    fixup! chore: ibc make targets

--- makefiles/ibc.mk
+++ makefiles/ibc.mk
@@ -3,19 +3,19 @@
 ###############################
 .PHONY: agd_shell
 agd_shell: check_kubectl check_docker_ps check_kind
-	kubectl exec -it $$(kubectl get pods|grep agoric|cut -f 1 -d " ") -- bash
+	kubectl exec -it $$(kubectl get pods|grep agoric-validator|cut -f 1 -d " ") -- bash
 
 .PHONY: agd_query_tx
 agd_query_tx:
-	kubectl exec -it $$(kubectl get pods|grep agoric|cut -f 1 -d " ") -- agd query tx $(TX_HASH) --chain-id=agoriclocal
+	kubectl exec -it $$(kubectl get pods|grep agoric-validator|cut -f 1 -d " ") -- agd query tx $(TX_HASH) --chain-id=agoriclocal
 
 .PHONY: agd_query_tx_json
 agd_query_tx_json:
-	kubectl exec -it $$(kubectl get pods|grep agoric|cut -f 1 -d " ") -- agd query tx $(TX_HASH) --chain-id=agoriclocal -o json
+	kubectl exec -it $$(kubectl get pods|grep agoric-validator|cut -f 1 -d " ") -- agd query tx $(TX_HASH) --chain-id=agoriclocal -o json
 
 .PHONY: fund_agoric_account
 fund_agoric_account: check_kubectl check_docker_ps check_kind
-	kubectl exec $$(kubectl get pods|grep agoric|cut -f 1 -d " ") -- agd tx bank send validator agoric1vaj34dfx94y6nvwt57dfyag5gfsp6eqjmvzu8c 1000000000ubld --keyring-backend=test --chain-id=agoriclocal --from=validator --yes
+	kubectl exec $$(kubectl get pods|grep agoric-validator|cut -f 1 -d " ") -- agd tx bank send validator agoric1vaj34dfx94y6nvwt57dfyag5gfsp6eqjmvzu8c 1000000000ubld --keyring-backend=test --chain-id=agoriclocal --from=validator --yes
 
 #####################
 # IBC query targets #
@@ -33,7 +33,7 @@ ibc_list_connections:
 ########################
 .PHONY: ibc_test_transfer_agoric_to_pocket
 ibc_test_transfer:
-	kubectl exec -it $$(kubectl get pods|grep agoric|cut -f 1 -d " ") -- agd tx ibc-transfer transfer transfer channel-0 pokt1mrqt5f7qh8uxs27cjm9t7v9e74a9vvdnq5jva4 1000ubld --keyring-backend=test --chain-id=agoriclocal --from=validator --yes
+	kubectl exec -it $$(kubectl get pods|grep agoric-validator|cut -f 1 -d " ") -- agd tx ibc-transfer transfer transfer channel-0 pokt1mrqt5f7qh8uxs27cjm9t7v9e74a9vvdnq5jva4 1000ubld --keyring-backend=test --chain-id=agoriclocal --from=validator --yes
 
 .PHONY: ibc_test_transfer_pocket_to_agoric
 ibc_test_transfer:
