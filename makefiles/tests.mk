2636580c7c7bc78a719bb5f7607d71dd1c86ac28
Bryan White <bryanchriswhite@gmail.com>
2025-07-22 11:37:20 +0200

    fix: make target flags

--- makefiles/tests.mk
+++ makefiles/tests.mk
@@ -51,19 +51,19 @@ test_e2e_oneshot: test_e2e_env ## Run only the E2E suite that exercises the ones
 .PHONY: test_load_relays_stress_custom
 test_load_relays_stress_custom: ## Run the stress test for E2E relays using custom manifest. "loadtest_manifest_example.yaml" manifest is used by default. Set 'LOAD_TEST_CUSTOM_MANIFEST' environment variable to use the different manifest.
 	go test -v -count=1 ./load-testing/tests/... \
-	-tags=load,test -run LoadRelays --log_level=debug --timeout=30m \
+	-tags=load,test -run LoadRelays --log-level=debug --timeout=30m \
 	--manifest ./load-testing/$(LOAD_TEST_CUSTOM_MANIFEST)
 
 .PHONY: test_load_relays_stress_localnet
 test_load_relays_stress_localnet: test_e2e_env warn_message_local_stress_test ## Run the stress test for E2E relays on LocalNet.
 	go test -v -count=1 ./load-testing/tests/... \
-	-tags=load,test -run TestLoadRelays --log_level=debug --timeout=30m \
+	-tags=load,test -run TestLoadRelays --log-level=debug --timeout=30m \
 	--manifest ./load-testing/loadtest_manifest_localnet.yaml
 
 .PHONY: test_load_relays_stress_localnet_single_supplier
 test_load_relays_stress_localnet_single_supplier: test_e2e_env warn_message_local_stress_test ## Run the stress test for E2E relays on LocalNet using exclusively one supplier.
 	go test -v -count=1 ./load-testing/tests/... \
-	-tags=load,test -run TestSingleSupplierLoadRelays --log_level=debug --timeout=30m \
+	-tags=load,test -run TestSingleSupplierLoadRelays --log-level=debug --timeout=30m \
 	--manifest ./load-testing/loadtest_manifest_localnet_single_supplier.yaml
 
 .PHONY: test_verbose
