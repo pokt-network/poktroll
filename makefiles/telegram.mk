7fb24e5bc1424299b5a9449ee2adfd6d839ffe0d
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-08-22 15:19:13 -0700

    self review

--- makefiles/telegram.mk
+++ makefiles/telegram.mk
@@ -34,16 +34,6 @@ telegram_test_message: ## Test broadcast message from file. Usage: make telegram
 	gh workflow run telegram-broadcast.yml -f message="$$MSG" -f test_mode=false --ref="$(shell git rev-parse --abbrev-ref HEAD)";
 	@echo "\nTest message sent. Check the workflow status at: $(CYAN)https://github.com/pokt-network/poktroll/actions/workflows/telegram-broadcast.yml$(RESET)"
 
-# .PHONY: telegram_test_message
-# telegram_test_message: ## Test broadcast message to testing group only. Usage: make telegram_test_message MSG="Your test message"
-# 	@if [ -z "$(MSG)" ]; then \
-# 		echo "Error: MSG parameter is required. Usage: make telegram_test_message MSG=\"Your test message\""; \
-# 		exit 1; \
-# 	fi
-# 	@echo "Sending test message to Telegram testing group...\n"
-# 	@gh workflow run telegram-broadcast.yml -f message="$(MSG)" -f test_mode=true
-# 	@echo "\nTest message sent. Check the workflow status at: $(CYAN)https://github.com/pokt-network/poktroll/actions/workflows/telegram-broadcast.yml$(RESET)"
-
 .PHONY: telegram_test_release
 telegram_test_release: ## Test release notification to testing group only. Usage: make telegram_test_release [RELEASE_TAG=v0.1.26]
 	@if [ -n "$(RELEASE_TAG)" ]; then \
