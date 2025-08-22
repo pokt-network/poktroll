7e24e23e7fd66a487f1f4489a28707d90c2a59f3
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-08-22 15:16:07 -0700

    [pocketd] Hide `UpdatePram` and `UpdateParams` from `pocketd tx ...` commands (#1743)
    All `UpdateParam` and `UpdateParams` method are authority-gated and must be executed via `pocktet tx authz...`.
    
    Removing it from `pocketd` in `autocli` to reduce cognitive overload on our users.
    
    <img width="1840" height="1191" alt="Screenshot 2025-08-22 at 3 11 57 PM" src="https://github.com/user-attachments/assets/e4a970d7-d97d-4971-92da-60cee5395d0b" />

--- x/proof/module/autocli.go
+++ x/proof/module/autocli.go
@@ -45,11 +45,15 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 		Tx: &autocliv1.ServiceCommandDescriptor{
 			Service:              prooftypes.Msg_serviceDesc.ServiceName,
 			EnhanceCustomCommand: true, // only required if you want to use the custom command
-			RpcCommandOptions:    []*autocliv1.RpcCommandOptions{
-				// {
-				// 	RpcMethod: "UpdateParams",
-				// 	Skip:      true, // skipped because authority gated
-				// },
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "UpdateParams",
+					Skip:      true, // skipped because authority gated
+				},
+				{
+					RpcMethod: "UpdateParam",
+					Skip:      true, // skipped because authority gated
+				},
 				// {
 				// 	RpcMethod:      "CreateClaim",
 				// 	Use:            "create-claim [session-header] [root-hash]",
