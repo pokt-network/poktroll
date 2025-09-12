a631ef59e981b5503fafac88d7a876f6fd96055e
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-09-11 17:47:35 -0700

    [Protocol Upgrade] Docs and `v0.1.29` release (#1771)
    - Protocol Upgrade Documentation
    - `v0.1.29` release artifacts
    
    ---
    
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

--- makefiles/telegram.mk
+++ makefiles/telegram.mk
@@ -2,15 +2,27 @@
 ### Telegram Helpers ###
 ########################
 
-.PHONY: telegram_broadcast
-telegram_broadcast: ## Broadcast a custom message to all Telegram groups. Usage: make telegram_broadcast MSG="Your message here"
-	@if [ -z "$(MSG)" ]; then \
-		echo "Error: MSG parameter is required. Usage: make telegram_broadcast MSG=\"Your message here\""; \
+.PHONY: telegram_broadcast_msg
+telegram_broadcast_msg: ## Broadcast a custom message to all Telegram groups. Usage: make telegram_broadcast_msg MSG_FILE=message.txt
+	@if [ -z "$(MSG_FILE)" ]; then \
+		echo "Error: MSG_FILE parameter is required. Usage: make telegram_broadcast_msg MSG_FILE=message.txt"; \
 		exit 1; \
 	fi
 	@echo "Broadcasting message to all Telegram groups...\n"
-	@gh workflow run telegram-broadcast.yml -f message="$(MSG)"
+	@MSG="$$(cat $(MSG_FILE) | sed -E 's/([][()~`>#+=|{}.!_-])/\\\1/g')"; \
+	gh workflow run telegram-broadcast.yml -f message="$$MSG" -f test_mode=false;
 	@echo "\nBroadcast initiated. Check the workflow status at: $(CYAN)https://github.com/pokt-network/poktroll/actions/workflows/telegram-broadcast.yml$(RESET)"
+
+.PHONY: telegram_test_broadcast_msg
+telegram_test_broadcast_msg: ## Test broadcast message from file. Usage: make telegram_test_broadcast_msg MSG_FILE=message.txt
+	@if [ -z "$(MSG_FILE)" ]; then \
+		echo "Error: MSG_FILE parameter is required. Usage: make telegram_test_broadcast_msg MSG_FILE=message.txt"; \
+		exit 1; \
+	fi
+	@echo "Sending test message to Telegram testing group...\n"
+	@MSG="$$(cat $(MSG_FILE) | sed -E 's/([][()~`>#+=|{}.!_-])/\\\1/g')"; \
+	gh workflow run telegram-broadcast.yml -f message="$$MSG" -f test_mode=true --ref="$(shell git rev-parse --abbrev-ref HEAD)";
+	@echo "\nTest message initiated. Check the workflow status at: $(CYAN)https://github.com/pokt-network/poktroll/actions/workflows/telegram-broadcast.yml$(RESET)"
 
 .PHONY: telegram_release_notify
 telegram_release_notify: ## Notify all Telegram groups of the latest release. Usage: make telegram_release_notify [RELEASE_TAG=v0.1.26]
@@ -23,16 +35,6 @@ telegram_release_notify: ## Notify all Telegram groups of the latest release. Us
 	fi
 	@echo "\nRelease notification initiated. Check the workflow status at: $(CYAN)https://github.com/pokt-network/poktroll/actions/workflows/telegram-notify-release.yml$(RESET)"
 
-.PHONY: telegram_test_message
-telegram_test_message: ## Test broadcast message from file. Usage: make telegram_test_message MSG_FILE=message.txt
-	@if [ -z "$(MSG_FILE)" ]; then \
-		echo "Error: MSG_FILE parameter is required. Usage: make telegram_test_message MSG_FILE=message.txt"; \
-		exit 1; \
-	fi
-	@echo "Sending test message to Telegram testing group...\n"
-	@MSG="$$(cat $(MSG_FILE) | sed -E 's/([][()~`>#+\-=|{}.!_])/\\\1/g')"; \
-	gh workflow run telegram-broadcast.yml -f message="$$MSG" -f test_mode=false --ref="$(shell git rev-parse --abbrev-ref HEAD)";
-	@echo "\nTest message sent. Check the workflow status at: $(CYAN)https://github.com/pokt-network/poktroll/actions/workflows/telegram-broadcast.yml$(RESET)"
 
 .PHONY: telegram_test_release
 telegram_test_release: ## Test release notification to testing group only. Usage: make telegram_test_release [RELEASE_TAG=v0.1.26]
