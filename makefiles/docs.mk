1588b0a53acb1ef53a5bc85204707bbfba9fd2e0
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-09-26 14:23:24 -0700

    [Performance] Update `go_dleq` dependencies (#1792)
    Updating dependencies to enable usage of Ethereum's seckp256k1 libraries.
    
    See #1793 for reference.

--- makefiles/docs.mk
+++ makefiles/docs.mk
@@ -14,3 +14,26 @@ docusaurus_start: check_yarn check_node ## Start the Docusaurus server
 .PHONY: docs_update_gov_params_page
 docs_update_gov_params_page: ## Update the page in Docusaurus documenting all the governance parameters
 	go run tools/scripts/docusaurus/generate_docs_params.go
+
+################
+### OpenAPI ###
+################
+
+.PHONY: openapi_ignite_gen
+openapi_ignite_gen: ignite_check_version ## Generate OpenAPI spec natively and process output
+	@ignite generate openapi --yes
+	@$(MAKE) process_openapi
+
+.PHONY: openapi_ignite_gen_docker
+openapi_ignite_gen_docker: ## Generate OpenAPI spec using Docker (workaround for ignite/cli#4495)
+	@docker build -f ./proto/Dockerfile.ignite -t ignite-openapi .
+	@docker run --rm -v "$(PWD):/workspace" ignite-openapi
+	@$(MAKE) process_openapi
+
+.PHONY: process_openapi
+# Internal helper: Process OpenAPI output to proper JSON/YAML format
+process_openapi:
+	@# Fix incorrectly named .yml file that contains JSON
+	@mv docs/static/openapi.yml docs/static/openapi.json
+	@yq -o=json '.' docs/static/openapi.json -I=4 > docs/static/openapi.json.tmp && mv docs/static/openapi.json.tmp docs/static/openapi.json
+	@yq -P -o=yaml '.' docs/static/openapi.json > docs/static/openapi.yml
