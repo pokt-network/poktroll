0013f3dc728fa7f0519ead9cdef63136a58bec30
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-09-30 22:13:21 -0700

    Update deps

--- makefiles/docs.mk
+++ makefiles/docs.mk
@@ -28,27 +28,6 @@ docs_build: ## Generate OpenAPI spec (Docker) and Docusaurus API docs
 	$(MAKE) openapi_ignite_gen_docker
 	$(MAKE) docusaurus_gen_api_docs
 
-.PHONY: openapi_ignite_gen
-openapi_ignite_gen: ignite_check_version ## Generate the OpenAPI spec natively and process the output
-	# Ensure Buf deps (incl. grpc-gateway) are available for imports like openapiv2 annotations
-	buf dep update
-	ignite generate openapi --yes
-	$(MAKE) openapi_process
-
-.PHONY: openapi_ignite_gen_docker
-openapi_ignite_gen_docker: ## Generate the OpenAPI spec using Docker and process the output; workaround due to https://github.com/ignite/cli/issues/4495
-	docker build -f ./proto/Dockerfile.ignite -t ignite-openapi .
-	docker run --rm -v "$(PWD):/workspace" ignite-openapi
-	$(MAKE) openapi_process
-
-
-.PHONY: openapi_process
-# Internal helper target - Ensure OpenAPI JSON and YAML files are properly formatted
-openapi_process: check_yq
-# Ignite currently writes JSON content to docs/static/openapi.yml. Create canonical JSON, then YAML from it.
-	yq -o=json '.' docs/static/openapi.yml -I=4 > docs/static/openapi.json
-	yq -P -o=yaml '.' docs/static/openapi.json > docs/static/openapi.yml
-
 ################
 ### OpenAPI ###
 ################
