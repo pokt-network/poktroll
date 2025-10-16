d3341d5e989dbb3d96a4d85186ca87471efbf360
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-10-16 13:51:39 -0700

    Making things work locally

--- makefiles/services.mk
+++ makefiles/services.mk
@@ -10,7 +10,7 @@
 
 .PHONY: service_list
 service_list: ## List all services registered on the network
-	pocketd --home=$(POCKETD_HOME) q service list-service --node $(POCKET_NODE)
+	pocketd --home=$(POCKETD_HOME) q service all-services --node $(POCKET_NODE)
 
 .PHONY: service_show
 service_show: ## Show details of a service (must specify SERVICE_ID env var)
@@ -23,7 +23,7 @@ service_add: ## Add a new service (must specify SERVICE_ID, SERVICE_NAME, COMPUT
 		--from $(SERVICE_OWNER) --keyring-backend test --node $(POCKET_NODE) -y
 
 .PHONY: service_add_with_metadata_file
-service_add_with_metadata_file: ## Add a service with metadata from file (must specify SERVICE_ID, SERVICE_NAME, COMPUTE_UNITS, SERVICE_OWNER, METADATA_FILE env vars)
+service_add_with_metadata_file: ## Internal Helper: Add a service with metadata from file (must specify SERVICE_ID, SERVICE_NAME, COMPUTE_UNITS, SERVICE_OWNER, METADATA_FILE env vars)
 	pocketd --home=$(POCKETD_HOME) tx service add-service \
 		$(SERVICE_ID) "$(SERVICE_NAME)" $(COMPUTE_UNITS) \
 		--experimental--metadata-file $(METADATA_FILE) \
@@ -36,8 +36,8 @@ service_add_with_metadata_base64: ## Add a service with base64-encoded metadata 
 		--experimental--metadata-base64 $(METADATA_BASE64) \
 		--from $(SERVICE_OWNER) --keyring-backend test --node $(POCKET_NODE) -y
 
-.PHONY: service_add_pocket
-service_add_pocket: ## Add the pocket service with OpenAPI specification metadata
+.PHONY: service_pocket_add_metadata_file
+service_pocket_add_metadata_file: ## Add the pocket service with OpenAPI specification metadata
 	SERVICE_ID=pocket \
 	SERVICE_NAME="Pocket Network RPC" \
 	COMPUTE_UNITS=1 \
@@ -45,8 +45,8 @@ service_add_pocket: ## Add the pocket service with OpenAPI specification metadat
 	METADATA_FILE=docs/static/openapi_small.json \
 	make service_add_with_metadata_file
 
-.PHONY: service_update_pocket_metadata
-service_update_pocket_metadata: ## Update the pocket service metadata with OpenAPI specification
+.PHONY: service_pocket_update_metadata_file
+service_pocket_update_metadata_file: ## Update the pocket service metadata with OpenAPI specification
 	SERVICE_ID=pocket \
 	SERVICE_NAME="Pocket Network RPC" \
 	COMPUTE_UNITS=1 \
