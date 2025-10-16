7b5b0162c3502a603851cf07cedd1045e710ce42
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-10-16 12:26:11 -0700

    Fix docusurus build

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -16,6 +16,9 @@ const config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
   },
 
   themes: [
@@ -90,7 +93,6 @@ const config = {
   trailingSlash: false,
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
